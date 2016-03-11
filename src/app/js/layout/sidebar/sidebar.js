127ee41ac3de5dd6ea2562044760fe2f3753e217
fbellina <francescobellina84@gmail.com>
2016-03-11 15:06:13 +0100

    bozza

--- src/app/js/layout/sidebar/sidebar.js
+++ src/app/js/layout/sidebar/sidebar.js
@@ -1,3 +1,90 @@
+var t = require('i18n.service.js');
+
 Vue.component('sidebar',{
-    template: require('./sidebar.html')
-})
+    template: require('./sidebar.html'),
+    data: function() {
+    	return {
+        	bOpen: true,
+    		bPageMode: false,
+    		header: t('main navigation'),
+        };
+    },
+    methods: {
+    	activeModule: function(index) {
+    		if (this.currentModule === index) {
+    			this.currentModule = undefined;
+    			return false;
+    		}
+    		this.currentModule = index;
+    	}
+	}
+});
+
+Vue.component('sidebar-item',{
+	props: ['data-icon','data-label','data-content','data-component'],
+    template: require('./sidebar-item.html'),
+    data: function() {
+    	return {
+        	
+        };
+    },
+    methods: {
+    	
+	}
+});
+
+Vue.component('search',{
+    template: require('./search.html'),
+    data: function() {
+    	return {
+        	
+        };
+    },
+    methods: {
+    	
+	}
+});
+
+Vue.component('catalog',{
+    template: require('./catalog.html'),
+    data: function() {
+    	return {
+        	tree: [
+		       {
+		    	    text: "Parent 1",
+		    	    nodes: [
+		    	      {
+		    	        text: "Child 1",
+		    	        nodes: [
+		    	          {
+		    	            text: "Grandchild 1"
+		    	          },
+		    	          {
+		    	            text: "Grandchild 2"
+		    	          }
+		    	        ]
+		    	      },
+		    	      {
+		    	        text: "Child 2"
+		    	      }
+		    	    ]
+		    	  },
+		    	  {
+		    	    text: "Parent 2"
+		    	  },
+		    	  {
+		    	    text: "Parent 3"
+		    	  },
+		    	  {
+		    	    text: "Parent 4"
+		    	  },
+		    	  {
+		    	    text: "Parent 5"
+		    	  }
+		    	]
+        };
+    },
+    methods: {
+    	
+	}
+});
