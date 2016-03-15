01070dab280547f74082d37645706d99990a2eaf
giohappy <giohappy@gmail.com>
2016-03-15 16:31:43 +0100

    restructure app template

--- src/app/index.js
+++ src/app/index.js
@@ -4,3 +4,9 @@ $('.content-wrapper').html(t('text1'));
 
 var appUi = require('app.ui');
 var appService = require('app.service');
+
+var app = new Vue({
+	el: 'body'
+});
+
+module.exports = app;
--- src/app/js/app.ui.js
+++ src/app/js/app.ui.js
@@ -1,16 +1,9 @@
 var t = require('i18n.service');
 var layout = require('layout/layout');
 
-var app = new Vue({
-	el: 'body',
-	data: {
-	},
-	methods: {
-		
-	},
-	ready: function(){
+Vue.component('app',{
+    template: require('./app.html'),
+    ready: function(){
 		layout.setup();
 	}
-});
-
-module.exports = app;
+})
