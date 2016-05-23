3c03dd3302b56ad4c3c3d9d45de3b8ad8303cb3a
giohappy <giohappy@gmail.com>
2016-05-23 15:56:05 +0200

    Caricamento plugin e strumenti iternet solo per progetti Iternet

--- src/libs/plugins/g3w-iternet/plugin.js
+++ src/libs/plugins/g3w-iternet/plugin.js
@@ -1,4 +1,7 @@
 var inherit = require('g3w/core/utils').inherit;
+var resolvedValue = require('g3w/core/utils').resolvedValue;
+var rejectedValue = require('g3w/core/utils').rejectedValue;
+var ProjectsRegistry = require('g3w/core/projectsregistry');
 var GUI = require('g3w/gui/gui');
 var G3WPlugin = require('g3w/core/plugin');
 
@@ -7,6 +10,7 @@ var Service = require('./iternetservice');
 var EditingPanel = require('./editorpanel')
 
 function IternetPlugin(){
+  this.config = null;
   this.name = "iternet"
   this.tools = [
     {
@@ -18,14 +22,28 @@ function IternetPlugin(){
   ];
   
   this.init = function(config){
-    Service.init(config);
-  }
+    this.config = config;
+    if (this.isCurrentProjectCompatible(config)) {
+      Service.init(config);
+      return resolvedValue();
+    }
+    return rejectedValue();
+  };
+  
+  this.isCurrentProjectCompatible = function(config){
+    var gid = config.gid;
+    var project = ProjectsRegistry.getCurrentProject();
+    if (gid == project.gid) {
+      return true;
+    }
+    return false;
+  };
   
   // metodo in risposta all'azione iternet:startEditing; aggiunge il pannello alla sidebar
   this.startEditing = function(){
     var panel = new EditingPanel();
     GUI.showPanel(panel);
-  }
+  };
 }
 inherit(IternetPlugin,G3WPlugin);
 
