25d60435c900369a8c6956f86412d99a51b28f74
giohappy <giohappy@gmail.com>
2016-07-11 12:52:25 +0200

    ridistribuzione dei compiti di bootstrap (codice non funzionante)

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,64 @@
+var i18ninit = require('sdk/core/i18n/i18n.service').init;
 var config = require('./config/config.js');
 var TemplateConfig = ('./config/config.template.js');
-//var ApplicationService = require('sdk/sdk').core.Application;
-var ApplicationService = {};
+var ApplicationService = require('sdk/sdk').core.Application;
 var ApplicationTemplate = require('./template/js/template');
-//istanzio l'appication template
-ApplicationTemplate = new ApplicationTemplate();
-//passo la configurazione del template e l'applicationService che fornisce API del progetto
-ApplicationTemplate.init(TemplateConfig, ApplicationService);
-//rimasto da codice precedente. Da togliere?
 
+i18ninit(baseconfig.i18n);
+
+function createConfig(config){
+    return {
+      debug: baseconfig.client.debug || false,
+      group: null,
+      urls: baseconfig.server.urls,
+      resourcesurl: baseconfig.server.urls.staticurl,
+      projects: baseconfig.group.projects,
+      initproject: baseconfig.group.initproject,
+      baselayers: baseconfig.group.baselayers,
+      crs: baseconfig.group.crs,
+      proj4: baseconfig.group.proj4,
+      minscale: baseconfig.group.minscale,
+      maxscale: baseconfig.group.maxscale,
+      // richiesto da ProjectService
+      getWmsUrl: baseconfig.getWmsUrl,
+      // richiesto da ProjectsRegistry
+      getProjectConfigUrl: baseconfig.getProjectConfigUrl,
+      plugins: {
+        plugins: baseconfig.plugins,
+        configs: baseconfig.group.plugins
+      },
+      tools: baseconfig.tools,
+      views: baseconfig.views || {},
+      map: baseconfig.map
+    }
+  };
+  
+if (window.initConfig) {
+    baseconfig.server.urls.staticurl = initConfig.staticurl;
+    baseconfig.group = window.initConfig.group; // config is inlined by g3w-admin inside the index template as a <script> tag
+    var config = createConfig(config);
+    appService.init(config);// emette evento ready dopo aver letto la configurazione
+  }
+  // altrimenti devo aspettare che local.initconfig.js abbia caricato l'initconfig
+  else{
+    $(document).on('initconfigReady',function(e,initconfig){
+      baseconfig.server.urls.staticurl = initconfig.staticurl;
+      baseconfig.group = initconfig.group;
+      var config = createConfig(config);
+      run()
+    })
+  }
+
+ApplicationService.on('ready',function(){
+  //istanzio l'appication template
+  applicationTemplate = new ApplicationTemplate();
+  //passo la configurazione del template e l'applicationService che fornisce API del progetto
+  applicationTemplate.init(ApplicationService,TemplateConfig);
+});
+
+ApplicationService.init();
+
+/*
 $(function (){
   config.getWmsUrl = function(project){
     return config.server.urls.ows+'/'+config.group.id+'/'+project.type+'/'+project.id;
@@ -17,6 +67,7 @@ $(function (){
     return config.server.urls.config+'/'+config.group.id+'/'+project.type+'/'+project.id;
   }
 });
+*/
 
 /* -----------------------------------------------------------------------------------------*/
 
--- src/app/template/js/template.js
+++ src/app/template/js/template.js
@@ -1,4 +1,45 @@
-var bootstrap = require('./bootstrap');
+var t = require('sdk/core/i18n/i18n.service').t;
+require('sdk/gui/vue.directives');
+var isMobileMixin = require('sdk/gui/vue.mixins').isMobileMixin;
+var layout = require('./layout');
+
+
+/* TUTTO QUESTO CODICE ERA NEL BOOTSTRAP. LO DEVE GESTIRE IL TEMPLATE 
+Vue.filter('t', function (value) {
+  return t(value);
+});
+
+if (config.client.debug){
+  Vue.config.debug = true;
+}
+
+Vue.mixin(isMobileMixin);
+
+var SidebarComponent = require('./sidebar').SidebarComponent;
+var FloatbarComponent = require('./floatbar').FloatbarComponent;
+var AppUI = require('./applicationui');
+
+var SideBar = SidebarComponent.extend({
+  mixins: [isMobileMixin]
+});
+Vue.component('sidebar',SidebarComponent);
+Vue.component('floatbar',FloatbarComponent);
+Vue.component('app', AppUI);
+
+
+
+//inizializza la vue appicazione
+function run(){
+  app = new Vue({
+    el: 'body',
+    ready: function(){
+      $(document).localize();
+    }
+  });
+};
+layout.loading();
+*/
+
 var ApplicationTemplate = function() {
   this.config = {};
   this.init = function(config, ApplicationService) {
