04ad24d56dd591b960a3ad24f6cd1c2d7364f06c
giohappy <giohappy@gmail.com>
2016-11-03 13:27:09 +0100

    transizione componenti catasto; inserimento componente "menu" per sidebar (attualmente non usato nel client)

---
+++ src/app/template/js/menu.js
@@ -0,0 +1,51 @@
+var t = require('core/i18n/i18n.service').t;
+var inherit = require('core/utils/utils').inherit;
+var base = require('core/utils/utils').base;
+var merge = require('core/utils/utils').merge;
+var Component = require('gui/vue/component');
+
+var InternalComponent = Vue.extend({
+  template: require('../html/menu.html'),
+  data: function() {
+    return {
+      state: null
+    }
+  },
+  methods: {
+    trigger: function(item) {
+      if (item.cbk) {
+        item.cbk.apply(item);
+      }
+      else if (item.href) {
+        window.open(item.href, '_blank');
+      }
+      else {
+        console.log("Nessuna azione per "+item.title);
+      }
+    }
+  }
+});
+
+function MenuComponent(options){
+  base(this,options);
+  //this.id = "menu_"+Date.now();
+  this.title = options.title || "menu";
+  this.state.visible = true;
+  this.state.menuitems = options.menuitems
+
+  merge(this, options);
+  this.internalComponent = new InternalComponent({
+    service: this
+  });
+  this.internalComponent.state = this.state;
+};
+inherit(MenuComponent, Component);
+
+var proto = MenuComponent.prototype;
+
+proto.trigger = function(item) {
+
+};
+
+module.exports = MenuComponent;
+
