2d1f2fde3906797b98f0ddbe92c379cab4e82689
giohappy <giohappy@gmail.com>
2017-07-06 17:16:42 +0200

    inserita sostituzione variabile di versioning a tempo di compilazione

--- gulpfile.js
+++ gulpfile.js
@@ -68,6 +68,7 @@ gulp.task('browserify', [], function() {
       })
       .pipe(source('build.js'))
       .pipe(buffer())
+      .pipe(gulpif(production, replace("{G3W_VERSION}",versionHash)))
       .pipe(gulpif(!production,sourcemaps.init({ loadMaps: true })))
       .pipe(gulpif(production, uglify().on('error', gutil.log)))
       .pipe(gulpif(!production,sourcemaps.write()))
