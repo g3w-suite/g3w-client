0064e39b3e2c48a919d02c368103d5afcd2dee42
volterra79 <francescoboccacci@libero.it>
2018-11-13 15:17:00 +0100

    moved a plugin specific transaltion to own code

--- src/app/config/locales/app.js
+++ src/app/config/locales/app.js
@@ -6,8 +6,6 @@ const translations = {
       "back":"Indietro",
       "backto": "Torna a ",
       "changemap": "Cambia Mappa",
-      "editing_data": "Editing Dati",
-      "editing_attributes": "Editita attributi",
       "component": "Componente Generico",
       "search": "Ricerche",
       "no_results": "Nessun risultato trovato",
@@ -40,70 +38,6 @@ const translations = {
       "exitnosave": "Esci senza salvare",
       "annul": "Annulla",
       "layer_is_added": "Layer già aggiunto",
-      "back_to_relations": "Ritorna alle Relazioni",
-      "no_relations_found": "Non ci sono relazioni",
-      "relation_data": "Dati relativi alla relazione",
-      "relation_already_added" : "Relazione già presente",
-      "list_of_relations_feature": "Elenco Relazioni della feature ",
-      "editing": {
-        "edit_relation": "Edita relazione",
-        "inputs": {
-          "media": {
-            "unsupported": "Formato non supportato"
-          },
-        },
-        "tools": {
-          "add_feature": "Aggiungi feature",
-          "delete_feature": "Elimina feature",
-          "move_feature": "Muovi feature",
-          "update_vertex": "Aggiorna vertici feature",
-          "update_feature": "Modifica feature"
-        },
-        "steps": {
-          "help": {
-            "new": "Creo una nuova feature",
-            "edit_table": "Edita le features della tabella",
-            "draw_new_feature": "Disegna sulla mappa la feature",
-            "action_confirm": "Conferma azione",
-            "double_click_delete": "Doppio Click sulla feature che vuoi cancellare",
-            "edit_feature_vertex": "Modifica o aggiungi un vertice alla feature selezionata",
-            "move": "Muovi la feature selezionata",
-            "select_feature_to_relation": "Seleziona la feature che vuoi mettere in relazione",
-            "show_edit_feature_form":  "Mostra il form della feature per poter editare gli attributi",
-            "pick_feature": "Clicca su una feature per poterla modificare",
-            "insert_attributes_feature": "Inserisci gli attributi della feature"
-          }
-        },
-        "messages": {
-          "delete_feature": "Vuoi eliminare l'elemento selezionato?",
-          "commit_feature": "Vuoi salvare definitivamente le modifiche",
-          "change_toolbox_relation": "Layer in relazione. Prima di passare ad altri editing è obbligatorio salvare le modifiche correnti.",
-          "saved": "I dati sono stati salvati correttamente",
-          "loading_data": "Caricamento dati",
-          "saving": "Salvataggio dati in corso. Attendere ...",
-          "qgis_input_widget_relation": "Gestisci le relazioni tramite form dedicato",
-          "commit": {
-            "add": "Aggiunte",
-            "delete": "Cancellate",
-            "update": "Modificate"
-          }
-        },
-        "relation": {
-          "table": {
-            "info": `
-                  <div>
-                    Questa è la lista delle relazioni orfane o associate ad altre geometrie.
-                    Clicca sulla singola relazione per associarla alla geometria in editing.
-                  </div>
-                  <div>
-                    <span style="font-weight: bold">
-                      ATTENZIONE
-                    </span>: nel caso in cui la relazione sia attualmente associata ad un'altra geometria, verrà dissociata da questa
-                  </div>
-                `
-          }
-        },
-      },
       "sidebar": {},
       "info": {
         "title": "Risultati",
@@ -119,14 +53,6 @@ const translations = {
           "text": "testuale",
           "string": "stringa",
           "date": "data"
-        },
-        "relations": {
-          "tooltips": {
-            "add_relation": "Crea ed aggiungi nuova relazione",
-            "link_relation": "Associa una relazione esistente a questa feature",
-            "open_relation_tools": "Apri strumenti relatione",
-            "unlink_relation": "Annulla relazione"
-          }
         },
         "footer": {
           "required_fields": "Campi richiesti"
@@ -182,8 +108,6 @@ const translations = {
       "back": "Back",
       "backto": "Back to ",
       "changemap": "Change Map",
-      "editing_data": "Editing Layers",
-      "editing_attributes": "Editing attributes",
       "component": "Generic Component",
       "search": "Search",
       "no_results": "No results found",
@@ -215,71 +139,6 @@ const translations = {
       "exitnosave": "Exit without save",
       "annul": "Cancel",
       "layer_is_added": "Layer already added",
-      "back_to_relations": "Back to Relations",
-      "no_relations_found": "No relations found",
-      "relation_data": "Relation data",
-      "relation_already_added": "Relation already added",
-      "list_of_relations_feature": "List of relations feature ",
-      "editing": {
-        "edit_relation": "Edit relation",
-        "inputs": {
-          "media": {
-            "unsupported": "Unsopported format"
-          }
-        },
-        "tools": {
-          "add_feature": "Add feature",
-          "delete_feature": "Delete feature",
-          "move_feature": "Move feature",
-          "update_vertex": "Update feature vertex",
-          "update_feature": "Update feature"
-        },
-        "steps": {
-          "help": {
-            "new": "Create new feature",
-            "edit_table": "Edit table features",
-            "draw_new_feature": "Draw feature on map",
-            "action_confirm": "Confirm action",
-            "double_click_delete": "Double Click on the feature to delete",
-            "edit_feature_vertex": "Modify or add a vertex on selected feature",
-            "move": "Move selected feature",
-            "select_feature_to_relation": "Select feature that you bind relation",
-            "show_edit_feature_form":  "Show feature form to edit attributes",
-            "pick_feature": "Click on feature to modify",
-            "insert_attributes_feature": "Insert attributes of the feature"
-          }
-        },
-        "messages": {
-          "delete_feature": "Do you want delete selected feature ?",
-          "commit_feature": "Do you want to save the changes",
-          "change_toolbox_relation": "Layer has relation/relations. Before switch editing you need to save changes done.",
-          "saved": "Data saved successfully",
-          "loading_data": "Loading data",
-          "saving": "Saving data. Please wait ...",
-          "qgis_input_widget_relation": "Use relation specific form to work with relation",
-          "pdf": "Document preview not available. Please click here ",
-          "commit": {
-            "add": "Added",
-            "delete": "Deleted",
-            "update": "Modified"
-          }
-        },
-        "relation": {
-          "table": {
-            "info": `
-                  <div>
-                    This is a list of orphan or binded relations to other features.
-                    Click on single relation to link it to current editing feature.
-                  </div>
-                  <div>
-                    <span style="font-weight: bold">
-                      ATTENTION
-                    </span>: in case of this relation is already bind to another, the previous link is lost
-                  </div>
-                `
-          }
-        },
-      },
       "sidebar": {},
       "info": {
         "title": "Results",
@@ -295,14 +154,6 @@ const translations = {
           "text": "text",
           "string": "string",
           "date": "date"
-        },
-        "relations": {
-          "tooltips": {
-            "add_relation": "Create and link new relation",
-            "link_relation": "Join a relation to this feature",
-            "open_relation_tools": "Show relation tools",
-            "unlink_relation": "Unlink relation"
-          }
         },
         "footer": {
           "required_fields": "Required fields"
