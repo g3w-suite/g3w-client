d4df96e619e9c5623dd3e181caed5f4a4bd83452
volterra79 <boccacci.francesco@gmail.com>
2021-01-05 17:52:14 +0100

    added clear search when enable disable dilter

--- src/app/gui/table/vue/components/g3w-table-toolbar.vue
+++ src/app/gui/table/vue/components/g3w-table-toolbar.vue
@@ -2,7 +2,7 @@
   <div v-show="tools.show" style="display: flex; justify-content: space-between; width: 50px; padding: 3px;">
     <div class="skin-color action-button skin-tooltip-right" data-placement="right" data-toggle="tooltip" :class="g3wtemplate.getFontClass('clear')" v-t-tooltip.create="'layer_selection_filter.tools.clear'" @click.stop="clearAllSelection"></div>
     <div class="skin-color action-button skin-tooltip-right" data-placement="right" data-toggle="tooltip" :class="[g3wtemplate.getFontClass('invert'), tools.filter.active ? 'g3w-disabled': '']" v-t-tooltip.create="'layer_selection_filter.tools.invert'"   @click.stop="switchSelection"></div>
-    <div class="skin-color action-button skin-tooltip-right" data-placement="right" data-toggle="tooltip"  @click.stop="activeDeactiveLayerFilter" :class="[g3wtemplate.getFontClass('filter'), tools.filter.active ? 'toggled' : '']" v-t-tooltip.create="'layer_selection_filter.tools.filter'"></div>
+    <div class="skin-color action-button skin-tooltip-right" data-placement="right" data-toggle="tooltip"  @click.stop="toggleFilterToken" :class="[g3wtemplate.getFontClass('filter'), tools.filter.active ? 'toggled' : '']" v-t-tooltip.create="'layer_selection_filter.tools.filter'"></div>
   </div>
 </template>
 
@@ -19,7 +19,7 @@
       clearAllSelection: {
         type: Function
       },
-      activeDeactiveLayerFilter: {
+      toggleFilterToken: {
         type: Function
       }
     }
