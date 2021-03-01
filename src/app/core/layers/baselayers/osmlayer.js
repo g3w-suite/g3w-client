309de22257d2a0ce4e003762d2c27b02aaaa7ff8
volterra79 <boccacci.francesco@gmail.com>
2021-03-01 14:03:48 +0100

    fixed issue on baselayers osm

--- src/app/core/layers/baselayers/osmlayer.js
+++ src/app/core/layers/baselayers/osmlayer.js
@@ -14,7 +14,8 @@ const proto = OSMLayer.prototype;
 proto._makeOlLayer = function() {
   const olLayer = BasesLayers.OSM.get({
     id: this.config.name,
-    title: this.config.title
+    title: this.config.title,
+    url: this.config.url
   });
   return olLayer
 };
