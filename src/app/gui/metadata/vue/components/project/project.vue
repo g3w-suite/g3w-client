70df984255923c95829f416a1d4aac7190d19f0b
volterra79 <boccacci.francesco@gmail.com>
2021-03-15 14:43:30 +0100

    set metadata tilte centered and change tooltip multi feature on query response bottom

--- src/app/gui/metadata/vue/components/project/project.vue
+++ src/app/gui/metadata/vue/components/project/project.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="project-catalog">
-    <h4 class="content-header-component skin-color">{{ state.name }}</h4>
+    <h4 style="text-align: center" class="content-header-component skin-color">{{ state.name }}</h4>
     <div id="project-catalog-container" style="padding:0" :class="isMobile() ? 'mobile' : null">
       <ul role="tablist" class="nav nav-tabs metadata-nav-bar">
         <li class="active">
