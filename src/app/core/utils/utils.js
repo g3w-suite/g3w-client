8b0a3e3977aafcbbd46b3f683ce60199105afae6
volterra79 <boccacci.francesco@gmail.com>
2021-04-09 11:13:50 +0200

    add color converter method from hex to rgb

--- src/app/core/utils/utils.js
+++ src/app/core/utils/utils.js
@@ -506,7 +506,19 @@ const utils = {
       context,
       method
     }
-  }
+  },
+
+  /**
+   * Convert Hex value color to RGB array
+   * @param color
+   * @returns {number[]}
+   */
+  colorHEXToRGB(color='#FFFFFF'){
+    const r = parseInt(color.substr(1,2), 16);
+    const g = parseInt(color.substr(3,2), 16);
+    const b = parseInt(color.substr(5,2), 16);
+    return [r,g,b]
+  },
 };
 
 module.exports = utils;
