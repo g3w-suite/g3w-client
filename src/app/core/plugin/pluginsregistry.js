cfccccc61e270279cc6f002cce6dec0027e4b120
volterra79 <boccacci.francesco@gmail.com>
2021-10-04 17:42:39 +0200

    aligned with dev

--- src/app/core/plugin/pluginsregistry.js
+++ src/app/core/plugin/pluginsregistry.js
@@ -57,7 +57,7 @@ function PluginsRegistry() {
   };
 
   this.setDependencyPluginConfig = function(){
-    for (pluginName in this.pluginsConfigs){
+    for (const pluginName in this.pluginsConfigs){
       const dependecyPluginConfig = this.pluginsConfigs[pluginName].plugins;
       dependecyPluginConfig && Object.keys(dependecyPluginConfig).forEach(pluginName =>{
         this.pluginsConfigs[pluginName] = {...this.pluginsConfigs[pluginName], ...dependecyPluginConfig[pluginName]}
