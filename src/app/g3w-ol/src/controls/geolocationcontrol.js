0f0a85c313111855be1c454f2a7947269e07bee6
volterra79 <boccacci.francesco@gmail.com>
2021-10-08 15:39:33 +0200

    fixed issue on mobile change position geolocation control

--- src/app/g3w-ol/src/controls/geolocationcontrol.js
+++ src/app/g3w-ol/src/controls/geolocationcontrol.js
@@ -6,7 +6,19 @@ function GeolocationControl() {
     tipLabel: "sdk.mapcontrols.geolocation.tooltip",
     label: "\ue904"
   };
-  this._layer;
+  this.initiliazed = false;
+  this._layer = new ol.layer.Vector({
+    source: new ol.source.Vector(),
+    style: new ol.style.Style({
+      text: new ol.style.Text({
+        text: '\uf3c5',
+        font: '900 3em "Font Awesome 5 Free"',
+        fill: new ol.style.Fill({
+          color: 'red'
+        })
+      })
+    })
+  });
   InteractionControl.call(this, options);
 }
 
@@ -14,54 +26,72 @@ ol.inherits(GeolocationControl, InteractionControl);
 
 const proto = GeolocationControl.prototype;
 
-proto._showMarker = function(coordinates, show=true){
-  const feature = new ol.Feature({
-    geometry: new ol.geom.Point(coordinates)
-  });
+/**
+ * Method to add market position layer and feature point always on top of map
+ * @param map
+ * @param coordinates
+ * @param show
+ * @private
+ */
+proto._showMarker = function({map, coordinates, show=true}){
   this._layer.getSource().clear();
-  show && setTimeout(()=>this._layer.getSource().addFeature(feature));
+  if (show)  {
+    map.getView().setCenter(coordinates);
+    const feature = new ol.Feature({
+      geometry: new ol.geom.Point(coordinates)
+    });
+    this._layer.getSource().addFeature(feature);
+    map.addLayer(this._layer);
+  } else map.removeLayer(this._layer);
+};
+
+proto.getMap = function(){
+  return InteractionControl.prototype.getMap.call(this);
 };
 
 proto.setMap = function(map) {
   InteractionControl.prototype.setMap.call(this, map);
+
   const geolocation = new ol.Geolocation({
     projection: map.getView().getProjection(),
-    tracking: true,
+    tracking: true, // set tracking
     trackingOptions: {
       enableHighAccuracy: true
     }
   });
-  geolocation.once('change:position', e => {
+
+  geolocation.on('change:position', () => {
     const coordinates = geolocation.getPosition();
     if (coordinates) {
-      this._layer = new ol.layer.Vector({
-        source: new ol.source.Vector(),
-        style: new ol.style.Style({
-          text: new ol.style.Text({
-            text: '\uf3c5',
-            font: '900 3em "Font Awesome 5 Free"',
-            fill: new ol.style.Fill({
-              color: 'red'
-            })
-          })
-        })
-      });
-      const coordinates = geolocation.getPosition();
-      const view = map.getView();
-      map.addLayer(this._layer);
-      $(this.element).removeClass('g3w-ol-disabled');
-      this.on('toggled', event => {
-        const toggled = event.target.isToggled();
-        toggled &&  view.setCenter(coordinates);
-        this._showMarker(coordinates, toggled);
-      });
-    } else this.hideControl();
+      if (!this.initiliazed) {
+        $(this.element).removeClass('g3w-ol-disabled');
+        this.initiliazed = true;
+      }
+      this._showMarker({
+        map,
+        coordinates,
+        show: this.isToggled()
+      })
+    } else {
+      this.initiliazed = true;
+      this.hideControl();
+    }
   });
-  geolocation.once('error', (e) => {
+
+  geolocation.once('error', evt => {
     this.hideControl();
-    if (e.code !== 1) this.dispatchEvent('error');
+    this._layer = null;
+    evt.code !== 1 && this.dispatchEvent('error');
+  });
+
+  this.on('toggled', () => {
+    const coordinates = geolocation.getPosition();
+    this._showMarker({
+      map,
+      coordinates,
+      show: this.isToggled()
+    })
   });
 };
-
 
 module.exports = GeolocationControl;
