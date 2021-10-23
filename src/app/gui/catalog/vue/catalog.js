85f190dda8da027e7ecdb384eeb015ff1fed1a16
volterra79 <boccacci.francesco@gmail.com>
2021-10-23 13:56:34 +0200

    fiexed issue on mutually exlusive

--- src/app/gui/catalog/vue/catalog.js
+++ src/app/gui/catalog/vue/catalog.js
@@ -651,7 +651,7 @@ Vue.component('tristate-tree', {
             layerId: id
           }));
           if (parentGroup.mutually_exclusive) {
-            parentGroup.nodes.forEach(node => node.checked = node.id === layer.id);
+            parentGroup.nodes.forEach(node => node.checked = node.id === id);
           }
           while (parentGroup){
             parentGroup.checked = true;
