5ac39aefed6faca21f6701d99848eb1cba22a03a
volterra79 <boccacci.francesco@gmail.com>
2022-03-30 09:16:12 +0200

    fixed issue on wms url

--- src/app/core/utils/utils.js
+++ src/app/core/utils/utils.js
@@ -179,18 +179,10 @@ const utils = {
   trimValue: value => value.replace(/ /g,''),
   /**
    * Method to check if is a url
-   * @param str
+   * @param url
    * @returns {boolean}
    */
-  isURL: str =>  {
-    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return !!pattern.test(str);
-  },
+  isURL: url => url && url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g),
 
   sanitizeUrl({url, reserverParameters=[]}={}){
     const checkUrl = new URL(url);
