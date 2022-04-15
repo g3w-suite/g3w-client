40a7129c9d121384668f5c9b3c83b5e30e11dcbd
volterra79 <boccacci.francesco@gmail.com>
2022-04-15 17:54:04 +0200

    handle async condition action visualization

--- src/app/gui/queryresults/vue/components/action.vue
+++ src/app/gui/queryresults/vue/components/action.vue
@@ -1,5 +1,5 @@
 <template>
-   <span v-if="showAction(action)" @contextmenu.prevent.stop="" @click.stop="clickAction(action, layer, feature, featureIndex, $event)" v-download="action.download"
+   <span v-if="show" @contextmenu.prevent.stop="" @click.stop="clickAction(action, layer, feature, featureIndex, $event)" v-download="action.download"
       :class="{'toggled': action.state && action.state.toggled[featureIndex] }" class="action-button skin-tooltip-right" data-placement="right" data-toggle="tooltip" v-t-title="action.hint">
       <span style="padding: 2px;"  :style="action.style" :class="`action-button-icon ${action.class}`"></span>
     </span>
@@ -9,6 +9,11 @@
   const {t} = require('core/i18n/i18n.service');
   export default {
     name: "action",
+    data(){
+      return {
+        show: true
+      }
+    },
     props: {
       featureIndex: {
         type: Number
@@ -28,10 +33,6 @@
       },
     },
     methods: {
-      showAction(action){
-        action.init && action.init({layer: this.layer, feature: this.feature, index:this.featureIndex, action});
-        return typeof action.condition === 'function' ? action.condition({layer:this.layer, feature:this.feature}) : true;
-      },
       async clickAction(action, layer, feature, featureIndex, event){
         await this.trigger(action, layer, feature, featureIndex);
         if (action.hint_change) {
@@ -45,6 +46,13 @@
           }, action.hint_change.duration || 600)
         }
         this.$emit('action-clicked', action)
+      }
+    },
+    async created(){
+      this.action.init && this.action.init({layer: this.layer, feature: this.feature, index:this.featureIndex, action:this.action});
+      if (typeof this.action.condition === 'function') {
+        const show = this.action.condition({layer:this.layer, feature:this.feature});
+        this.show = show instanceof Promise ? await show: show;
       }
     },
     async mounted(){
