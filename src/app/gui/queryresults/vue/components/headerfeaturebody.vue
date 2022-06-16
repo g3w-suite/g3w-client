e301e04d4e87f48c363217cf42fbd9c6c687cec5
volterra79 <boccacci.francesco@gmail.com>
2022-06-16 14:01:52 +0200

    handle header feature on result with link and image value with icon link and image

--- src/app/gui/queryresults/vue/components/headerfeaturebody.vue
+++ src/app/gui/queryresults/vue/components/headerfeaturebody.vue
@@ -5,7 +5,10 @@
       :class="[boxLayerFeature.collapsed ? '' : 'featurebox-header-open']">
     <actions :layer="layer" :featureIndex="index" :trigger="trigger" :feature="feature" :actions="actions"></actions>
     <td class="attribute" v-for="attribute in attributesSubset(layer)">
-      <span>{{feature.attributes[attribute.name]}}</span>
+      <span class="skin-color" v-if="isLink(getLayerField({layer, feature, fieldName: attribute.name}))" :class="g3wtemplate.getFontClass('link')"></span>
+      <span class="skin-color" v-else-if="isPhoto(getLayerField({layer, feature, fieldName: attribute.name}))" :class="g3wtemplate.getFontClass('image')"></span>
+      <span class="skin-color" v-else-if="isImage(getLayerField({layer, feature, fieldName: attribute.name}))" :class="g3wtemplate.getFontClass('image')"></span>
+      <span v-else >{{feature.attributes[attribute.name]}}</span>
     </td>
     <td class="collapsed" v-if="!hasLayerOneFeature(layer)" :class="{noAttributes: attributesSubset(layer).length === 0}">
       <span class="fa link morelink skin-color" :class="[boxLayerFeature.collapsed ? g3wtemplate.font['plus'] : g3wtemplate.font['minus']]"></span>
@@ -15,8 +18,10 @@
 
 <script>
   import Actions from './actions.vue'
+  const {fieldsMixin} = require('gui/vue/vue.mixins');
   export default {
     name: "headerfeaturebody",
+    mixins : [fieldsMixin],
     props: {
       toggleFeatureBoxAndZoom: {
         type: Function
@@ -29,6 +34,9 @@
       },
       boxLayerFeature: {
         type: Object
+      },
+      getLayerField:{
+        type: Function,
       },
       attributesSubset:{
         type: Function
