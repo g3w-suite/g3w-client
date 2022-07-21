f518aa9e16679c7a8a402247830f89e00c529e80
Raruto <Raruto@users.noreply.github.com>
2022-07-21 16:38:51 +0200

    Deprecate "core/parsers" folder
    - deprecate notice for "core/parsers" folder (will be removed after v3.4)
    - move entire parsing logic within "core/utils/parsers"

--- src/app/core/parsers/table/parser.js
+++ src/app/core/parsers/table/parser.js
@@ -1,29 +1,6 @@
-const Feature = require('core/layers/features/feature');
-const TableParser = function() {
-  this.get = function(options={}) {
-    const type = options.type;
-    let parser;
-    switch (type) {
-      case 'json':
-        parser = this._parserJSON.bind(this);
-        break;
-      default:
-        parser = this._parserJSON.bind(this);
-    }
-    return parser;
-  };
+const { table } = require('core/utils/parsers');
 
-  this._parserJSON = function(data={}) {
-    const {features=[]} = data;
-    return features.map(_feature => {
-      const {id, properties} = _feature;
-      const feature = new Feature();
-      feature.setProperties(properties);
-      feature.setId(id);
-      return feature;
-    });
-  }
-};
-
-module.exports = new TableParser();
-
+/**
+ * DEPRECATED: this folder will be removed after v3.4 (use "core/utils/parsers" instead)
+ */
+module.exports = table;
