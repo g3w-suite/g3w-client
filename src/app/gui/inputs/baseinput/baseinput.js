c510c089ce7f41a0faa68563efcc1f04e2ae7ce5
Raruto <Raruto@users.noreply.github.com>
2022-07-26 10:27:09 +0200

    deprecate notice for `BaseInputMixin` (use `gui/vue/vue.mixins.js` instead)

--- src/app/gui/inputs/baseinput/baseinput.js
+++ src/app/gui/inputs/baseinput/baseinput.js
@@ -1,35 +1,4 @@
-const BaseInputMixin = {
-  computed: {
-    notvalid() {
-      return this.state.validate.valid === false;
-    },
-    editable() {
-      return this.state.editable;
-    },
-    showhelpicon(){
-      return this.state.help && this.state.help.message.trim();
-    }
-  },
-  methods: {
-    showHideHelp(){
-      this.state.help.visible = !this.state.help.visible
-    },
-    // used to text input to listen mobile changes
-    mobileChange(event){
-      this.state.value = event.target.value;
-      this.change();
-    },
-    // called when input value change
-    change() {
-      this.service.setEmpty();
-      // validate input
-      this.state.validate.required && this.service.validate();
-      // emit change input
-      this.$emit('changeinput', this.state);
-    },
-    isVisible() {}
-  }
-};
+const {baseInputMixin:BaseInputMixin} = require('gui/vue/vue.mixins');
 
 const BaseInput = {
   props: ['state'],
@@ -39,5 +8,9 @@ const BaseInput = {
 
 module.exports = {
   BaseInput,
+
+  /**
+   * DEPRECATED: will be removed after v3.4 (use "gui/vue/vue.mixins" instead)
+   */
   BaseInputMixin
 };
--- src/app/gui/vue/vue.mixins.js
+++ src/app/gui/vue/vue.mixins.js
@@ -360,7 +360,40 @@ const metadataMixin = {
       return this.state[name] !== undefined;
     }
   }
-}
+};
+
+const baseInputMixin = {
+  computed: {
+    notvalid() {
+      return this.state.validate.valid === false;
+    },
+    editable() {
+      return this.state.editable;
+    },
+    showhelpicon(){
+      return this.state.help && this.state.help.message.trim();
+    }
+  },
+  methods: {
+    showHideHelp(){
+      this.state.help.visible = !this.state.help.visible
+    },
+    // used to text input to listen mobile changes
+    mobileChange(event){
+      this.state.value = event.target.value;
+      this.change();
+    },
+    // called when input value change
+    change() {
+      this.service.setEmpty();
+      // validate input
+      this.state.validate.required && this.service.validate();
+      // emit change input
+      this.$emit('changeinput', this.state);
+    },
+    isVisible() {}
+  }
+};
 
 
 module.exports = {
@@ -374,4 +407,5 @@ module.exports = {
   formInputsMixins,
   widgetMixins,
   metadataMixin,
+  baseInputMixin,
 };
