cfdecefffc7b3417c6aca557c5e7fd8d8290da30
volterra79 <boccacci.francesco@gmail.com>
2022-08-09 17:53:07 +0200

    Refactor sidebar

---
+++ src/components/SidebarItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <li :id="component.id" v-show="state.visible" class="treeview sidebaritem" :class="{'active': open}" v-disabled="state.disabled">
+    <bar-loader :loading="state.loading"></bar-loader>
+    <a @click.prevent="onClickItem" ref="anchor_click" href="#" style="display: flex; justify-content: space-between; align-items: center">
+      <div>
+        <i :class="icon" :style="{color: iconColor}"></i>
+        <span class="treeview-label" v-t="title"></span>
+      </div>
+      <div>
+        <span v-if="info.state" style="position: absolute; right: 5px; font-weight: bold" :class="info.class" :style="info.style" :title="info.tooltip">{{ info.state }}</span>
+        <span v-for="action in actions" :key="action.id" data-placement="left" data-toggle="tooltip"
+          @click.stop="action.fnc"
+          style="font-weight: bold; padding:3px;"
+          :class="action.class"
+          class="action skin-tooltip-left"
+          :style="action.style"
+          :title="action.tooltip">
+      </span>
+      </div>
+      <i v-if="collapsible" :class="g3wtemplate.getFontClass('angle-left')" class="pull-right"></i>
+    </a>
+    <div id="g3w-sidebarcomponent-placeholder"></div>
+  </li>
+</template>
+
+<script>
+  /*
+  * Temporaney
+   */
+  import SIDEBAREVENTBUS from 'gui/sidebar/eventbus';
+
+  export default {
+    name: "SidebarItem",
+    data() {
+      return {
+        info: this.$options.info || {
+          state: null,
+          style: null,
+          class: null
+        },
+        main: true,
+        component: this.$options.component,
+        active: false,
+        title: '',
+        open: false,
+        icon: null,
+        iconColor: null,
+        collapsible: null
+      };
+    },
+    methods: {
+      onClickItem(evt) {
+        // force to close
+        this.component.isolate && evt.stopPropagation();
+        if (!this.component.isolate) {
+          // set state of opened component
+          this.$options.service.state.components.forEach(component => {
+            if (component !== this.component) {
+              if (component.getOpen()) {
+                component.click({
+                  open:component.isolate
+                });
+              }
+            }
+          });
+          !this.component.collapsible && isMobile.any && SIDEBAREVENTBUS.$emit('sidebaritemclick');
+        }
+        this.component.setOpen(!this.component.state.open);
+      }
+    },
+    created() {
+      this.component.openClose = () => this.$refs.anchor_click.click();
+    },
+    async mounted() {
+      await this.$nextTick();
+      $('.sidebaritem .action[data-toggle="tooltip"]').tooltip();
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
