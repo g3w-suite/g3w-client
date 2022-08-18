c14d11cd8f145f3717bf65c7bc8fe732606cbe5c
volterra79 <boccacci.francesco@gmail.com>
2022-08-18 12:31:29 +0200

    Add InputHtml.vue Single File Component

---
+++ src/components/InputHTML.vue
@@ -0,0 +1,22 @@
+<template>
+  <baseinput :state="state">
+    <input
+      :placeholder="state.default"
+      slot="body"
+      @keyup="isMobile() ? mobileChange($event) : change()"
+      v-disabled="!editable"
+      :field="state.name"
+      class="form-control"
+      v-model="state.value"
+      :class="{'input-error-validation' : notvalid}"
+      :id="state.name">
+  </baseinput>
+</template>
+
+<script>
+const Input = require('gui/inputs/input');
+
+export default {
+  mixins: [Input]
+};
+</script>
