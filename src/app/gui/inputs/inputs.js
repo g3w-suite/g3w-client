6b0a4738528a18d4d0ad5c5770b4f9d934c578e9
volterra79 <boccacci.francesco@gmail.com>
2022-09-06 15:10:42 +0200

    Fix ui of input text area widget

--- src/app/gui/inputs/inputs.js
+++ src/app/gui/inputs/inputs.js
@@ -1,5 +1,6 @@
 const InputsComponents = {
-  'text_input': require('./text/vue/text'),
+  //'text_input': require('./text/vue/text'),
+  'text_input': require('./textarea/vue/textarea'), //TEMPORARY FOR TEST PUTPOSE
   'textarea_input': require('./textarea/vue/textarea'),
   'integer_input': require('./integer/vue/integer'),
   'string_input':require('./text/vue/text'), //temporary
