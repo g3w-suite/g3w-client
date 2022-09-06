c1b8412036f5d7571b30cc89f1e548883ff36864
volterra79 <boccacci.francesco@gmail.com>
2022-09-06 11:43:18 +0200

    Add show hide html raw text

--- src/components/InputHtml.vue
+++ src/components/InputHtml.vue
@@ -1,9 +1,6 @@
 <template>
-  <baseinput :state="state">
-    <div
-      slot="body"
-      @keydown.stop=""
-      v-disabled="!editable"
+  <baseinput :state="state" v-disabled="!editable">
+    <div @keydown.stop="" slot="body"
       class="form-control"
       :style="{border: novalid ? '1px solid reed' : '1px solid #ccc'}"
       :id="state.name"></div>
@@ -20,99 +17,95 @@ export default {
       /**
        * Column left
        */
-      document.querySelectorAll('.ql-column-left').forEach(button => {
-        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z"/></svg>'
-        button.title = "Add column left";
-        button.addEventListener('click', () => {
-          this.table.insertColumnLeft();
-        });
-      });
+      const buttonColumnLeft = this.$el.querySelector('.ql-column-left');
+      buttonColumnLeft.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z"/></svg>'
+      buttonColumnLeft.title = "Add column left";
 
       /**
        * Column Right
        */
-      document.querySelectorAll('.ql-column-right').forEach(button => {
-        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M5.795 12.456A.5.5 0 0 1 5.5 12V4a.5.5 0 0 1 .832-.374l4.5 4a.5.5 0 0 1 0 .748l-4.5 4a.5.5 0 0 1-.537.082z"/></svg>';
-        button.title = "Add column right";
-        button.addEventListener('click', () => {
-          this.table.insertColumnRight();
-        });
-      });
+      const buttonColumnRight = this.$el.querySelector('.ql-column-right');
+      buttonColumnRight.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M5.795 12.456A.5.5 0 0 1 5.5 12V4a.5.5 0 0 1 .832-.374l4.5 4a.5.5 0 0 1 0 .748l-4.5 4a.5.5 0 0 1-.537.082z"/></svg>';
+      buttonColumnRight.title = "Add column right";
 
       /**
        * Column Remove
        * */
-      document.querySelectorAll('.ql-column-remove').forEach(button => {
-        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
-        button.title = "Remove column";
-        button.addEventListener('click', () => {
-          this.table.deleteColumn();
-        });
-      });
+      const buttonColumnRemove = this.$el.querySelector('.ql-column-remove');
+      buttonColumnRemove.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
+      buttonColumnRemove.title = "Remove column";
 
       /**
        * Row above
        */
 
-      document.querySelectorAll('.ql-row-above').forEach(button => {
-        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M3.544 10.705A.5.5 0 0 0 4 11h8a.5.5 0 0 0 .374-.832l-4-4.5a.5.5 0 0 0-.748 0l-4 4.5a.5.5 0 0 0-.082.537z"/></svg>';
-        button.title = "Add row above";
-        button.addEventListener('click', () => {
-          this.table.insertRowAbove();
-        });
-      });
-
+      const buttonRowAbove = this.$el.querySelector('.ql-row-above');
+      buttonRowAbove.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M3.544 10.705A.5.5 0 0 0 4 11h8a.5.5 0 0 0 .374-.832l-4-4.5a.5.5 0 0 0-.748 0l-4 4.5a.5.5 0 0 0-.082.537z"/></svg>';
+      buttonRowAbove.title = "Add row above";
       /**
        * Row below
        */
 
-      document.querySelectorAll('.ql-row-below').forEach(button => {
-        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 16 16"><path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/></svg>'
-        button.title = "Add row below";
-        button.addEventListener('click', () => {
-          this.table.insertRowBelow();
-        });
-      });
+      const buttonRowBelow = this.$el.querySelector('.ql-row-below');
+      buttonRowBelow.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 16 16"><path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/></svg>'
+      buttonRowBelow.title = "Add row below";
 
       /**
        * Row remove
        */
-      document.querySelectorAll('.ql-row-remove').forEach(button => {
-        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>';
-        button.title = "Remove row";
-        button.addEventListener('click', () => {
-          this.table.deleteRow();
-        });
-      });
-
+      const buttonRowRemove= this.$el.querySelector('.ql-row-remove');
+      buttonRowRemove.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>';
+      buttonRowRemove.title = "Remove row";
     }
   },
   async mounted(){
+    const toolbarOptions = [
+      ['bold', 'italic', 'underline', { 'list': 'ordered' }, { 'list': 'bullet' }, 'link', 'clean', 'html'],
+      ['table', 'column-left', 'column-right', 'column-remove', 'row-above', 'row-below', 'row-remove'],
+    ];
     await this.$nextTick();
+    let html = false;
+
     this.quill = new Quill(`#${this.state.name}`, {
       modules: {
         table: true,
-        toolbar:[
-          [{ header: [1, 2, 3, 4, 5, 6, false] }],
-          ['bold', 'italic', 'underline'],
-          [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-          ['link', 'clean'],
-          ['table', 'column-left', 'column-right', 'column-remove'],
-          ['row-above', 'row-below', 'row-remove'],
-        ]
+        toolbar: {
+          container: toolbarOptions,
+          handlers: {
+            html: () => {
+              html = !html;
+              if (html) this.quill.container.firstChild.innerText = this.quill.container.firstChild.innerHTML;
+              else this.quill.container.firstChild.innerHTML = this.quill.container.firstChild.innerText;
+              for (const button of this.$el.querySelectorAll('button')) {
+                if (!button.classList.contains('ql-html')) button.classList.toggle('g3w-disabled');
+                else button.classList.toggle('skin-color')
+              }
+            },
+            'column-left': () => this.table.insertColumnLeft(),
+            'column-right': () => this.table.insertColumnRight(),
+            'column-remove': () => this.table.deleteColumn(),
+            'row-above': () => this.table.insertRowAbove(),
+            'row-below': () => this.table.insertRowBelow(),
+            'row-remove': () => this.table.deleteRow()
+          },
+
+        }
       },
       theme: 'snow'
     });
-    this.quill.container.firstChild.innerHTML = this.state.value;
+
+    this.table = this.quill.getModule('table');
+    this.setupTableCustomTools();
+
     this.handler = () => {
-      this.state.value = this.quill.container.firstChild.innerHTML;
+      this.state.value = html ? this.quill.container.firstChild.innerText : this.quill.container.firstChild.innerHTML;
       this.change()
     };
-    this.quill.on('text-change', this.handler);
-    this.table = this.quill.getModule('table');
 
-    this.setupTableCustomTools();
+    this.quill.on('text-change', this.handler);
+    this.quill.container.firstChild.innerHTML = this.state.value;
   },
+
   beforeDestroy(){
     this.quill.off('text-change', this.handler);
     this.handler = null;
@@ -120,3 +113,11 @@ export default {
   }
 };
 </script>
+<style>
+  button.ql-html {
+    width: 40px !important;
+  }
+  button.ql-html:after {
+    content: "html";
+  }
+</style>
