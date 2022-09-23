13e09f1c2b7ad0383ec84ddbe4525cc236035563
volterra79 <boccacci.francesco@gmail.com>
2022-09-23 17:09:18 +0200

    Get formats key options from server configuration

--- src/components/SearchDatetime.vue
+++ src/components/SearchDatetime.vue
@@ -1,6 +1,6 @@
 <template>
   <div :id="id" class="input-group date">
-    <input :id="forminput.id" type='text' class="form-control" />
+    <input :id="forminput.id" type='text' class="form-control"/>
     <span class="input-group-addon skin-color">
       <span :class="g3wtemplate.getFontClass('calendar')"></span>
     </span>
@@ -10,7 +10,7 @@
 
 <script>
   import ApplicationState from 'core/applicationstate';
-  const {getUniqueDomId} = require('core/utils/utils');
+  const {getUniqueDomId, convertQGISDateTimeFormatToMoment} = require('core/utils/utils');
 
   export default {
     name: 'Searchdatetime',
@@ -22,12 +22,23 @@
     },
     created(){
       this.id = `search_datetime_${getUniqueDomId()}`;
+      this.forminput.options.formats = [{
+        "date": true,
+        "time": false,
+        "fieldformat": "yyyy-MM-dd",
+        "displayformat": "yyyy",
+        "default": null
+      }]
     },
     async mounted() {
       await this.$nextTick();
+      const {options: {formats}} = this.forminput;
+      let {fieldformat, displayformat} = formats[0];
+      fieldformat = convertQGISDateTimeFormatToMoment(fieldformat);
+      displayformat = convertQGISDateTimeFormatToMoment(displayformat);
       $(`#${this.id}`).datetimepicker({
         defaultDate: null,
-        format: 'YYYY-MM-DD',
+        format: displayformat,
         ignoreReadonly: true,
         allowInputToggle: true,
         toolbarPlacement: 'top',
@@ -41,8 +52,8 @@
 
       $(`#${this.id}`).on("dp.change", () => {
         const newDate = $(`#${this.forminput.id}`).val();
-        this.forminput.value = _.isEmpty(_.trim(newDate)) ? null : moment(newDate, 'YYYY/MM/DD').format('YYYY/MM/DD');
-        this.emit('change', this.forminput);
+        this.forminput.value = _.isEmpty(_.trim(newDate)) ? null : moment(newDate, displayformat).format(fieldformat);
+        this.$emit('change', this.forminput);
       });
 
       ApplicationState.ismobile && setTimeout(()=>{
