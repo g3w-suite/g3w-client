0e096204a2bd045eb677687fffd1f13e1487a56b
volterra79 <boccacci.francesco@gmail.com>
2022-09-28 16:11:32 +0200

    Refactor Map.vue component splitting in MapFooter component in MapFooterRight and MapFooterLeft

---
+++ src/components/MapFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="map_footer" class="skin-border-color">
+    <map-footer-left/>
+    <map-footer-right :service="service"/>
+  </div>
+</template>
+
+<script>
+  import MapFooterRight from "./MapFooterRight.vue";
+  import MapFooterLeft from "./MapFooterLeft.vue";
+  export default {
+    name: 'Mapfooter',
+    props: {
+      service: {
+        type: Object
+      }
+    },
+    components: {
+      MapFooterRight,
+      MapFooterLeft
+    }
+  };
+</script>
---
+++ src/components/MapFooterLeft.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="map_footer_left" style="display: flex;" >
+    <a href="https://g3wsuite.it/" style="margin-left: 5px; align-self: center;" target="_blank">
+      <img height="15" src="/static/client/images/g3wsuite_logo.png" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Mapfooter'
+  };
+</script>
