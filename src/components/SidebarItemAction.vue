eab6d7f66d1000b3c9925292aa3c586d8bfbf4ec
Raruto <Raruto@users.noreply.github.com>
2022-09-30 11:16:14 +0200

    add jsdoc tag `@since v3.7.0`

--- src/components/SidebarItemAction.vue
+++ src/components/SidebarItemAction.vue
@@ -1,6 +1,16 @@
+<!-- 
+  @since v3.7.0
+-->
+
 <template>
-    <span  @click.stop="action.fnc" v-t-tooltip:left.create="action.tooltip" style="font-weight: bold; padding:3px;" :class="action.class" class="action skin-tooltip-left" :style="action.style">
-    </span>
+  <span
+    @click.stop="action.fnc"
+    v-t-tooltip:left.create="action.tooltip"
+    style="font-weight: bold; padding:3px;"
+    :class="action.class"
+    class="action skin-tooltip-left"
+    :style="action.style"
+  ></span>
 </template>
 
 <script>
