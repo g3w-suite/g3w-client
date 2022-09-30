989e1f142bd544616a3fd639dc8080cecc2c95a5
volterra79 <boccacci.francesco@gmail.com>
2022-09-30 10:19:39 +0200

    Change project-menu-too-long-description class with css without specific vendor rules

--- src/components/ProjectsMenu.vue
+++ src/components/ProjectsMenu.vue
@@ -86,10 +86,44 @@ export default {
 };
 </script>
 <style scoped>
+  /* styles for '...' */
   .project-menu-too-long-description {
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
+    /* hide text if it more than N lines  */
     overflow: hidden;
+    /* for set '...' in absolute position */
+    position: relative;
+    /* use this value to count block height */
+    line-height: 1.2em;
+    /* max-height = line-height (1.2) * lines max number (3) */
+    max-height: 3.6em;
+    /* fix problem when last visible word doesn't adjoin right side  */
+    text-align: justify;
+    /* place for '...' */
+    padding-right: 1em;
+  }
+  /* create the ... */
+  .project-menu-too-long-description:before {
+    /* points in the end */
+    content: '...';
+    /* absolute position */
+    position: absolute;
+    /* set position to right bottom corner of block */
+    right: 0;
+    bottom: 0;
+  }
+  /* hide ... if we have text, which is less than or equal to max lines */
+  .project-menu-too-long-description:after {
+    /* points in the end */
+    content: '';
+    /* absolute position */
+    position: absolute;
+    /* set position to right bottom corner of text */
+    right: 0;
+    /* set width and height */
+    width: 1em;
+    height: 1em;
+    margin-top: 0.2em;
+    /* bg color = bg color under block */
+    background: white;
   }
 </style>
