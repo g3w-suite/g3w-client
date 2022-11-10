8ba7584a0a9a215b453af967496ba1a2d072e275
Francesco Boccacci <boccacci.francesco@gmail.com>
2022-11-10 11:13:51 +0100

    Add name to wms sidebar item (#256)

--- src/components/WMS.vue
+++ src/components/WMS.vue
@@ -6,11 +6,20 @@
     <li v-if="loading">
       <bar-loader :loading="loading"></bar-loader>
     </li>
-    <li style="display: flex; justify-content: space-between; background: transparent !important; margin-bottom: 10px;">
-      <input v-model="newwmsurl" class="form-control" style="flex-grow: 1; color:#000000;">
-      <button v-disabled="!newwmsurlvalid" style="width: 50px; border-radius: 0" @click.stop="addNewWmsUrl" class="btn btn-block skin-background-color">
-        <i :class="g3wtemplate.getFontClass('plus-square')"></i>
-      </button>
+    <li style="display: flex; flex-direction: column; justify-content: space-between; background: transparent !important; margin-bottom: 10px;">
+      <div class="wms_url_input_content">
+        <label for="add_custom_name_url_wms_input" v-t="'sidebar.wms.panel.label.name'"></label>
+        <input id="add_custom_name_url_wms_input" v-model="id" class="form-control" style="width: 100%; color:#000000;">
+      </div>
+      <div class="wms_url_input_content">
+        <label for="add_custom_url_wms_input">URL</label>
+        <input id="add_custom_url_wms_input" v-model="url" class="form-control" style="width: 100%; color:#000000;">
+      </div>
+      <div>
+        <button v-disabled="!inputswmsurlvalid" style="width: 100%;" @click.prevent.stop="addwmsurl" class="btn btn-block skin-background-color">
+          <i :class="g3wtemplate.getFontClass('plus-square')"></i>
+        </button>
+      </div>
     </li>
     <li v-if="status.error">
       <div class="g3w-add-wms-url-message g3w-wmsurl-error" v-t="'server_error'"></div>
@@ -18,22 +27,25 @@
     <li v-else-if="status.added">
       <div class="g3w-add-wms-url-message g3w-wmsurl-already-added" v-t="'sidebar.wms.url_already_added'"></div>
     </li>
-    <li style="background: transparent !important;">
+    <li class="skin-border-color" style="background: transparent !important; border-top: 2px solid">
       <div style="max-height: 200px; overflow-y: auto;">
-        <div style="display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid #cccccc; padding-bottom: 3px;" v-for="wmsurl in state.adminwmsurls" :key="wmsurl">
-          <span style="flex-grow: 1; margin-right: 15px;" class="new_line_too_long_text" :title="wmsurl">{{ wmsurl }}</span>
-          <span class="skin-color" style="padding: 5px;" @click.stop="showWmsLayersPanel(wmsurl)">
+        <div v-for="({id, url}) in state.adminwmsurls" :key="wmsurl" style="display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid #cccccc; padding-bottom: 3px;">
+          <span style="flex-grow: 1; margin-right: 15px;" class="new_line_too_long_text">{{ id }}</span>
+          <span class="skin-color" style="padding: 5px;" @click.stop="showWmsLayersPanel(id)">
             <i style="font-weight: bold; font-size: 1.3em;" :class="g3wtemplate.getFontClass('plus-square')"></i>
           </span>
         </div>
-        <div style="display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid #cccccc; padding-bottom: 3px;" v-for="wmsurl in state.localwmsurls" :key="wmsurl">
-          <span style="flex-grow: 1; margin-right: 15px;" class="new_line_too_long_text" :title="wmsurl">{{ wmsurl }}</span>
-          <span class="skin-color" style="padding: 5px;" @click.stop="showWmsLayersPanel(wmsurl)" v-t-tooltip:top.create="'sidebar.wms.add_wms_layer'">
-            <i style="font-weight: bold; font-size: 1.3em;" :class="g3wtemplate.getFontClass('plus-square')"></i>
-          </span>
-          <span style="color: red; padding: 5px;" @click.stop="deleteWmsUrl(wmsurl)" v-t-tooltip:top.create="'sidebar.wms.delete_wms_url'">
-            <i style="font-weight: bold; font-size: 1.3em; color:red" :class="g3wtemplate.getFontClass('trash')" ></i>
-          </span>
+        <div v-for="({id, url}) in state.localwmsurls" :key="id" style="border-bottom: 1px solid #cccccc; padding-bottom: 3px;">
+          <div style="display: flex; justify-content: space-between; align-items: center; padding-top: 3px">
+            <span style="flex-grow: 1; margin-right: 15px; font-weight: bold" class="new_line_too_long_text" >{{ id }}</span>
+            <span class="skin-color" style="padding: 3px; margin: 2px;" @click.stop="showWmsLayersPanel(url)" v-t-tooltip:top.create="'sidebar.wms.add_wms_layer'">
+              <i class="wms-icon-action" :class="g3wtemplate.getFontClass('plus-square')"></i>
+            </span>
+            <span style="color: red; padding: 3px; margin: 2px;" @click.stop="deleteWmsUrl(id)" v-t-tooltip:top.create="'sidebar.wms.delete_wms_url'">
+              <i style="color: red" class="wms-icon-action" :class="g3wtemplate.getFontClass('trash')" ></i>
+            </span>
+          </div>
+          <div style="font-size: 0.7em; " class="new_line_too_long_text" :title="url">{{url}}</div>
         </div>
       </div>
     </li>
@@ -47,7 +59,8 @@
     name: "sidebaritem",
     data(){
       return {
-        newwmsurl: null,
+        url: null,
+        id: null,
         loading: false,
         status: {
           error: false,
@@ -56,25 +69,28 @@
       }
     },
     computed: {
-      newwmsurlvalid() {
-        return this.newwmsurl !== null && this.newwmsurl.trim() && isURL(this.newwmsurl);
+      inputswmsurlvalid() {
+        return (this.url !== null && this.url.trim() && isURL(this.url)) && (this.id !== null && this.id.trim());
       }
     },
     methods: {
-      async addNewWmsUrl(){
+      async addwmsurl(){
         this.loading = true;
-        const {error, added} = await this.$options.service.addNewWmsUrl(this.newwmsurl);
+        const {error, added} = await this.$options.service.addNewUrl({
+          url: this.url,
+          id: this.id
+        });
         this.status.error = error;
         this.status.added = added;
         this.loading = false;
       },
-      deleteWmsUrl(wmsurl) {
-        this.$options.service.deleteWmsUrl(wmsurl)
+      deleteWmsUrl(id) {
+        this.$options.service.deleteWmsUrl(id)
       },
-      async showWmsLayersPanel(wmsurl){
+      async showWmsLayersPanel(url){
         try {
           this.loading = true;
-          const {error, added} = await this.$options.service.loadWMSDataAndShowWmsLayersPanel(wmsurl);
+          const {error, added} = await this.$options.service.loadWMSDataAndShowWmsLayersPanel(url);
           this.status.error = error;
           this.status.added = added;
           this.loading = false;
@@ -96,5 +112,16 @@
   }
   .g3w-wmsurl-already-added {
     background-color: orange;
+  }
+  .wms_url_input_content{
+    margin-bottom: 5px;
+  }
+  .wms-icon-action {
+    font-weight: bold;
+    font-size: 1.3em;
+    box-shadow: 0 5px 10px #1e282c;
+  }
+  .wms-icon-action:hover {
+    box-shadow: none;
   }
 </style>
