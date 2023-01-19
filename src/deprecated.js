98a4d12a839c27cd747776214615f00d6e0ed02a
Raruto <Raruto@users.noreply.github.com>
2023-01-19 14:10:46 +0100

    add a fallback for jQuery fail() and always() methods

--- src/deprecated.js
+++ src/deprecated.js
@@ -5,6 +5,18 @@
 
 import * as VueColor from 'vue-color';
 
+const deprecate = require('util-deprecate');
+
+/**
+ * @deprecated since v3.8. Will be removed in v4.x. Use native 'Promise.catch' instead
+ */
+Promise.prototype.fail = deprecate(Promise.prototype.catch, '[G3W-CLIENT] jQuery $.Defferred() is deprecated, use native Promise() instead');
+
+/**
+ * @deprecated since v3.8. Will be removed in v4.x. Use native 'Promise.finally' instead
+ */
+Promise.prototype.always = deprecate(Promise.prototype.finally, '[G3W-CLIENT] jQuery $.Defferred() is deprecated, use native Promise() instead');
+
 /**
  * @deprecated since v3.8. Will be removed in v4.x. Use ESM imports from 'vue-color' instead
  */
