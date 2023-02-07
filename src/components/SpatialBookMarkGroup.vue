4074e5a6541df46dcf8fea2dbb4441ad54579913
volterra79 <boccacci.francesco@gmail.com>
2023-02-07 11:10:07 +0100

    Add SpatialBookmarkGroup component

---
+++ src/components/SpatialBookMarkGroup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <spatial-book-mark-item></spatial-book-mark-item>
+  </div>
+</template>
+
+<script>
+  import SpatialBookMarkItem from "components/SpatialBookMarkItem.vue";
+
+  export default {
+    props: {
+      bookmark: {
+        type: Object,
+        required: true,
+        default: {
+          name: '',
+        }
+      }
+    },
+    components: {
+      SpatialBookMarkItem
+    },
+    methods: {},
+    async mounted() {}
+  };
+</script>
+
+<style scoped>
+</style>
