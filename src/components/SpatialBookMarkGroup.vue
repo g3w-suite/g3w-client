6640049d4fbf3f4d64c8a193d4825ffb165c103b
volterra79 <boccacci.francesco@gmail.com>
2023-02-22 11:05:31 +0100

    Change style of inner spatial book mark item component to have more readability between group and item

--- src/components/SpatialBookMarkGroup.vue
+++ src/components/SpatialBookMarkGroup.vue
@@ -7,10 +7,14 @@
   <li>
     <div style="font-weight: bold; width: 100%;" :style="{borderBottom: group.expanded ? '2px solid #2c3b41' : 'none'}"
       @click.stop="group.expanded = !group.expanded">
-      <span :class="[g3wtemplate.getFontClass(group.expanded ? 'caret-down' : 'caret-right')]" style="margin-right: 5px;"></span>
+      <span
+        :class="[g3wtemplate.getFontClass(group.expanded ? 'caret-down' : 'caret-right')]"
+        style="margin-right: 5px;"
+      >
+      </span>
       <span>{{group.name}}</span>
     </div>
-    <ul style="margin-left: 5px;" v-show="group.expanded">
+    <ul style="margin-left: 10px;" v-show="group.expanded">
       <spatial-book-mark-item v-for="node in group.nodes" :bookmark="node"/>
     </ul>
   </li>
