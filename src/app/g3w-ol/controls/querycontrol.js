1fd5b22471024eefba07af18715028c31ba14380
Raruto <Raruto@users.noreply.github.com>
2023-03-08 15:58:56 +0100

    refactor function `QueryControl::setMap(map)`

--- src/app/g3w-ol/controls/querycontrol.js
+++ src/app/g3w-ol/controls/querycontrol.js
@@ -2,12 +2,12 @@ import GUI from 'services/gui';
 import ProjectsRegistry from 'store/projects';
 import DataRouterService from 'services/data';
 
-const {throttle} = require('core/utils/utils');
-const utils = require('core/utils/ol');
-const InteractionControl = require('g3w-ol/controls/interactioncontrol');
+const { throttle }               = require('core/utils/utils');
+const utils                      = require('core/utils/ol');
+const InteractionControl         = require('g3w-ol/controls/interactioncontrol');
 const PickCoordinatesInteraction = require('g3w-ol/interactions/pickcoordinatesinteraction');
 
-const QueryControl = function(options={}){
+const QueryControl = function(options = {}){
   const _options = {
     offline: false,
     name: "querylayer",
@@ -24,45 +24,50 @@ ol.inherits(QueryControl, InteractionControl);
 
 const proto = QueryControl.prototype;
 
+/**
+ * @param {ol.Map} map
+ * 
+ * @listens InteractionControl~toggled
+ */
 proto.setMap = function(map) {
   let eventKey = null;
+
   if (map) {
     this.on('toggled', ({toggled}) => {
 
-      if (true === toggled) {
-        if (null === eventKey) {
-
-          eventKey = map.on('singleclick', throttle(async event => {
-            const coordinates = event.coordinate;
-            GUI.closeOpenSideBarComponent();
-            try {
-              const {data=[]} = await DataRouterService.getData('query:coordinates', {
-                inputs: {
-                  coordinates,
-                  feature_count: ProjectsRegistry.getCurrentProject().getQueryFeatureCount(),
-                  query_point_tolerance: ProjectsRegistry.getCurrentProject().getQueryPointTolerance,
-                  multilayers: ProjectsRegistry.getCurrentProject().isQueryMultiLayers(this.name),
-                }
-              });
-              data.length && GUI.getService('map').showMarker(coordinates);
-            } catch(error) {}
-
-            this.dispatchEvent({
-              type: 'picked',
-              coordinates
-            });
-
-          }));
-
-          this.setEventKey({
-            eventType: 'picked',
-            eventKey
-          });
-        }
-      } else {
+      if (true !== toggled) {
         ol.Observable.unByKey(eventKey);
         eventKey = null;
-      }
+      } else if (null === eventKey) {
+
+          eventKey = map
+            .on('singleclick', throttle(async event => {
+              const coordinates = event.coordinate;
+
+              GUI.closeOpenSideBarComponent();
+
+              try {
+                const project = ProjectsRegistry.getCurrentProject();
+                const { data = [] } = await DataRouterService.getData('query:coordinates', {
+                  inputs: {
+                    coordinates,
+                    feature_count: project.getQueryFeatureCount(),
+                    query_point_tolerance: project.getQueryPointTolerance(),
+                    multilayers: project.isQueryMultiLayers(this.name),
+                  }
+                });
+                data.length && GUI.getService('map').showMarker(coordinates);
+              } catch(err) {
+                console.warn('Error running spatial query: ', err)
+              }
+
+              this.dispatchEvent({ type: 'picked', coordinates });
+
+            }));
+
+          this.setEventKey({ eventType: 'picked', eventKey });
+        }
+
     });
 
   }
