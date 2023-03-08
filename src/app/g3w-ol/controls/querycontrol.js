031b754a33ed35a4ac6266029fee28e6b15dcbbf
Raruto <Raruto@users.noreply.github.com>
2023-03-08 16:01:02 +0100

    move `if (map)` conditional

--- src/app/g3w-ol/controls/querycontrol.js
+++ src/app/g3w-ol/controls/querycontrol.js
@@ -32,45 +32,43 @@ const proto = QueryControl.prototype;
 proto.setMap = function(map) {
   let eventKey = null;
 
-  if (map) {
-    this.on('toggled', ({toggled}) => {
+  this.on('toggled', ({toggled}) => {
 
-      if (true !== toggled) {
-        ol.Observable.unByKey(eventKey);
-        eventKey = null;
-      } else if (null === eventKey) {
+    if (true !== toggled) {
+      ol.Observable.unByKey(eventKey);
+      eventKey = null;
+    } else if (null === eventKey && map) {
+      eventKey = map
+        .on('singleclick', throttle(async event => {
+          const coordinates = event.coordinate;
 
-          eventKey = map
-            .on('singleclick', throttle(async event => {
-              const coordinates = event.coordinate;
+          GUI.closeOpenSideBarComponent();
 
-              GUI.closeOpenSideBarComponent();
-
-              try {
-                const project = ProjectsRegistry.getCurrentProject();
-                const { data = [] } = await DataRouterService.getData('query:coordinates', {
-                  inputs: {
-                    coordinates,
-                    feature_count: project.getQueryFeatureCount(),
-                    query_point_tolerance: project.getQueryPointTolerance(),
-                    multilayers: project.isQueryMultiLayers(this.name),
-                  }
-                });
-                data.length && GUI.getService('map').showMarker(coordinates);
-              } catch(err) {
-                console.warn('Error running spatial query: ', err)
+          try {
+            const project = ProjectsRegistry.getCurrentProject();
+            const { data = [] } = await DataRouterService.getData('query:coordinates', {
+              inputs: {
+                coordinates,
+                feature_count: project.getQueryFeatureCount(),
+                query_point_tolerance: project.getQueryPointTolerance(),
+                multilayers: project.isQueryMultiLayers(this.name),
               }
+            });
+            if (data.length) {
+              GUI.getService('map').showMarker(coordinates);
+            }
+          } catch(err) {
+            console.warn('Error running spatial query: ', err)
+          }
 
-              this.dispatchEvent({ type: 'picked', coordinates });
+          this.dispatchEvent({ type: 'picked', coordinates });
 
-            }));
+        }));
 
-          this.setEventKey({ eventType: 'picked', eventKey });
-        }
+      this.setEventKey({ eventType: 'picked', eventKey });
+    }
 
-    });
-
-  }
+  });
 
   InteractionControl.prototype.setMap.call(this, map);
 };
