3dc3553be92c2a093a628f23ba3215f385ca7f4e
Raruto <Raruto@users.noreply.github.com>
2023-03-13 10:30:59 +0100

    add `@TODO` and `@since` tags

--- src/store/map-controls.js
+++ src/store/map-controls.js
@@ -11,21 +11,25 @@ const { base, inherit } = require('core/utils/utils');
 const G3WObject = require('core/g3wobject');
 
 function ControlsRegistry() {
+
   this._controls = {};
+
   this._offlineids = [];
+
+  /**
+   * @since 3.8.0
+   */
   this.selectedLayer = null;
+
+  /**
+   * @since 3.8.0
+   */
   this.externalLayers = [];
 
-  ApplicationService.on('complete', ()=> {
+  ApplicationService.on('complete',
     /**
-     * since 3.8.0
-     */
-    const CatalogService = GUI.getService('catalog');
-
-    // 0. store unwatches of external layers (selected or visible)
-    const unWatches = {};
-
-    /**
+     * @TODO extract this into a class function (named callback function)
+     * 
      * Handle temporary layers added by `addlayers` map control (Polygon or Multipolygon)
      *
      * @listens CatalogService~addExternalLayer
@@ -33,65 +37,83 @@ function ControlsRegistry() {
      *
      * @since 3.8.0
      */
+      () => {
 
-    // 1. update list `this.externalLayers`
-    // 2. update list `unWatches` of layer un-watchers (based on unique name of layer)
-    // 3. call `this.onAddExternalLayer`
-    CatalogService.onafter('addExternalLayer', ({layer, type}) => {
-      if ('vector' === type) {
-        this.externalLayers.push(layer);
-        // Add event listener of selected property to set selected layer
-        unWatches[layer.name] = [
-          VM.$watch(() => layer.selected, // watch `layer.selected` property
-            selected => {
-              this.setSelectedLayer(true === selected ? layer : null);
-            })
-        ];
+      const CatalogService = GUI.getService('catalog');
 
-        this.callControlsEventHandler({
-          handler: 'onAddExternalLayer',
-          param: {
-            layer,
-            unWatches: unWatches[layer.name]
-          }
-        });
-      }
-    });
+      // 0. store unwatches of external layers (selected or visible)
+      const unWatches = {};
 
-    // 4. clean up any previously attached event listener
-    CatalogService.onafter('removeExternalLayer', ({name, type}) => {
-      if ('vector' === type) {
-        this.externalLayers = this.externalLayers.filter(layer => {
-          if (name === layer.name) {
-            this.callControlsEventHandler({
-              handler: 'handleRemoveExternalLayer',
-              param: layer
-            });
-            (layer === this.selectedLayer) && this.setSelectedLayer(null);
-          }
-          return name !== layer.name;
-        });
-        unWatches[name].forEach(unWatch => unWatch());
-        delete unWatches[name];
-      }
-    });
-  });
+      // 1. update list `this.externalLayers`
+      // 2. update list `unWatches` of layer un-watchers (based on unique name of layer)
+      // 3. call `this.onAddExternalLayer`
+      CatalogService.onafter('addExternalLayer', ({layer, type}) => {
+        if ('vector' === type) {
+          this.externalLayers.push(layer);
+          // Add event listener of selected property to set selected layer
+          unWatches[layer.name] = [
+            VM.$watch(() => layer.selected, // watch `layer.selected` property
+              selected => {
+                this.setSelectedLayer(true === selected ? layer : null);
+              })
+          ];
 
-  ApplicationService.onbefore('offline', () =>{
-    this._offlineids.forEach(controlItem => {
-      const {id} = controlItem;
-      const control = this._controls[id];
-      controlItem.enable = control.getEnable();
-      control.setEnable(false);
-    })
-  });
+          this.callControlsEventHandler({
+            handler: 'onAddExternalLayer',
+            param: {
+              layer,
+              unWatches: unWatches[layer.name]
+            }
+          });
+        }
+      });
 
-  ApplicationService.onbefore('online', ()=>{
-    this._offlineids.forEach(controlItem => {
-      const {id, enable} = controlItem;
-      this._controls[id].setEnable(enable);
-    })
-  });
+      // 4. clean up any previously attached event listener
+      CatalogService.onafter('removeExternalLayer', ({name, type}) => {
+        if ('vector' === type) {
+          this.externalLayers = this.externalLayers.filter(layer => {
+            if (name === layer.name) {
+              this.callControlsEventHandler({ handler: 'handleRemoveExternalLayer', param: layer });
+            }
+            if (name === layer.name && layer === this.selectedLayer) {
+              this.setSelectedLayer(null);
+            }
+            return name !== layer.name;
+          });
+          unWatches[name].forEach(unWatch => unWatch());
+          delete unWatches[name];
+        }
+      });
+    }
+  );
+
+  ApplicationService.onbefore('offline',
+    /**
+     * @TODO extract this into a class function (named callback function)
+     * 
+     * @since 3.8.0
+     */
+    () => {
+      this._offlineids.forEach(controlItem => {
+        const control = this._controls[controlItem.id];
+        controlItem.enable = control.getEnable();
+        control.setEnable(false);
+      })
+    }
+  );
+
+  ApplicationService.onbefore('online',
+    /**
+     * @TODO extract this into a class function (named callback function)
+     * 
+     * @since 3.8.0
+     */
+    () => {
+      this._offlineids.forEach(controlItem => {
+        this._controls[controlItem.id].setEnable(controlItem.enable);
+      })
+    }
+  );
 
   this.setters = {
     registerControl(id, control) {
@@ -100,8 +122,9 @@ function ControlsRegistry() {
   };
 
   /**
-   * @since 3.8.0
    * @param { unknown | null } layer
+   * 
+   * @since 3.8.0
    */
   this.setSelectedLayer= function(layer) {
     this.selectedLayer = layer;
@@ -115,8 +138,9 @@ function ControlsRegistry() {
   };
 
   /**
-   * @since 3.8.0
    * @returns {Array}
+   * 
+   * @since 3.8.0
    */
   this.getExternalLayers = function(){
     return this.externalLayers;
@@ -135,9 +159,11 @@ function ControlsRegistry() {
   };
 
   /**
+   * NB `handler` = method name
+   * 
+   * @param {{ handler: string, param: any }}
+   * 
    * @since 3.8.0
-   * @param handler <String> method name
-   * @param param <Any>
    */
   this.callControlsEventHandler = function({handler, param}){
     Object.values(this._controls).forEach((control) => {
@@ -168,17 +194,17 @@ function ControlsRegistry() {
 
   this.unregisterControl = function(id) {
     const control = this.getControl(id);
-    const mapService = GUI.getService('map');
-    const map = mapService.getMap();
-    if (control) {
-      map.removeControl(control);
-      delete this._controls[id];
-      this._offlineids = this._offlineids.filter(_id => _id !== id);
-      return true
+    if (!control) {
+      return false;
     }
-    return false
+    GUI.getService('map').getMap().removeControl(control);
+    delete this._controls[id];
+    this._offlineids = this._offlineids.filter(_id => _id !== id);
+    return true;
   };
+
   base(this);
+
 }
 
 inherit(ControlsRegistry, G3WObject);
