56470ec7538b0505a65397cd1ef4355da33a88fe
Francesco Boccacci <boccacci.francesco@gmail.com>
2023-04-21 12:19:08 +0200

    Allow to print external WMS layers with same origin URL of application (ref: `"geoscreenshot"` and `"screenshot"` map controls) (#390)
    * Show geoscrenshot or screenshot map control if wms layer is external and has same origin
    
    * Refactor checkVisible
    
    * checkVisible using a utility method sameOrigin
    
    * one liner condition
    
    * remove console
    
    ---------
    
    Co-authored-by: Raruto <Raruto@users.noreply.github.com>

--- src/app/core/utils/utils.js
+++ src/app/core/utils/utils.js
@@ -632,7 +632,24 @@ const utils = {
     const matchDayInDate = datetimeformat.match(/d/g);
     if (matchDayInDate && matchDayInDate.length < 3) datetimeformat = datetimeformat.replace(/d/g, 'D');
     return datetimeformat
-  }
+  },
+
+  /**
+   * @param {string} url1
+   * @param {string} url2
+   * 
+   * @returns {boolean} whether URLs have same origin.
+   * 
+   * @since 3.8.0
+   */
+  sameOrigin(url1, url2) {
+    try {
+      return new URL(url1).origin === new URL(url2).origin;
+    } catch(err) {
+      return false
+    }
+  },
+
 };
 
 module.exports = utils;
