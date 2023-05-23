7ad12d867459000fd83acac892f784e97a37876e
Francesco Boccacci <boccacci.francesco@gmail.com>
2023-05-23 09:03:21 +0200

    Stop click event prograpagation on `removeExternalLayer` and `expandCollapse` handlers (ref: tri-state legend items) (#415)
    * :bug: Fix issue on remove external layer
    
    * add console warn
    
    * mispelling
    
    * :art: Add space for good readability
    
    * reduce delta
    
    * indentation
    
    * Update Catalog.vue
    
    * vertical indentation
    
    ---------
    
    Co-authored-by: Raruto <Raruto@users.noreply.github.com>

--- src/components/CatalogLayersGroup.vue
+++ src/components/CatalogLayersGroup.vue
@@ -6,8 +6,11 @@
 <template>
   <div style="border: 1px solid #ffffff33; margin: 5px">
     <h4 style="margin: 5px; font-weight: bold">{{ layersgroup.title }}</h4>
-    <tristate-tree  :layerstree="node" v-for="node in layersgroup.nodes" class="item">
-    </tristate-tree>
+    <tristate-tree
+      v-for="node in layersgroup.nodes"  
+      :layerstree="node"
+      class="item"
+    />
   </div>
 </template>
 
