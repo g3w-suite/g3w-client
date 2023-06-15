c3fbdcddaa6457278e131171c66073c1c44c951e
volterra79 <boccacci.francesco@gmail.com>
2023-06-15 16:21:36 +0200

    :bug: check visibility of screenshot map controls only if is visible. Once is not visible at moment no other check visibility is done. Need a way to redraw canvas element of map

--- src/app/g3w-ol/controls/screenshotcontrol.js
+++ src/app/g3w-ol/controls/screenshotcontrol.js
@@ -18,8 +18,11 @@ function ScreenshotControl(options = {}) {
 
   OnClickControl.call(this, options);
 
-  GUI.getService('map').onafter('loadExternalLayer', this._addLayer.bind(this));
-  GUI.getService('map').onafter('unloadExternalLayer', this._removeLayer.bind(this));
+  this.isVisible() && GUI.getService('map').onafter('loadExternalLayer', this._addLayer.bind(this));
+  /**
+   * Comment because at 3.8.3 need to be find a way to redraw a map canvas
+   */
+  //GUI.getService('map').onafter('unloadExternalLayer', this._removeLayer.bind(this));
 }
 
 ol.inherits(ScreenshotControl, OnClickControl);
@@ -30,9 +33,15 @@ const proto = ScreenshotControl.prototype;
  * @since 3.8.3 
  */
 proto._addLayer = function(layer) {
-  this.layers.push(layer);
-  this.change(this.layers);
-  layer.on('change:visible', () => this.change(this.layers));
+  // only in case visible otherwise we need to find a solution to redraw map canvas
+  if (this.isVisible()) {
+    this.layers.push(layer);
+    this.change(this.layers);
+    /**
+     * Comment because at 3.8.3 need to be find a way to redraw a map canvas
+     */
+    //layer.on('change:visible', () => this.change(this.layers));
+  }
 };
 
 /**
