da8784b472d92caaf49fec18d18bc5964df540a7
volterra79 <boccacci.francesco@gmail.com>
2023-07-13 17:06:05 +0200

    :bug: Fix background tooltip color of enlarge/reduce icon

--- src/components/GlobalResizeIcon.vue
+++ src/components/GlobalResizeIcon.vue
@@ -5,8 +5,8 @@
 
 <template>
   <div style="display: flex; justify-content: space-between">
-      <i :class="g3wtemplate.getFontClass(`resize-${type}`)" v-t-tooltip:bottom.create="'enlange_reduce'" style="cursor: pointer; margin-right: 3px;" class="action-button skin-color-dark" @click="toggleFull"></i>
-      <i :class="g3wtemplate.getFontClass(`resize-default`)" v-t-tooltip:left.create="'reset_default'" style="cursor: pointer" class="action-button skin-color-dark" @click="resetToDefault"></i>
+    <i v-if="show" :class="g3wtemplate.getFontClass(`resize-${this.type}`)" v-t-tooltip:bottom.create="'enlange_reduce'" style="cursor: pointer; margin-right: 3px;" class="action-button skin-color-dark" @click="toggleFull"></i>
+    <i :class="g3wtemplate.getFontClass(`resize-default`)" v-t-tooltip:left.create="'reset_default'" style="cursor: pointer" class="action-button skin-color-dark" @click="resetToDefault"></i>
   </div>
 </template>
 
@@ -14,23 +14,35 @@
   import GUI from 'services/gui';
 
   export default {
-      name: 'resize-icon',
-      props: {
-        type: {
-            type: String,
-            default: 'h'
-        }
-      },
-      computed:{},
-      methods:{
-          toggleFull(){
-              GUI.toggleFullViewContent();
-              GUI.emit('resize');
-          },
-          resetToDefault(){
-              GUI.resetToDefaultContentPercentage();
-              GUI.emit('resize');
-          }
+    name: 'resize-icon',
+    props: {
+      type: {
+        type: String,
+        default: 'h'
       }
+    },
+    data(){
+      return {
+        show: "undefined" !== typeof this.type
+      }
+    },
+    watch: {
+      async type() {
+        this.show = false;
+        await this.$nextTick();
+        this.show = true;
+      }
+    },
+
+    methods:{
+      toggleFull(){
+        GUI.toggleFullViewContent();
+        GUI.emit('resize');
+      },
+      resetToDefault(){
+        GUI.resetToDefaultContentPercentage();
+        GUI.emit('resize');
+      }
+    }
   };
 </script>
