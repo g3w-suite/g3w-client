8b44c246dce5c98d452d0bc92394d15007b21dfd
volterra79 <boccacci.francesco@gmail.com>
2023-08-21 11:51:09 +0200

    :bug: Fix providersfactory location path

--- src/app/core/layers/layer.js
+++ src/app/core/layers/layer.js
@@ -9,7 +9,7 @@ const { inherit, base, XHR } = require('core/utils/utils');
 const G3WObject = require('core/g3wobject');
 const { geometryFields, parseAttributes } =  require('core/utils/geo');
 const Relations = require('core/relations/relations');
-const ProviderFactory = require('core/layers/providers/providersfactory');
+const ProviderFactory = require('core/layers/providersfactory');
 
 // Base Class of all Layer
 function Layer(config={}, options={}) {
