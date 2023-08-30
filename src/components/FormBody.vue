b9a4d4b6b45a4866ae1b265aff4edc37a371d3d4
volterra79 <boccacci.francesco@gmail.com>
2023-08-30 16:06:01 +0200

    Fix CamelCase name of components

--- src/components/FormBody.vue
+++ src/components/FormBody.vue
@@ -39,7 +39,7 @@
 </template>
 
 <script>
-import G3WFormInputs from 'components/G3WFormInputs.vue';
+import G3wFormInputs from 'components/G3WFormInputs.vue';
 
 /**
  * @TODO remove "Vue.extend" from module export
@@ -61,7 +61,7 @@ export default Vue.extend({
   },
 
   components: {
-    G3WFormInputs
+    G3wFormInputs
   },
 
   methods: {
