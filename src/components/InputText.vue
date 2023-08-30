ce9777efbb8416f19d5fa76b5b0bbae9301393b3
volterra79 <boccacci.francesco@gmail.com>
2023-08-30 14:00:43 +0200

    Change to bosySlotProps props slot object from baseInput

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -5,16 +5,17 @@
 
 <template>
   <baseinput :state="state">
-    <template v-slot:body="{editable, notvalid, tabIndex, change, mobileChange}">
+    <template v-slot:body="bodySlotProps">
+      {{bodySlotProps.change}}
       <input
         :placeholder = "state.default"
-        @keyup       = "isMobile() ? mobileChange($event) : change()"
-        :tabIndex    = "tabIndex"
-        v-disabled   = "!editable"
+        @keyup       = "isMobile() ? bodySlotProps.mobileChange($event) : bodySlotProps.change()"
+        :tabIndex    = "bodySlotProps.tabIndex"
+        v-disabled   = "!bodySlotProps.editable"
         :field       = "state.name"
         class        = "form-control"
         v-model      = "state.value"
-        :class       = "{ 'input-error-validation' : notvalid }"
+        :class       = "{ 'input-error-validation' : bodySlotProps.notvalid }"
         :id          = "state.name"
       >
     </template>
@@ -32,8 +33,7 @@ export default {
     state: {
       type: Object,
       required: true,
-    }
+    },
   },
-
 };
 </script>
