1c510a2f64338aa49be2a864196abf19fbe1fda3
volterra79 <boccacci.francesco@gmail.com>
2023-08-30 14:08:08 +0200

    refactor inputFloat.vue removing baseInputMix

--- src/components/InputFloat.vue
+++ src/components/InputFloat.vue
@@ -5,32 +5,41 @@
 
 <template>
   <baseinput :state="state">
-    <input
-      @change      = "change"
-      @input       = "change"
-      class        = "form-control"
-      style        = "width: 100%"
-      :tabIndex    = "tabIndex"
-      v-disabled   = "!editable"
-      :class       = "{ 'input-error-validation' : notvalid }"
-      v-model      = "state.value"
-      slot         = "body"
-      type         = "number"
-      :step        = "state.step || 1"
-      :placeholder = "state.default"
-    >
+
+    <template v-slot:body="bodySlotProps">
+
+      <input
+        @change      = "bodySlotProps.change"
+        @input       = "bodySlotProps.change"
+        :tabIndex    = "bodySlotProps.tabIndex"
+        v-disabled   = "!bodySlotProps.editable"
+        :class       = "{ 'input-error-validation' : bodySlotProps.notvalid }"
+        v-model      = "state.value"
+        :step        = "state.step || 1"
+        :placeholder = "state.default"
+        class        = "form-control"
+        style        = "width: 100%"
+        type         = "number"
+      >
+    </template>
+
   </baseinput>
+
 </template>
 
 <script>
-import { baseInputMixin } from 'mixins';
 
 export default {
 
   /** @since 3.8.6 */
   name: 'input-float',
 
-  mixins: [ baseInputMixin ],
+  props: {
+    state: {
+      type: Object,
+      required: true,
+    }
+  }
 
 };
 </script>
