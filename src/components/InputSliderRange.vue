b1a501eec659a124e5405e26d62c4f4e10b5a5fe
Raruto <Raruto@users.noreply.github.com>
2023-09-05 12:58:37 +0200

    migrate `InputSliderRange.vue`

--- src/components/InputSliderRange.vue
+++ src/components/InputSliderRange.vue
@@ -5,43 +5,47 @@
 
 <template>
   <g3w-input :state="state">
-    <div slot="body" >
-      <span style="font-weight: bold">{{state.value}}</span>
-      <input
-        slot       = "body"
-        @change    = "change"
-        style      = "width: 100%; padding-right: 5px;"
-        :tabIndex  = "tabIndex"
-        v-disabled = "!editable"
-        :class     = "{ 'input-error-validation' : notvalid }"
-        v-model    = "state.value"
-        :min       = "state.input.options.min"
-        :max       = "state.input.options.max"
-        type       = "range"
-        :step      = "state.input.options.step"
-      >
-    </div>
+    <template #body="{ change, tabIndex, editable, notvalid }">
+      <div>
+        <span style="font-weight: bold">{{state.value}}</span>
+        <input
+          @change    = "change"
+          style      = "width: 100%; padding-right: 5px;"
+          :tabIndex  = "tabIndex"
+          v-disabled = "!editable"
+          :class     = "{ 'input-error-validation' : notvalid }"
+          v-model    = "state.value"
+          :min       = "state.input.options.min"
+          :max       = "state.input.options.max"
+          type       = "range"
+          :step      = "state.input.options.step"
+        >
+      </div>
+    </template>
   </g3w-input>
 </template>
 
 <script>
-import { baseInputMixin } from 'mixins';
-
 export default {
 
   /** @since 3.8.6 */
   name: 'input-slider-range',
 
-  mixins: [ baseInputMixin ],
+  props: {
+    state: {
+      type: Object,
+      required: true,
+    },
+  },
 
   watch: {
 
     'state.input.options.min'() {
-      this.service.changeInfoMessage()
+      this.$parent.getService().changeInfoMessage()
     },
 
     'state.input.options.max'() {
-      this.service.changeInfoMessage()
+      this.$parent.getService().changeInfoMessage()
     },
 
   },
