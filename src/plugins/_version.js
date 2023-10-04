ef2d04518e0515f600c59ae68754241f1151c867
Raruto <Raruto@users.noreply.github.com>
2023-10-04 11:37:20 +0200

    function closure

--- src/plugins/_version.js
+++ src/plugins/_version.js
@@ -1,3 +1,6 @@
 /** START: g3w-client/src/plugins/_version.js */
-window.initConfig.group.plugins[process.env.g3w_plugin_name] = Object.assign(window.initConfig.group.plugins[process.env.g3w_plugin_name] || {}, { version: process.env.g3w_plugin_version});
+(function() {
+  const plugins = window && window.initConfig && window.initConfig.group && window.initConfig.group.plugins;
+  if (plugins) plugins[process.env.g3w_plugin_name] = Object.assign(plugins[process.env.g3w_plugin_name] || {}, { version: process.env.g3w_plugin_version});
+})();
 /** END: g3w-client/src/plugins/_version.js */
