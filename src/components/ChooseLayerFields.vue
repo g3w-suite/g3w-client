1ffed1a5b7403a585113b57ef67b0d8c6c2d770c
volterra79 <boccacci.francesco@gmail.com>
2023-10-17 14:35:19 +0200

    Comments

--- src/components/ChooseLayerFields.vue
+++ src/components/ChooseLayerFields.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="g3w-fields-to-choose">
-    <h4 style="font-weight: bold" v-t="'download.fields.title'"></h4>
+    <h4 class="title" v-t="'download.fields.title'"></h4>
     <section class="g3w-fields-to-choose-inputs">
       <input
         @click.stop = "selectAll"
-        :id         = "`select_all_fields_to_download`"
+        :id         = "`select_all_fields_to_choose`"
         type        = "checkbox"
         class       = "magic-checkbox"
         :checked    = "all"
@@ -47,32 +47,42 @@ export default {
   },
   data() {
     return {
-      all: true,
+      all: true, // Boolen -  Change when select_all_fields_to_choose is change
     }
   },
   computed: {
     gridClass() {
-      return this.fields.length > 6
+      return this.fields.length > 6; // Boolean. Set a gid css class if there are more than 6 fields
     }
   },
   methods: {
+    /**
+     * Method to change field selected boolean poperty
+     * @param field
+     */
     selectField(field) {
       field.selected = !field.selected;
+      //if selected
       if (field.selected) {
+        //set all value if all fields have selected to true
         this.all = undefined === this.fields.find(field => !field.selected);
-        this.$emit('selected-fields', true);
       } else {
+        //noo all fields are selected. Sett all to false
         this.all = false;
-        this.$emit('selected-fields', undefined !== this.fields.find(field => field.selected));
       }
+      //need emit event selected-fields that pass true or false base if one field at least is selected
+      this.$emit('selected-fields', (field.selected || undefined !== this.fields.find(field => field.selected)));
     },
+    /**
+     * Method called when input checkbox select all is changed
+     */
     selectAll() {
       this.all = !this.all;
 
       this
         .fields
         .forEach(field => field.selected = this.all)
-
+      //emit event
       this.$emit('selected-fields', this.all);
     }
   },
@@ -82,6 +92,11 @@ export default {
   #g3w-fields-to-choose label{
     color: #000000 !important;
   }
+
+  #g3w-fields-to-choose .title {
+    font-weight: bold;
+  }
+
   #g3w-fields-to-choose .inputs-body.grid {
     display: grid;
     grid-template-columns: 50% 50%
