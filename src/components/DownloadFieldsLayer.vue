3a7dbcbb06e7b99d0ba827c14be67d15ddead3d1
volterra79 <boccacci.francesco@gmail.com>
2023-10-17 11:33:23 +0200

    :recycle: Clean component adding selected-fields event emit

--- src/components/DownloadFieldsLayer.vue
+++ src/components/DownloadFieldsLayer.vue
@@ -1,29 +1,35 @@
 <template>
-  <section id="g3w-fields-to-downloads">
+  <div id="g3w-fields-to-downloads">
+    <h4 style="font-weight: bold">Seleziona campi</h4>
+    <section class="g3w-fields-to-downloads-inputs">
       <input
         @click.stop = "selectAll"
         :id         = "`select_all_fields_to_download`"
         type        = "checkbox"
         class       = "magic-checkbox"
+        :checked    = "all"
       >
       <label
         :for  = "`select_all_fields_to_download`"
         style = "color: #FFF"> All
       </label>
-    <divider/>
-    <div v-for="field in fields" :key="field.name">
-      <input
-        @click.stop = "selectField(field)"
-        :id         = "`${field.name}_${index}_select_field_to_download`"
-        type        = "checkbox"
-        class       = "magic-checkbox"
-      >
-      <label
-        :for  = "`${field.name}_${index}_select_field_to_download`"
-        style = "color: #FFF"> {{ field.label }}
-      </label>
-    </div>
-  </section>
+      <divider/>
+      <div v-for="field in fields" :key="field.name">
+        <input
+          @click.stop = "selectField(field)"
+          :id         = "`${field.name}_${index}_select_field_to_download`"
+          type        = "checkbox"
+          class       = "magic-checkbox"
+          :checked    = "field.selected"
+        >
+        <label
+          :for  = "`${field.name}_${index}_select_field_to_download`"
+          style = "color: #FFF"> {{ field.label }}
+        </label>
+      </div>
+    </section>
+  </div>
+
 </template>
 
 <script>
@@ -44,18 +50,24 @@ export default {
   methods: {
     selectField(field) {
       field.selected = !field.selected;
+      if (field.selected) {
+        this.all = undefined === this.fields.find(field => !field.selected);
+        this.$emit('selected-fields', true);
+      } else {
+        this.all = false;
+        this.$emit('selected-fields', undefined !== this.fields.find(field => field.selected));
+      }
     },
     selectAll() {
       this.all = !this.all;
-    }
-  },
-  watch: {
-    all(bool) {
+
       this
         .fields
-        .forEach(field => field.selected = bool)
+        .forEach(field => field.selected = this.all)
+
+      this.$emit('selected-fields', this.all);
     }
-  }
+  },
 };
 </script>
 <style scoped>
