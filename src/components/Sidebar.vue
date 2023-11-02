5d858945092cbf97e9c434b1a83ed348ae4a9925
volterra79 <boccacci.francesco@gmail.com>
2023-11-02 10:21:39 +0100

    Fix in case of iframe, move show/hide sidebar on top

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -85,7 +85,7 @@
       <a
         href        = "#"
         class       = "sidebar-aside-toggle"
-        :class      = "{ 'g3w-disabled': disabled }"
+        :class      = "{ 'g3w-disabled': disabled, 'iframe': iframe}"
         :style      = "{zIndex: zIndex}"
         data-toggle = "offcanvas" role="button">
           <i :class="g3wtemplate.getFontClass('bars')"></i>
