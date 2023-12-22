541726b909199f02b82c4e90da053075d285e71b
volterra79 <boccacci.francesco@gmail.com>
2023-12-22 15:23:22 +0100

    Clean code and comments

--- src/app/gui/print/printservice.js
+++ src/app/gui/print/printservice.js
@@ -88,6 +88,10 @@ const PRINT_UTILS = {
     })
   },
 
+  /**
+   * Get wms url for current project
+   * @returns {*}
+   */
   getUrl() {
     return ProjectsRegistry.getCurrentProject().getLayersStore().getWmsUrl();
   },
@@ -138,6 +142,9 @@ inherit(PrintComponentService, G3WObject);
 
 const proto = PrintComponentService.prototype;
 
+/**
+ *
+ */
 proto.setInitState = function(){
   this.state.template = this.state.print[0].name;
   this.state.atlas = this.state.print[0].atlas;
@@ -157,6 +164,9 @@ proto.setInitState = function(){
   this.state.labels = this.state.print[0].labels;
 };
 
+/**
+ *
+ */
 proto.changeTemplate = function() {
   if (!this.state.template) return;
   const isPreviousAtlas = this.state.atlas;
@@ -165,31 +175,64 @@ proto.changeTemplate = function() {
   this.state.atlas = atlas;
   this.state.labels = labels;
   this.state.atlasValues = [];
-  this.state.atlas ? this._clearPrint() : isPreviousAtlas ? this.showPrintArea(true) : this._setPrintArea();
+  this.state.atlas ?
+
+    this._clearPrint() :
+
+    isPreviousAtlas ?
+      this.showPrintArea(true) :
+      this._setPrintArea();
 };
 
+/**
+ *
+ */
 proto.changeScale = function() {
   this.state.scala && this._setPrintArea();
 };
 
+/**
+ *
+ */
 proto.changeRotation = function() {
   this._mapService.setInnerGreyCoverBBox({
     rotation: this.state.rotation
   });
 };
 
+/**
+ *
+ * @returns {string}
+ * @private
+ */
 proto._getPrintExtent = function() {
   const [minx, miny, maxx, maxy] = [... this.state.printextent.lowerleft, ...this.state.printextent.upperright];
-  const extent = this._mapService.isAxisOrientationInverted() ? [miny, minx, maxy, maxx ] : [minx, miny, maxx, maxy];
-  return extent.join()
+  return (
+    this._mapService.isAxisOrientationInverted() ?
+      [miny, minx, maxy, maxx ] :
+      [minx, miny, maxx, maxy]
+  ).join();
 };
 
+/**
+ *
+ * @param extent
+ * @returns {string}
+ */
 proto.getOverviewExtent = function(extent={}) {
   const {xmin, xmax, ymin, ymax} = extent;
-  const overviewextent =  this._mapService.isAxisOrientationInverted() ? [ymin, xmin, ymax, xmax ] : [xmin, ymin, xmax, ymax];
-  return overviewextent.join();
+  return (
+    this._mapService.isAxisOrientationInverted() ?
+      [ymin, xmin, ymax, xmax ] :
+      [xmin, ymin, xmax, ymax]
+  ).join();
 };
 
+/**
+ *
+ * @returns {{template, maps: {extent: *, name: *, preset_theme: *, scale: *|null}[], rotation, format, scale: (null|*), is_maps_preset_theme: boolean, dpi: (number|*), labels}}
+ * @private
+ */
 proto._getOptionsPrint = function() {
   let is_maps_preset_theme = false;
   const maps = this.state.maps.map(map => {
@@ -201,6 +244,7 @@ proto._getOptionsPrint = function() {
       extent: map.overview ? this.getOverviewExtent(map.extent) : this._getPrintExtent()
     }
   });
+
   const options = {
     rotation: this.state.rotation,
     dpi: this.state.dpi,
@@ -215,11 +259,18 @@ proto._getOptionsPrint = function() {
   return options;
 };
 
+/**
+ *
+ */
 proto.setPrintAreaAfterCloseContent = function() {
   this._map.once('postrender', this._setPrintArea.bind(this));
   this.stopLoading();
 };
 
+/**
+ *
+ * @returns {Promise<unknown>}
+ */
 proto.print = function() {
   return new Promise((resolve, reject) => {
     //disable sidebar
@@ -260,7 +311,7 @@ proto.print = function() {
         perc: 100
       });
       const options = this._getOptionsPrint();
-      print(options, method=this.state.output.method)
+      print(options, this.state.output.method)
         .then(data => {
           this.state.output.url = data.url;
           this.state.output.layers = data.layers;
@@ -273,53 +324,80 @@ proto.print = function() {
         })
         .finally(()=> {
           // in case of no layers
-          !this.state.output.layers && GUI.disableSideBar(false)
+          if (!this.state.output.layers) {
+            GUI.disableSideBar(false);
+          }
         });
     }
   })
 
 };
 
+/**
+ *
+ */
 proto.startLoading = function() {
   this.state.output.loading = true;
 };
 
+/**
+ *
+ */
 proto.stopLoading = function() {
   this.state.output.loading = false;
 };
 
+/**
+ *
+ * @param error
+ */
 proto.showError = function(error) {
   GUI.notify.error(error || t("info.server_error"));
   GUI.closeContent();
 };
 
+/**
+ *
+ * @private
+ */
 proto._calculateInternalPrintExtent = function() {
-  const resolution = this._map.getView().getResolution();
-  const scala = parseFloat(this.state.scala);
+  const resolution            = this._map.getView().getResolution();
+  const scala                 = parseFloat(this.state.scala);
   const {h: height, w: width} = this.state.maps.find(map=> !map.overview);
-  const resolutionInMeters = this._mapService.getMapUnits() === 'm' ? resolution : getMetersFromDegrees(resolution);
-  const w = (((width / 1000.0) * scala) / resolutionInMeters);
-  const h = (((height  / 1000.0) * scala) / resolutionInMeters);
+  const resolutionInMeters    = this._mapService.getMapUnits() === 'm' ? resolution : getMetersFromDegrees(resolution);
+  const w                     = (((width / 1000.0) * scala) / resolutionInMeters);
+  const h                     = (((height  / 1000.0) * scala) / resolutionInMeters);
   // get current map center ( in pixel)
-  const center = [
+  const center                = [
     (this.state.size[0]) / 2, // X
     (this.state.size[1]) / 2  // Y
   ];
   // Calculate the inner bbox in pixel
-  const xmin = center[0] - (w / 2);
-  const ymin = center[1] - (h / 2);
-  const xmax = center[0] + (w / 2);
-  const ymax = center[1] + (h / 2);
-  this.state.printextent.lowerleft = this._map.getCoordinateFromPixel([xmin, ymax]) ? this._map.getCoordinateFromPixel([xmin, ymax]) : this.state.printextent.lowerleft;
-  this.state.printextent.upperright = this._map.getCoordinateFromPixel([xmax, ymin]) ? this._map.getCoordinateFromPixel([xmax, ymin]) : this.state.printextent.upperright;
+  const xmin                  = center[0] - (w / 2);
+  const ymin                  = center[1] - (h / 2);
+  const xmax                  = center[0] + (w / 2);
+  const ymax                  = center[1] + (h / 2);
+
+  this.state.printextent.lowerleft = this._map.getCoordinateFromPixel([xmin, ymax]) ?
+    this._map.getCoordinateFromPixel([xmin, ymax]) :
+    this.state.printextent.lowerleft;
+
+  this.state.printextent.upperright = this._map.getCoordinateFromPixel([xmax, ymin]) ?
+    this._map.getCoordinateFromPixel([xmax, ymin]) :
+    this.state.printextent.upperright;
+
   this.state.inner =  [xmin, ymax, xmax, ymin];
 };
 
+/**
+ *
+ * @private
+ */
 proto._setPrintArea = function() {
-  this.state.size = this._map.getSize();
-  const resolution = this._map.getView().getResolution();
+  this.state.size         = this._map.getSize();
+  const resolution        = this._map.getView().getResolution();
   this.state.currentScala = getScaleFromResolution(resolution, this._mapUnits);
-  this.state.center = this._map.getView().getCenter();
+  this.state.center       = this._map.getView().getCenter();
   this._calculateInternalPrintExtent();
   this._mapService.setInnerGreyCoverBBox({
     type: 'pixel',
@@ -328,17 +406,27 @@ proto._setPrintArea = function() {
   });
 };
 
+/**
+ *
+ * @param reset
+ * @private
+ */
 proto._clearPrint = function(reset=false) {
   ol.Observable.unByKey(this._moveMapKeyEvent);
   this._moveMapKeyEvent = null;
   this._mapService.stopDrawGreyCover();
 };
 
+/**
+ *
+ * @param maxResolution
+ * @private
+ */
 proto._setAllScalesBasedOnMaxResolution = function(maxResolution) {
-  let resolution = maxResolution;
-  const mapScala = getScaleFromResolution(resolution, this._mapUnits);
-  const orderScales = _.orderBy(this.state.scale, ['value'], ['desc']);
-  let scale = [];
+  let resolution             = maxResolution;
+  const mapScala             = getScaleFromResolution(resolution, this._mapUnits);
+  const orderScales          = _.orderBy(this.state.scale, ['value'], ['desc']);
+  let scale                  = [];
   let addedFirstHighestScale = false;
   const handleScala = scala => {
     scale.push(scala);
@@ -346,37 +434,58 @@ proto._setAllScalesBasedOnMaxResolution = function(maxResolution) {
     this._scalesResolutions[scala.value] = resolution;
     resolution = resolution / 2;
   };
-  orderScales.forEach((scala, index) => {
-    if (mapScala > scala.value) {
-      if (!addedFirstHighestScale) {
-        const higherScale = orderScales[index-1];
-        handleScala(higherScale);
-        addedFirstHighestScale = true;
+  orderScales
+    .forEach((scala, index) => {
+      if (mapScala > scala.value) {
+        if (!addedFirstHighestScale) {
+          const higherScale = orderScales[index-1];
+          handleScala(higherScale);
+          addedFirstHighestScale = true;
+        }
+        handleScala(scala);
       }
-      handleScala(scala);
-    }
-  });
+    });
+
   this.state.scale = scale;
 };
 
+/**
+ *
+ * @private
+ */
 proto._setInitialScalaSelect = function() {
   this.state.scala = this.state.scale[0].value;
   $('#scala').val(this.state.scala);
 };
 
+/**
+ *
+ * @param resolution
+ * @private
+ */
 proto._setCurrentScala = function(resolution) {
-  Object.entries(this._scalesResolutions).find(([scala, res]) => {
-    if (resolution <= res) {
-      this.state.scala = scala;
-      return true
-    }
-  });
+  Object
+    .entries(this._scalesResolutions
+    ).find(([scala, res]) => {
+      if (resolution <= res) {
+        this.state.scala = scala;
+        return true
+      }
+    });
 };
 
+/**
+ *
+ * @private
+ */
 proto._setMoveendMapEvent = function() {
   this._moveMapKeyEvent = this._map.on('moveend', this._setPrintArea.bind(this));
 };
 
+/**
+ *
+ * @private
+ */
 proto._showPrintArea = function() {
   if (this.state.atlas === undefined) {
     this._setPrintArea();
@@ -384,6 +493,10 @@ proto._showPrintArea = function() {
   }
 };
 
+/**
+ *
+ * @private
+ */
 proto._initPrintConfig = function() {
   if (!this._initialized) {
     const maxResolution = this._map.getView().getMaxResolution();
@@ -394,10 +507,15 @@ proto._initPrintConfig = function() {
   this._setCurrentScala(resolution);
 };
 
+/**
+ *
+ * @param bool
+ */
 proto.showPrintArea = function(bool) {
   // close content if open
   this.state.isShow = bool;
-  GUI.closeContent()
+  GUI
+    .closeContent()
     .then(mapComponent => {
       setTimeout(() => {
         this._mapService = mapComponent.getService();
@@ -415,12 +533,16 @@ proto.showPrintArea = function(bool) {
     })
 };
 
+/**
+ *
+ */
 proto.reload = function() {
-  this._project = ProjectsRegistry.getCurrentProject();
-  this._mapService = GUI.getService('map');
-  this._map = this._mapService.viewer.map;
-  this.state.print = this._project.state.print || [];
+  this._project      = ProjectsRegistry.getCurrentProject();
+  this._mapService   = GUI.getService('map');
+  this._map          = this._mapService.viewer.map;
+  this.state.print   = this._project.state.print || [];
   this.state.visible = this.state.print.length > 0;
+
   if (this.state.visible) {
     this.state.template = this.state.print[0].name;
     !this._initialized && this.init();
@@ -430,7 +552,9 @@ proto.reload = function() {
       this.state.scale = scale;
       this._setAllScalesBasedOnMaxResolution(maxResolution);
     });
-  } else this._clearPrint();
+  } else {
+    this._clearPrint();
+  }
 };
 
 /**
@@ -446,7 +570,9 @@ proto.reload = function() {
  */
 function print(opts = {}, method = 'GET') {
   const store  = ProjectsRegistry.getCurrentProject().getLayersStore();
-  const layers = store.getLayers({ PRINTABLE: { scale: opts.scale }, SERVERTYPE: 'QGIS' }).reverse(); // reverse order is important
+  const layers = store
+    .getLayers({ PRINTABLE: { scale: opts.scale }, SERVERTYPE: 'QGIS' })
+    .reverse(); // reverse order is important
 
   // skip when ..
   if (!layers.length) {
