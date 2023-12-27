c259b604dd675de8fbfdcffb1db1cfd4bd67a4c6
volterra79 <boccacci.francesco@gmail.com>
2023-12-27 10:37:22 +0100

    Clean Code

--- src/components/InputSelect.vue
+++ src/components/InputSelect.vue
@@ -5,31 +5,62 @@
 
 <template>
   <baseinput :state="state">
-    <span slot="label-action" data-placement="top" v-t-tooltip="'sdk.form.inputs.tooltips.picklayer'" v-disabled="disabled"
-      @click="pickLayerValue" v-if="showPickLayer" style="cursor: pointer; position:relative; top: 2px; font-size: 1.2em"
+    <span
+      v-if="showPickLayer"
+      slot="label-action"
+      data-placement="top"
+      v-t-tooltip="'sdk.form.inputs.tooltips.picklayer'"
+      v-disabled="disabled"
+      @click.stop="pickLayerValue"
+      style="cursor: pointer; position:relative; top: 2px; font-size: 1.2em"
       :class="g3wtemplate.font['crosshairs']" class="skin-color">
     </span>
-    <div slot="body" v-disabled="disabled" :tabIndex="tabIndex">
-      <select ref="select" style="width:100%;"  class="form-control">
-        <option v-if="showNullOption" :value="select2NullValue"></option>
-        <option :value="getValue(value.value)" v-for="value in state.input.options.values" :key="getValue(value.value)">{{ value.key }}</option>
+    <div
+      slot="body"
+      v-disabled="disabled"
+      :tabIndex="tabIndex"
+    >
+      <select
+        ref="select"
+        style="width:100%;"
+        class="form-control"
+      >
+        <option
+          v-if="showNullOption"
+          :value="select2NullValue">
+        </option>
+        <option
+          v-for="value in state.input.options.values"
+          :key="getValue(value.value)"
+          :value="getValue(value.value)">
+            {{ value.key }}
+        </option>
       </select>
     </div>
-    <p v-if="loadingState === 'error'" class="error-input-message" slot="message" v-t="'server_error'"></p>
+    <p
+      v-if="loadingState === 'error'"
+      class="error-input-message"
+      slot="message"
+      v-t="'server_error'">
+    </p>
   </baseinput>
 </template>
 
 <script>
+
 import CatalogLayersStoresRegistry from 'store/catalog-layers';
-import MapLayersStoresRegistry from 'store/map-layers';
-import GUI from 'services/gui';
-import { selectMixin, select2Mixin } from 'mixins';
+import MapLayersStoresRegistry     from 'store/map-layers';
+import GUI                         from 'services/gui';
+import {
+  selectMixin,
+  select2Mixin
+}                                  from 'mixins';
 
-const PickLayerInputService = require('gui/inputs/picklayer/service');
-const Layer = require('core/layers/layer');
-const InputMixin = require('gui/inputs/input');
+const PickLayerInputService  = require('gui/inputs/picklayer/service');
+const Layer                  = require('core/layers/layer');
+const InputMixin             = require('gui/inputs/input');
 
-const G3W_SELECT2_NULL_VALUE = null; // neede to set nul value instead of empty string
+const G3W_SELECT2_NULL_VALUE = null; // need to set nul value instead of empty string
 
 export default {
 
@@ -44,13 +75,20 @@ export default {
     }
   },
   computed:{
-    showNullOption(){
+    /**
+     *
+     * @returns {boolean}
+     */
+    showNullOption() {
       return this.state.nullOption === undefined || this.state.nullOption === true;
     },
-    select2NullValue(){
+    /**
+     *
+     * @returns {false|null}
+     */
+    select2NullValue() {
       return this.showNullOption && G3W_SELECT2_NULL_VALUE;
     },
-
   },
   watch: {
     async 'state.input.options.values'(values) {
@@ -58,29 +96,37 @@ export default {
       let changed = false;
       if (!this.autocomplete) {
         let value;
-        if (values.length === 0) value = G3W_SELECT2_NULL_VALUE;
-        else {
+        if (values.length === 0) {
+          value = G3W_SELECT2_NULL_VALUE;
+        } else {
           const findvalue = values.find(keyvalue => keyvalue.value == this.state.value);
-          if (!findvalue) value = G3W_SELECT2_NULL_VALUE;
-          else value = findvalue.value;
+          value = undefined === findvalue ? G3W_SELECT2_NULL_VALUE : findvalue.value;
         }
+
         changed = value != this.state.value;
         this.state.value = value;
         this.setValue();
       }
-      changed && this.change();
+
+      if (changed) {
+        this.change();
+      }
     }
   },
   methods: {
-    async pickLayerValue(){
+    /**
+     *
+     * @returns {Promise<void>}
+     */
+    async pickLayerValue() {
       try {
-        if (this.picked){
+        if (this.picked) {
           this.pickLayerInputService.unpick();
           this.picked = false;
         } else {
           this.picked = true;
           const values = await this.pickLayerInputService.pick();
-          const {value:field}= this.state.input.options;
+          const { value:field }= this.state.input.options;
           const value = values[field];
           this.select2.val(value).trigger('change');
           this.changeSelect(value);
@@ -90,7 +136,7 @@ export default {
           });
           this.picked = false;
         }
-      } catch(err){
+      } catch(err) {
         GUI.showUserMessage({
           type: "warning",
           message: 'sdk.form.inputs.messages.errors.picklayer',
@@ -99,10 +145,22 @@ export default {
         this.picked = false;
       }
     },
-    setAndListenSelect2Change(){
+    /**
+     *
+     */
+    setAndListenSelect2Change() {
       this.select2.on('select2:select', event => {
-        let value = event.params.data.$value ? event.params.data.$value : event.params.data.id;
-        value = this.showNullOption ? value === G3W_SELECT2_NULL_VALUE ? null : value.toString() : value.toString();
+        let value = event.params.data.$value ?
+          event.params.data.$value :
+          event.params.data.id;
+        value = this.showNullOption ?
+
+          value === G3W_SELECT2_NULL_VALUE ?
+            null :
+            value.toString() :
+
+          value.toString();
+
         this.changeSelect(value);
       });
     }
@@ -112,8 +170,12 @@ export default {
     if (this.state.input.type === 'select_autocomplete') {
       const dependencyLayerId = this.state.input.options.layer_id;
       try {
-        const dependencyLayer = MapLayersStoresRegistry.getLayerById(dependencyLayerId).getEditingLayer() || CatalogLayersStoresRegistry.getLayerById(dependencyLayerId);
-        this.showPickLayer = dependencyLayer ? dependencyLayer.getType() !== Layer.LayerTypes.TABLE : false;
+        const dependencyLayer = MapLayersStoresRegistry
+          .getLayerById(dependencyLayerId)
+          .getEditingLayer() || CatalogLayersStoresRegistry.getLayerById(dependencyLayerId);
+        this.showPickLayer = dependencyLayer ?
+          dependencyLayer.getType() !== Layer.LayerTypes.TABLE :
+          false;
         const {value:field, layer_id} = this.state.input.options;
         const options = {
           layer_id,
@@ -123,9 +185,11 @@ export default {
         this.pickLayerInputService = this.showPickLayer && new PickLayerInputService(options);
       } catch(err) {}
     }
-    this.autocomplete && this.state.value && this.service.getKeyByValue({
-      search: this.state.value
-    });
+    if (this.autocomplete && this.state.value) {
+      this.service.getKeyByValue({
+        search: this.state.value
+      });
+    }
   },
   async mounted() {
     await this.$nextTick();
@@ -148,7 +212,8 @@ export default {
             this.resetValues();
             this.service.getData({
               search
-            }).then(values => success(values))
+            })
+              .then(values => success(values))
               .catch(err => failure(err))
           },
           processResults: (data, params) => {
@@ -161,16 +226,18 @@ export default {
             }
           }},
       });
-    } else this.select2 = selectElement.select2({
-          language,
-          dropdownParent,
-          minimumResultsForSearch: this.isMobile() ? -1 : null
-        });
+    } else {
+      this.select2 = selectElement.select2({
+        language,
+        dropdownParent,
+        minimumResultsForSearch: this.isMobile() ? -1 : null
+      });
+    }
     this.setAndListenSelect2Change();
     this.setValue();
   },
   beforeDestroy() {
-    if (this.pickLayerInputService){
+    if (this.pickLayerInputService) {
       this.pickLayerInputService.clear();
       this.pickLayerInputService = null;
     }
