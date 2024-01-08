8c4065ed8dd41a9c8adcd77e8521439b3893047a
volterra79 <boccacci.francesco@gmail.com>
2024-01-08 11:24:18 +0100

    Clean code

--- src/utils/geo/index.js
+++ src/utils/geo/index.js
@@ -4,17 +4,21 @@
  * @since 3.9.0
  */
 
-import CONSTANT from 'app/constant';
+import CONSTANT                from 'app/constant';
 import MapLayersStoresRegistry from 'store/map-layers';
-import GUI from 'services/gui';
+import GUI                     from 'services/gui';
 
-const { toRawType, uniqueId } = require('utils');
-const WMSLayer = require('core/layers/map/wmslayer');
-const Filter = require('core/layers/filter/filter');
+const { toRawType, uniqueId }      = require('utils');
+const WMSLayer                     = require('core/layers/map/wmslayer');
+const Filter                       = require('core/layers/filter/filter');
 const { response: responseParser } = require('utils/parsers');
 
 const geometryFields = CONSTANT.GEOMETRY_FIELDS;
-const { QUERY_POINT_TOLERANCE, G3W_FID, GEOMETRY_TYPES: GeometryTypes } = CONSTANT;
+const {
+  QUERY_POINT_TOLERANCE,
+  G3W_FID,
+  GEOMETRY_TYPES: GeometryTypes
+}                   = CONSTANT;
 
 const Geometry = {
 
