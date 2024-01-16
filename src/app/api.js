a86b8d7185db1c95fa2b09f19f5e9f362de32505
volterra79 <boccacci.francesco@gmail.com>
2024-01-16 16:04:03 +0100

    extract getInfo() from plugin

--- src/app/api.js
+++ src/app/api.js
@@ -271,15 +271,30 @@ module.exports = {
         new Promise((resolve) => $script('https://unpkg.com/platform@1.3.6/platform.js', resolve)),
         new Promise((resolve) => ApplicationService.complete ? resolve() : ApplicationService.on('complete', resolve))
       ])
-      .finally(() => {
+      .finally(async () => {
         /** @since 3.8.0 */
         const platform = window.platform || {};
+        /** @since 3.10.0 */
+        await Promise.allSettled(Object
+          .keys(PluginsRegistry.pluginsConfigs)
+          .map(p => {
+            if (PluginsRegistry.getPlugin(p)) {
+              return Promise.resolve(PluginsRegistry.getPlugin(p));
+            } else {
+              return new Promise((resolve, reject) => {
+                PluginsRegistry.onceafter('registerPlugin', resolve);
+              })
+            }
+          })
+        );
 
         window.console.info(`
 [g3wsdk.info]\n
 - g3w-admin: __${initConfig.version}__
-- g3w-client: __${G3W_CONSTANT.APP_VERSION}${__git__ ? `.${__git__.branch}:${__git__.commit}` : ''}__
-- plugins:\n${Object.entries(PluginsRegistry.pluginsConfigs).map((p) => (`    - ${p[0]}: __${p[1].version}__`)).join('\n')}
+- g3w-client: __${G3W_CONSTANT.APP_VERSION}${__git__ 
+   ? `[${__git__.branch}][${__git__.commit}]__` 
+   : '__'}
+- plugins:\n${Object.entries(PluginsRegistry.pluginsConfigs).map((p) => (`    - ${p[0]}: __${PluginsRegistry.getPlugin(p[0]).getInfo()}__`)).join('\n')}
 - browser: __${platform.name} ${platform.version}__
 - operating system: __${platform.os.toString()}__
 `.trim());
