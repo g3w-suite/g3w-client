eca264ddbad4b900bb1e0d2c61d4a96b0b2f0f2d
Francesco Boccacci <boccacci.francesco@gmail.com>
2024-01-16 11:38:06 +0100

    Handle deep relations on editing [g3w-admin#PR685] (#526)
    * Clean code
    
    * Handle deep commit relations
    
    * Clean code
    
    * Clean code
    
    * Change comment
    
    * Change comment
    
    * Fix show relation on right height
    
    * refactor
    
    Refactor relations commit object
    
    Co-authored-by: Raruto <Raruto@users.noreply.github.com>
    
    * Clean code and comments
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * Remove _.isNull lodash dependency
    
    * Clean code
    
    * jsdoc
    
    * jsdoc
    
    * jsdoc
    
    ---------
    
    Co-authored-by: Raruto <Raruto@users.noreply.github.com>

--- src/app/core/editing/history.js
+++ src/app/core/editing/history.js
@@ -1,4 +1,7 @@
-const { base, inherit } = require('utils');
+const {
+  base,
+  inherit
+}               = require('utils');
 const G3WObject = require('core/g3wobject');
 
 function History(options = {}) {
@@ -24,10 +27,11 @@ function History(options = {}) {
   *
   *
   * */
-  // set maximun "buffer history" lenght for  undo redo
+  // set maximum "buffer history" lenght for  undo redo
   this._maxSteps = 10;
+  //Array of states of a layer in editing
   this._states = [];
-  // reactive state of histrory
+  // reactive state of history
   this.state = {
     commit: false,
     undo:false,
@@ -40,23 +44,28 @@ inherit(History, G3WObject);
 
 const proto = History.prototype;
 
+/**
+ * @param uniqueId
+ * @param items
+ * 
+ * @returns {*}
+ */
 proto.add = function(uniqueId, items) {
   //state object is an array of feature/features changed in a transaction
   const d = $.Deferred();
   // before insert an item into the history
   // check if are at last state step (no redo was done)
-  // in this way avoid starge barch in the history
-  //If we are in the middle of undo, delete all changes in the histroy from the current "state"
-  // so i can create a new history
-
+  // If we are in the middle of undo, delete all changes in the history from the current "state"
+  // so if it can create a new history
   if (this._current === null) {
     this._states = [{
       id: uniqueId,
       items
     }]
   } else {
-    if (this._states.length && this._current < this.getLastState().id)
+    if (this._states.length > 0 && this._current < this.getLastState().id) {
       this._states = this._states.filter(state => state.id <= this._current);
+    }
     this._states.push({
       id: uniqueId,
       items
@@ -71,22 +80,33 @@ proto.add = function(uniqueId, items) {
   return d.promise();
 };
 
+/**
+ * @param layerId
+ * @param clear
+ * 
+ * @returns {*[]}
+ */
 proto.getRelationStates = function(layerId, {clear=false}={}) {
   const relationStates = [];
   for (let i=0; i < this._states.length; i++) {
     const state = this._states[i];
-    const relationItems = state.items.filter((item) => {
-      const _layerId = Array.isArray(item) ? item[0].layerId: item.layerId;
-      return _layerId === layerId
-    });
-    relationItems.length && relationStates.push({
-      id: state.id,
-      items:relationItems
-    })
+    const relationItems = state
+      .items
+      .filter((item) => (Array.isArray(item) ? item[0].layerId : item.layerId) === layerId);
+
+    if (relationItems.length > 0) {
+      relationStates.push({
+        id: state.id,
+        items:relationItems
+      })
+    }
   }
   return relationStates;
 };
 
+/**
+ * @param state
+ */
 proto.insertState = function(state) {
   const stateId = state.id;
   let index = this._states.length;
@@ -95,31 +115,40 @@ proto.insertState = function(state) {
     if (_state.id > stateId) {
       index = i;
       break;
-    } else if (_state.id === stateId)
+    } else if (_state.id === stateId) {
       index = -1;
       break;
+    }
   }
   if (index > -1) {
-    if (this._current < stateId)
+    if (this._current < stateId) {
       this._current = stateId;
+    }
     this._states.splice(index, 0, state)
   }
 };
 
+/**
+ * @param stateId
+ */
 proto.removeState = function(stateId) {
   let index;
-  for (i = 0; i < this._states.length; i++) {
+  for (let i = 0; i < this._states.length; i++) {
     const state = this._states[i];
     if (state.id === stateId) {
       index = i;
       break;
     }
   }
-  if (this._current === stateId)
+  if (this._current === stateId) {
     this._current = this._states.length > 1 ? this._states[index-1].id : null;
+  }
   this._states.splice(index,1);
 };
 
+/**
+ * @param stateIds
+ */
 proto.removeStates = function(stateIds = []) {
   for (let i = 0; i < stateIds.length; i++) {
     const stateId = stateIds[i];
@@ -127,6 +156,9 @@ proto.removeStates = function(stateIds = []) {
   }
 };
 
+/**
+ * @param states
+ */
 proto.insertStates = function(states=[]) {
   for (let i=0; i< states.length; i++) {
     this.insertState(states[i]);
@@ -134,15 +166,19 @@ proto.insertStates = function(states=[]) {
   this.canCommit();
 };
 
-// internal method to change the state of the  history when we check
-// a call to a function that modify the hsitory state
+/**
+ * internal method to change the state of the  history when we check
+ * a call to a function that modify the hsitory state
+ */
 proto._setState = function() {
   this.canUndo();
   this.canCommit();
   this.canRedo();
 };
 
-//check if was did an update (update are array contains two items , old e new value)
+/**
+ * check if was done an update (update are array contains two items , old e new value)
+ */
 proto._checkItems = function(items, action) {
   /**
    * action: <reffererd to array index>
@@ -153,25 +189,31 @@ proto._checkItems = function(items, action) {
     own: [], //array of changes of layer of the current session
     dependencies: {} // dependencies
   };
-  items.forEach((item) => {
-    if (Array.isArray(item))
-      item = item[action];
-    // check if belong to session
-    if (this.id === item.layerId) {
-      newItems.own.push(item)
-    } else {
-      if (!newItems.dependencies[item.layerId])
-        newItems.dependencies[item.layerId] = {
-          own: [],
-          dependencies: {}
-        };
-      newItems.dependencies[item.layerId].own.push(item);
-    }
-  });
+
+  items
+    .forEach((item) => {
+      if (Array.isArray(item)) {
+        item = item[action];
+      }
+      // check if belong to session
+      if (this.id === item.layerId) {
+        newItems.own.push(item)
+      } else {
+        if (!newItems.dependencies[item.layerId])
+          newItems.dependencies[item.layerId] = {
+            own: [],
+            dependencies: {}
+          };
+        newItems.dependencies[item.layerId].own.push(item);
+      }
+    });
+
   return newItems;
 };
 
-// method undo
+/**
+ * undo method
+ */
 proto.undo = function() {
   let items;
   if (this._current === this.getFirstState().id) {
@@ -191,7 +233,9 @@ proto.undo = function() {
   return items;
 };
 
-//method redo
+/**
+ * redo method
+ */
 proto.redo = function() {
   let items;
   // if not set get first state
@@ -213,33 +257,48 @@ proto.redo = function() {
   return items;
 };
 
+/**
+ * @param unsetnewids
+ */
 proto.setItemsFeatureIds = function(unsetnewids=[]) {
-  unsetnewids.forEach(unsetnewid =>{
-    const {id, clientid} = unsetnewid;
-    this._states.forEach(state => {
-      const {items} = state;
-      items.forEach(item => {
-        const feature = item.feature.getId() === clientid && item.feature;
-        if (feature) {
-          feature.setId(id);
-        }
-      })
-    });
-  })
+  unsetnewids
+    .forEach((unsetnewid) =>{
+      const {id, clientid} = unsetnewid;
+      this._states
+        .forEach(state => {
+          const {items} = state;
+          items
+            .forEach(item => {
+              const feature = item.feature.getId() === clientid && item.feature;
+              if (feature) {
+                feature.setId(id);
+              }
+            })
+        });
+    })
 };
 
+/**
+ * @param ids
+ */
 proto.clear = function(ids) {
-  if (ids)
+  if (ids) {
     this._states.forEach((state, idx) => {
       if (ids.indexOf(state.id) !== -1) {
-        if (this._current && this._current === state.id())
+        if (this._current && this._current === state.id()) {
           this.undo();
+        }
         this._states.splice(idx, 1);
       }
     });
-  else this._clearAll();
+  } else {
+    this._clearAll();
+  }
 };
 
+/**
+ * @private
+ */
 proto._clearAll =  function() {
   this._states = [];
   this._current = null;
@@ -248,19 +307,33 @@ proto._clearAll =  function() {
   this.state.undo = false;
 };
 
+/**
+ * @param id
+ * 
+ * @returns {T}
+ */
 proto.getState = function(id) {
   return this._states.find(state => state.id === id);
 };
 
+/**
+ * @returns {*|null}
+ */
 proto.getFirstState = function() {
   return this._states.length ? this._states[0] : null;
 };
 
+/**
+ * @returns {*|null}
+ */
 proto.getLastState = function() {
   const length = this._states.length;
   return length ? this._states[length -1] : null;
 };
 
+/**
+ * @returns {null}
+ */
 proto.getCurrentState = function() {
   let currentState = null;
   if (this._current && this._states.length) {
@@ -271,7 +344,11 @@ proto.getCurrentState = function() {
   return currentState;
 };
 
-// funzione che mi permette di ricavarel'indice dello stato corrente
+/**
+ * Get index of current state
+ * 
+ * @returns {null}
+ */
 proto.getCurrentStateIndex = function() {
   let currentStateIndex = null;
   if (this._current && this._states.length) {
@@ -285,64 +362,86 @@ proto.getCurrentStateIndex = function() {
   return currentStateIndex;
 };
 
-// method that response true if we can commit
+/**
+ * @returns { boolean } true if we can commit
+ */
 proto.canCommit = function() {
   const checkCommitItems = this.commit();
   let canCommit = false;
   for (let layerId in checkCommitItems) {
     const commitItem = checkCommitItems[layerId];
-    canCommit = canCommit || !!commitItem.length
+    canCommit = canCommit || commitItem.length > 0;
   }
   this.state.commit = canCommit;
   return this.state.commit;
 };
 
-//cauUdo method
+/**
+ * canUdo method
+ */
 proto.canUndo = function() {
   const steps = (this._states.length - 1) - this.getCurrentStateIndex();
-  this.state.undo = !_.isNull(this._current) && (this._maxSteps > steps);
+  this.state.undo = (null !== this._current) && (this._maxSteps > steps);
   return this.state.undo;
 };
 
-// Caon Redo function
+/**
+ * CanRedo function
+ */ 
 proto.canRedo = function() {
-  this.state.redo = this.getLastState() && this.getLastState().id != this._current || _.isNull(this._current) && this._states.length > 0;
+  this.state.redo = (
+    (this.getLastState() && this.getLastState().id != this._current))
+    || (null ===this._current && this._states.length > 0);
   return this.state.redo;
 };
 
+/**
+ * @returns {T[]}
+ * 
+ * @private
+ */
 proto._getStatesToCommit = function() {
   return this._states.filter(state => state.id <= this._current);
 };
 
-//get all changes to send to server (mandare al server)
+/**
+ * get all changes to send to server (mandare al server)
+ */
 proto.commit = function() {
   const commitItems = {};
   const statesToCommit = this._getStatesToCommit();
-  statesToCommit.forEach(state => {
-    state.items.forEach(item => {
+  statesToCommit
+    .forEach(state => {
+      state.items.forEach((item) => {
       let add = true;
-      if (Array.isArray(item)) item = item[1];
-      commitItems[item.layerId] && commitItems[item.layerId].forEach((commitItem, index) => {
-        // check if already inserted feature
-        if (commitItem.getUid() === item.feature.getUid()) {
-          if (item.feature.isNew() && !commitItem.isDeleted() && item.feature.isUpdated() ) {
-            const _item = item.feature.clone();
-            _item.add();
-            commitItems[item.layerId][index] = _item;
-          } else if (item.feature.isNew() && item.feature.isDeleted()) {
-            commitItems[item.layerId].splice(index, 1);
-          } else if (item.feature.isUpdated() || item.feature.isDeleted())
-            commitItems[item.layerId][index] = item.feature;
-          add = false;
-          return false;
-        }
-      });
+      if (Array.isArray(item)) {
+        item = item[1];
+      }
+      if (commitItems[item.layerId]) {
+        commitItems[item.layerId].forEach((commitItem, index) => {
+          // check if already inserted feature
+          if (commitItem.getUid() === item.feature.getUid()) {
+            if (item.feature.isNew() && !commitItem.isDeleted() && item.feature.isUpdated()) {
+              const _item = item.feature.clone();
+              _item.add();
+              commitItems[item.layerId][index] = _item;
+            } else if (item.feature.isNew() && item.feature.isDeleted()) {
+              commitItems[item.layerId].splice(index, 1);
+            } else if (item.feature.isUpdated() || item.feature.isDeleted()) {
+              commitItems[item.layerId][index] = item.feature;
+            }
+            add = false;
+            return false;
+          }
+        });
+      }
       if (add) {
         const feature = item.feature;
         const layerId = item.layerId;
         if (!(!feature.isNew() && feature.isAdded())) {
-          if (!commitItems[layerId])
+          if (!commitItems[layerId]) {
             commitItems[layerId] = [];
+          }
           commitItems[layerId].push(feature);
         }
       }
