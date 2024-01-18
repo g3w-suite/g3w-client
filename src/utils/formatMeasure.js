59d9f6909e50822e884b71a5dcef0c92d12c9861
volterra79 <boccacci.francesco@gmail.com>
2024-01-18 16:50:12 +0100

    Remove reference to require('utils/geo');

--- src/utils/formatMeasure.js
+++ src/utils/formatMeasure.js
@@ -1,24 +1,27 @@
-import { getCurrentMapUnit }    from 'utils/getCurrentMapUnit';
-import { getLengthMessageText } from 'utils/getLengthMessageText';
-import { getAreaMessageText }   from 'utils/getAreaMessageText';
+import { getCurrentMapUnit }              from 'utils/getCurrentMapUnit';
+import { getLengthMessageText }           from 'utils/getLengthMessageText';
+import { getAreaMessageText }             from 'utils/getAreaMessageText';
+import { isLineGeometryType }             from "utils/isLineGeometryType";
+import { isPolygonGeometryType }          from "utils/isPolygonGeometryType";
+import { isMultiGeometry }                from "utils/isMultiGeometry";
+import { multiGeometryToSingleGeometries} from 'utils/multiGeometryToSingleGeometries';
 
 export function formatMeasure({ geometry, projection } = {}, options = {}) {
   /**
    * @FIXME circular dependency (ie. empty object when importing at top level), ref: #130
    */
-  const { Geometry, multiGeometryToSingleGeometries } = require('utils/geo');
   //
   const geometryType = geometry.getType();
   const unit = getCurrentMapUnit();
-  if (Geometry.isLineGeometryType(geometryType)) {
+  if (isLineGeometryType(geometryType)) {
     return getLengthMessageText({
       unit,
       projection,
       geometry
     });
-  } else if (Geometry.isPolygonGeometryType(geometryType)){
+  } else if (isPolygonGeometryType(geometryType)){
     let segments;
-    if (Geometry.isMultiGeometry(geometryType)) {
+    if (isMultiGeometry(geometryType)) {
       segments = [];
       multiGeometryToSingleGeometries(geometry).forEach(geometry => {
         geometry.getLinearRing().getCoordinates().forEach(coordinates => segments.push(coordinates))
@@ -26,4 +29,4 @@ export function formatMeasure({ geometry, projection } = {}, options = {}) {
     } else segments = geometry.getLinearRing().getCoordinates();
     return getAreaMessageText({unit, geometry, projection, segments});
   }
-};
+}
