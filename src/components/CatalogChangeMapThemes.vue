5bfc3c88b352123f4c9bcafedb1caecf084a20a6
volterra79 <boccacci.francesco@gmail.com>
2024-01-18 10:42:24 +0100

    Add save section to CatalogChangeMapThemes.vue

--- src/components/CatalogChangeMapThemes.vue
+++ src/components/CatalogChangeMapThemes.vue
@@ -13,39 +13,78 @@
       <a
         href="#"
         ref="g3w-map-theme-ancor"
-        style="display: flex; align-items: center; padding: 5px 5px 5px 2px;"
+        style="padding: 0; margin-bottom: 5px;"
       >
-        <i
-          style="padding: 3px;"
-          :class="g3wtemplate.getFontClass('caret-down')">
-        </i>
-        <i
-          style="padding: 0 0 0 4px;"
-          :class="g3wtemplate.getFontClass('eye')">
-        </i>
-        <span
-          v-show="current_map_theme"
-          class="treeview-label g3w-long-text"
-          style="overflow: hidden; white-space: normal;text-overflow: ellipsis;"
-        >
+        <section style="display: flex;  flex-wrap: wrap; align-items: center; padding: 5px;">
+          <i
+                  style="padding: 3px;"
+                  :class="g3wtemplate.getFontClass('caret-down')">
+          </i>
+          <i
+                  style="padding: 0 0 0 4px;"
+                  :class="g3wtemplate.getFontClass('eye')">
+          </i>
+          <!-- Text of current theme -->
           <span
-            style="color: #cccccc !important;"
-            v-t:pre="'sdk.catalog.current_map_theme_prefix'"
-          >:</span>
+                  v-if="current_map_theme"
+                  class="treeview-label g3w-long-text"
+                  style="overflow: hidden; white-space: normal;text-overflow: ellipsis;"
+          >
+      <span
+              style="color: #cccccc !important;"
+              v-t:pre="'sdk.catalog.current_map_theme_prefix'"
+      >:</span>
+      <span
+              class="skin-color"
+              style="font-size: 1.1em;">
+          {{ current_map_theme }}
+      </span>
+    </span>
           <span
-            class="skin-color"
-            style="font-size: 1.1em;">
-              {{ current_map_theme }}
-          </span>
-        </span>
-        <span
-          v-show="!current_map_theme"
-          class="treeview-label"
-          style="color: #cccccc !important; font-weight: bold"
+                  v-else
+                  class="treeview-label"
+                  style="color: #cccccc !important; font-weight: bold"
+          >
+      <span v-t="'sdk.catalog.choose_map_theme'"></span>
+    </span>
+          <!-- Save theme button -->
+          <span
+            @click.stop="showSaveSection = !showSaveSection"
+            :class="g3wtemplate.getFontClass('save')"
+            class="action sidebar-button sidebar-button-icon skin-color"
+            style="margin-left: auto; font-size: 1.2em; padding: 5px;"
+          >
+      </span>
+        </section>
+
+        <!-- SAVE MAP THEME SECTION -->
+        <section
+          v-show="showSaveSection"
+          id="save-map-theme"
+          @click.stop=""
+          style="
+            flex-basis: 100%;
+            display: flex;
+            flex: 1 1;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #2c3b41;
+            padding: 0 5px;
+            margin-top: 5px;"
         >
-          <span v-t="'sdk.catalog.choose_map_theme'"></span>
-        </span>
+          <div class="form-group">
+            <label for="save-map-theme-input" class="skin-color">Nome</label>
+            <input id="save-map-theme-input" style="margin-bottom: 0" v-model="themeName" class="form-control">
+          </div>
+          <button
+            @click.stop="save"
+            v-disabled="!canSave"
+            style="height: 50px; font-weight: bold"
+            class="btn skin-background-color" v-t="'save'">
+          </button>
+        </section>
       </a>
+
       <ul
         id="g3w-catalog-views"
         class="treeview-menu"
@@ -80,18 +119,39 @@
 
   export default {
     name: "changemapthemes",
-    data() {
-      const collapsed = ProjectsRegistry.getCurrentProject().state.toc_themes_init_status === 'collapsed';
-      const current_map_theme = this.map_themes.find(map_theme => map_theme.default);
-      return {
-        current_map_theme: current_map_theme ? current_map_theme.theme : null,
-        collapsed
-      }
-    },
     props: {
       map_themes: {
         type: Array,
         default: []
+      },
+      layerstrees: {
+        type: Array,
+      }
+    },
+    data() {
+      const collapsed = 'collapsed' === ProjectsRegistry.getCurrentProject().state.toc_themes_init_status ;
+      const current_map_theme = this.map_themes.find(map_theme => map_theme.default);
+      return {
+        current_map_theme: current_map_theme ? current_map_theme.theme : null,
+        collapsed,
+        canSave: false, /** @since 3.10.0 */
+        themeName: null, /** @since 3.10.0 */
+        showSaveSection: false /** @since 3.10.0 */
+      }
+    },
+    methods: {
+      /**
+       * Save current theme (layerstree state)
+       * @since 3.10.0
+       */
+      save() {
+        /**
+         * Node: keys [id, name, showfeaturecount, visible]
+         * Group: keys [checked, expanded, mutually-exclusive, name, nodes]
+        */
+        console.log(this.layerstrees[0].tree)
+        //@TODO
+        this.$emit('save-map-theme');
       }
     },
     watch: {
@@ -100,6 +160,12 @@
         handler(map_theme) {
           this.$emit('change-map-theme', map_theme);
         }
+      },
+      'themeName'(name) {
+        this.canSave = name ? name.trim() : false;
+      },
+      showSaveSection() {
+        this.themeName = null;
       }
     }
   }
