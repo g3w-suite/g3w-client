79c61551deec4d2ec4ff338e2158bc606d9a153d
volterra79 <boccacci.francesco@gmail.com>
2024-01-22 14:35:48 +0100

    Fix refactoring. Missing Base geometry types check (Point, LineString, Polygon)

--- src/utils/getDefaultLayerStyle.js
+++ src/utils/getDefaultLayerStyle.js
@@ -1,69 +1,38 @@
-import { GEOMETRY_TYPES } from 'app/constant';
+import { GEOMETRY_TYPES }        from 'app/constant';
+import { isPointGeometryType }   from "./isPointGeometryType";
+import { isLineGeometryType }    from "./isLineGeometryType";
+import { isPolygonGeometryType } from "./isPolygonGeometryType";
 
 export function getDefaultLayerStyle(geometryType, options = {}) {
 
   const { color } = options;
 
-  switch (geometryType) {
-
-    // LineString
-    case GEOMETRY_TYPES.LINESTRINGZ:
-    case GEOMETRY_TYPES.LINESTRINGM:
-    case GEOMETRY_TYPES.LINESTRINGZM:
-    case GEOMETRY_TYPES.LINESTRING25D:
-    case GEOMETRY_TYPES.LINE:
-    case GEOMETRY_TYPES.LINEZ:
-    case GEOMETRY_TYPES.LINEM:
-    case GEOMETRY_TYPES.LINEZM:
-    case GEOMETRY_TYPES.LINE25D:
-    // MultiLineString
-    case GEOMETRY_TYPES.MULTILINESTRINGZ:
-    case GEOMETRY_TYPES.MULTILINESTRINGM:
-    case GEOMETRY_TYPES.MULTILINESTRINGZM:
-    case GEOMETRY_TYPES.MULTILINESTRING25D:
-    case GEOMETRY_TYPES.MULTILINE:
-    case GEOMETRY_TYPES.MULTILINEZ:
-    case GEOMETRY_TYPES.MULTILINEM:
-    case GEOMETRY_TYPES.MULTILINEZM:
-    case GEOMETRY_TYPES.MULTILINE25D:
-      return new ol.style.Style({
-        stroke: new ol.style.Stroke({ color, width: 3 }),
-      });
-
-    // Point
-    case GEOMETRY_TYPES.POINTZ:
-    case GEOMETRY_TYPES.POINTM:
-    case GEOMETRY_TYPES.POINTZM:
-    case GEOMETRY_TYPES.POINT25D:
-    // MultiPoint
-    case GEOMETRY_TYPES.MULTIPOINTZ:
-    case GEOMETRY_TYPES.MULTIPOINTM:
-    case GEOMETRY_TYPES.MULTIPOINTZM:
-    case GEOMETRY_TYPES.MULTIPOINT25D:
-      return new ol.style.Style({
-        image: new ol.style.Circle({
-          fill: new ol.style.Fill({ color }),
-          stroke: new ol.style.Stroke({ color, width: 1 }),
-          radius: 5,
-        })
-      });
-
-    // Polygon
-    case GEOMETRY_TYPES.POLYGONZ:
-    case GEOMETRY_TYPES.POLYGONM:
-    case GEOMETRY_TYPES.POLYGONZM:
-    case GEOMETRY_TYPES.POLYGON25D:
-    // MultiPolygon
-    case GEOMETRY_TYPES.MULTIPOLYGONZ:
-    case GEOMETRY_TYPES.MULTIPOLYGONM:
-    case GEOMETRY_TYPES.MULTIPOLYGONZM:
-    case GEOMETRY_TYPES.MULTIPOLYGON25D:
-      return new ol.style.Style({
-        fill: new ol.style.Fill({ color: 'rgba(255,255,255,0.5)' }),
-        stroke: new ol.style.Stroke({ color, width: 3 }),
+  //Point geometry type
+  if (isPointGeometryType(geometryType)) {
+    return new ol.style.Style({
+      image: new ol.style.Circle({
+        fill: new ol.style.Fill({ color }),
+        stroke: new ol.style.Stroke({ color, width: 1 }),
+        radius: 5,
       })
-
-    default:
-      console.warn('invalid geometry type: ', geometryType);
+    });
   }
+
+  //Line geometry type
+  if (isLineGeometryType(geometryType)) {
+    return new ol.style.Style({
+      stroke: new ol.style.Stroke({ color, width: 3 }),
+    });
+  }
+
+  //Polygon geometry type
+  if (isPolygonGeometryType(geometryType)) {
+    return new ol.style.Style({
+      fill: new ol.style.Fill({ color: 'rgba(255,255,255,0.5)' }),
+      stroke: new ol.style.Stroke({ color, width: 3 }),
+    })
+  }
+
+  console.warn('invalid geometry type: ', geometryType);
+
 }
