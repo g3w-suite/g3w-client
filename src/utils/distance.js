89ec540b3ac077e5603ba3464939785453b64e06
volterra79 <boccacci.francesco@gmail.com>
2024-01-22 12:39:58 +0100

    Missing import squaredDistance

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,6 +1,8 @@
+import { squaredDistance } from "utils/squaredDistance";
+
 /**
  * core/geometry/geom::distance@v3.4
  */
 export function distance(c1, c2) {
-  return Math.sqrt(geom.squaredDistance(c1, c2));
-};
+  return Math.sqrt(squaredDistance(c1, c2));
+}
