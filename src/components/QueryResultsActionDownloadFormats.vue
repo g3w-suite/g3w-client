b1b561558fb725b8f31f081ef5d653448f5f9baa
volterra79 <boccacci.francesco@gmail.com>
2024-01-29 11:26:42 +0100

    Clean code

--- src/components/QueryResultsActionDownloadFormats.vue
+++ src/components/QueryResultsActionDownloadFormats.vue
@@ -4,15 +4,36 @@
 -->
 
 <template>
-  <div class="g3w-download-formats-content" style="width: 100%; display: flex" @click.prevent.stop="">
-    <select  style="flex-grow: 1" v-select2="'download_format'" :search="false" class="form-control">
-      <option v-for="download in config.downloads" :key="download.id" v-download :value="download.format">
-        <span style="font-weight: bold">{{download.format}}</span>
+  <div
+    class="g3w-download-formats-content"
+    style="width: 100%; display: flex;"
+    @click.prevent.stop=""
+  >
+    <select
+      style="flex-grow: 1;"
+      v-select2="'download_format'"
+      :search="false"
+      class="form-control"
+    >
+      <option
+        v-for="download in config.downloads"
+        :key="download.id"
+        v-download
+        :value="download.format"
+      >
+        <span style="font-weight: bold;">
+          {{download.format}}
+        </span>
       </option>
     </select>
-    <button style="border-radius: 0 3px 3px 0;" class="btn skin-button" @click.stop=download v-download>
-      <span :class="g3wtemplate.getFontClass('download')">
-      </span>
+
+    <button
+      style="border-radius: 0 3px 3px 0;"
+      class="btn skin-button"
+      @click.stop=download
+      v-download
+    >
+      <span :class="g3wtemplate.getFontClass('download')"></span>
     </button>
   </div>
 </template>
@@ -23,10 +44,9 @@ export default {
   /** @since 3.8.7 */
   name: "downloadformats",
 
-  data(){
-    const download_format = this.config.downloads[0].format;
+  data() {
     return {
-      download_format
+      download_format: this.config.downloads[0].format
     }
   },
   props: {
@@ -45,16 +65,15 @@ export default {
     },
   },
   methods: {
-    async download(){
+    async download() {
       try {
         const download = this.config.downloads.find(action => action.format === this.download_format);
         await download.cbk(this.layer, this.feature ? this.feature : this.layer.features, download, this.featureIndex);
       }
-      catch(err){}
+      catch(err) {
+        console.warn(err);
+      }
     }
   }
 }
 </script>
-
-<style scoped>
-</style>
