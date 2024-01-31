82e1f1a5751a4eab131c280419ec2e101eb057d4
volterra79 <boccacci.francesco@gmail.com>
2024-01-31 16:33:57 +0100

    Clean code

--- src/components/CatalogChangeMapThemes.vue
+++ src/components/CatalogChangeMapThemes.vue
@@ -17,44 +17,42 @@
       >
         <section style="display: flex;  flex-wrap: wrap; align-items: center; padding: 5px;">
           <i
-                  style="padding: 3px;"
-                  :class="g3wtemplate.getFontClass('caret-down')">
+            style="padding: 3px;"
+            :class="g3wtemplate.getFontClass('caret-down')">
           </i>
           <i
-                  style="padding: 0 0 0 4px;"
-                  :class="g3wtemplate.getFontClass('eye')">
+            style="padding: 0 0 0 4px;"
+            :class="g3wtemplate.getFontClass('eye')">
           </i>
           <!-- Text of current theme -->
           <span
-                  v-if="current_map_theme"
-                  class="treeview-label g3w-long-text"
-                  style="overflow: hidden; white-space: normal;text-overflow: ellipsis;"
+            v-if="current_map_theme"
+            class="treeview-label g3w-long-text"
+            style="overflow: hidden; white-space: normal;text-overflow: ellipsis;"
           >
-      <span
+            <span
               style="color: #cccccc !important;"
               v-t:pre="'sdk.catalog.current_map_theme_prefix'"
-      >:</span>
-      <span
+            >:</span>
+            <span
               class="skin-color"
               style="font-size: 1.1em;">
-          {{ current_map_theme }}
-      </span>
-    </span>
+                {{ current_map_theme }}
+            </span>
+          </span>
           <span
-                  v-else
-                  class="treeview-label"
-                  style="color: #cccccc !important; font-weight: bold"
+            v-else
+            class="treeview-label"
+            style="color: #cccccc !important; font-weight: bold"
           >
-      <span v-t="'sdk.catalog.choose_map_theme'"></span>
-    </span>
+            <span v-t="'sdk.catalog.choose_map_theme'"></span>
+          </span>
           <!-- Save theme button -->
           <span
             @click.stop="showSaveSection = !showSaveSection"
             :class="g3wtemplate.getFontClass('save')"
             class="action sidebar-button sidebar-button-icon skin-color"
-            style="margin-left: auto; font-size: 1.2em; padding: 5px;"
-          >
-      </span>
+            style="margin-left: auto; font-size: 1.2em; padding: 5px;"></span>
         </section>
 
         <!-- SAVE MAP THEME SECTION -->
@@ -73,15 +71,21 @@
             margin-top: 5px;"
         >
           <div class="form-group">
-            <label for="save-map-theme-input" class="skin-color">Nome</label>
-            <input id="save-map-theme-input" style="margin-bottom: 0" v-model="themeName" class="form-control">
+            <label
+              for="save-map-theme-input"
+              class="skin-color">Nome</label>
+            <input
+              id="save-map-theme-input"
+              style="margin-bottom: 0"
+              v-model="themeName"
+              class="form-control">
           </div>
           <button
             @click.stop="save"
             v-disabled="!canSave"
             style="height: 50px; font-weight: bold"
-            class="btn skin-background-color" v-t="'save'">
-          </button>
+            class="btn skin-background-color"
+            v-t="'save'"></button>
         </section>
       </a>
 
@@ -92,7 +96,10 @@
         :style="{display: collapsed ? 'none' : 'block'}"
       >
         <li style="padding: 5px 5px 5px 17px">
-          <div v-for="(map_theme, index) in map_themes" :key="map_theme.theme">
+          <div
+            v-for="(map_theme, index) in map_themes"
+            :key="map_theme.theme"
+          >
             <input
               type="radio"
               name="radio"
@@ -124,7 +131,7 @@
    * group (Group) : keys [checked, expanded, mutually-exclusive, name, nodes]
    */
   const LAYERSTREES_KEYS = {
-    node: ['id', 'name', 'showfeaturecount', 'visible'],
+    node:  ['id', 'name', 'showfeaturecount', 'visible'],
     group: ['name', 'checked', 'expanded', 'mutually-exclusive', 'nodes']
   }
 
@@ -140,14 +147,13 @@
       }
     },
     data() {
-      const collapsed = 'collapsed' === ProjectsRegistry.getCurrentProject().state.toc_themes_init_status ;
       const current_map_theme = this.map_themes.find(map_theme => map_theme.default);
       return {
         current_map_theme: current_map_theme ? current_map_theme.theme : null,
-        collapsed,
-        canSave: false, /** @since 3.10.0 */
-        themeName: null, /** @since 3.10.0 */
-        showSaveSection: false /** @since 3.10.0 */
+        collapsed:         'collapsed' === ProjectsRegistry.getCurrentProject().state.toc_themes_init_status,
+        canSave:           false, /** @since 3.10.0 */
+        themeName:         null, /** @since 3.10.0 */
+        showSaveSection:   false /** @since 3.10.0 */
       }
     },
     methods: {
