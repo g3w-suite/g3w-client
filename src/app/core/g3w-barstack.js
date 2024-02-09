2ca5435526ebbfebad972b885b997f22e1c0a5e5
Raruto <Raruto@users.noreply.github.com>
2024-02-09 10:36:17 +0100

    convert Barstack to ES6 class

--- src/app/core/g3w-barstack.js
+++ src/app/core/g3w-barstack.js
@@ -6,7 +6,7 @@
 import Panel     from 'core/g3w-panel';
 import Component from 'core/g3w-component';
 
-const { resolve, inherit } = require('utils');
+const { resolve } = require('utils');
 const G3WObject = require('core/g3wobject');
 
 /**
@@ -15,160 +15,173 @@ const G3WObject = require('core/g3wobject');
  * ORIGINAL SOURCE src/app/gui/utils/barstack.js@v3.9.3
  * 
  */
-export function BarStack() {
-  this._parent = null;
-  // barstack state. It store the panels array
-  this.state = {
-    contentsdata: []
+export class BarStack extends G3WObject {
+
+  constructor() {
+
+    super();
+
+    /** identify the DOM element where insert the component/panel  */
+    this._parent = null;
+
+    /** barstack state. It store the panels array */
+    this.state = {
+      contentsdata: []
+    }
+
   }
-}
 
-inherit(BarStack, G3WObject);
+  /**
+   * push componenet on top of parent
+   */
+  push(content, options={}) {
+    this._parent = options.parent;
+    return this._mount(content, options);
+  }
 
-const proto = BarStack.prototype;
-
-// push componenet on top of parent
-proto.push = function(content, options={}) {
-  // parent identify the DOM element where insert (append o meno) the component/panel
-  this._parent = options.parent;
-  // call barstack mount method
-  return this._mount(content, options);
-};
-
-// remove last component from stack
-proto.pop = function() {
-  const d = $.Deferred();
-  if (this.state.contentsdata.length) {
-    const content = this.state.contentsdata.slice(-1)[0].content;
-    this._unmount(content).then(() => {
-      const content = this.state.contentsdata.pop();
-      d.resolve(content)
-    })
-  } else d.resolve();
-  return d.promise();
-};
-
-// clear all stack
-proto.clear = function() {
-  const d = $.Deferred();
-  if (this.state.contentsdata.length) {
-    let unmountRequests = [];
-    this.state.contentsdata.forEach((data) => {
-      unmountRequests.push(this._unmount(data.content));
-    });
-    $.when(unmountRequests).then(() => {
-      this.state.contentsdata.splice(0, this.state.contentsdata.length);
+  /**
+   * remove last component from stack
+   */
+  pop() {
+    const d = $.Deferred();
+    const data = this.state.contentsdata;
+    if (data.length) {
+      this._unmount(data.slice(-1)[0].content).then(() => { d.resolve(data.pop()) });
+    } else {
       d.resolve();
-    });
+    }
+    return d.promise();
   }
-  else d.resolve();
-  return d.promise();
-};
 
-proto.getContentData = function() {
-  return this.state.contentsdata
-};
-
-proto.getCurrentContentData = function() {
-  return this.state.contentsdata[this.state.contentsdata.length - 1];
-};
-
-proto.getPreviousContentData = function() {
-  return this.state.contentsdata[this.state.contentsdata.length - 2];
-};
-
-// mount component
-proto._mount = function(content, options) {
-  // check the type of content:
-  // JQuery type
-  if (content instanceof jQuery) return this._setJqueryContent(content);
-  //String
-  else if (_.isString(content)) {
-    let jqueryEl = $(content);
-    if (!jqueryEl.length) jqueryEl = $('<div>'+content+'</div>');
-    return this._setJqueryContent(jqueryEl);
+  /**
+   * clear all stack 
+   */
+  clear() {
+    const d = $.Deferred();
+    const data = this.state.contentsdata;
+    if (data.length) {
+      let reqs = [];
+      data.forEach((data) => reqs.push(this._unmount(data.content)));
+      $.when(reqs).then(() => { data.splice(0, data.length); d.resolve(); });
+    } else {
+      d.resolve();
+    }
+    return d.promise();
   }
-  // Vue
-  else if (content.mount && typeof content.mount == 'function') {
-    this._checkDuplicateVueContent(content); // if already exist it removed before based on id
-    return this._setVueContent(content,options)
+
+  getContentData() {
+    return this.state.contentsdata;
   }
-  // DOM
-  else return this._setDOMContent(content);
-};
 
-// JQuery append jQuery component
-proto._setJqueryContent = function(content, options) {
-  $(this._parent).append(content);
-  this.state.contentsdata.push({
-    content,
-    options
-  });
-  return resolve();
-};
-
-//Append DOM element
-proto._setDOMContent = function(content, options) {
-  this._parent.appendChild(content);
-  this.state.contentsdata.push({
-    content,
-    options
-  });
-  return resolve();
-};
-
-// Mount component to parent
-proto._setVueContent = function(content, options={}) {
-  const d = $.Deferred();
-  const append = options.append || false;
-  content.mount(this._parent, append)
-  .then(() => {
-    $(this._parent).localize();
-    // Insert the content into the array with the following attributes:
-    // content: component object
-    // options: es. title, perc etc ...
-    this.state.contentsdata.push({
-      content,
-      options
-    });
-    d.resolve(content);
-  });
-  return d.promise();
-};
-
-// Check duplicate Vue Content
-proto._checkDuplicateVueContent = function(content) {
-  let idxToRemove = null;
-  const id = content.getId();
-  this.state.contentsdata.forEach((data, idx) => {
-    if (data.content.getId && (data.content.getId() == id)) idxToRemove = idx;
-  });
-  if (!_.isNull(idxToRemove)) {
-    const data = this.state.contentsdata[idxToRemove];
-    data.content.unmount()
-      .then(() => this.state.contentsdata.splice(idxToRemove,1));
+  getCurrentContentData() {
+    return this.state.contentsdata[this.state.contentsdata.length - 1];
   }
-};
 
-// unmount component
-proto._unmount = function(content) {
-  const d = $.Deferred();
-  if (content instanceof Component || content instanceof Panel) {
-    content.unmount()
-    .then(() => d.resolve());
+  getPreviousContentData() {
+    return this.state.contentsdata[this.state.contentsdata.length - 2];
   }
-  else {
-    $(this._parent).empty();
-    d.resolve();
+
+  /**
+   * mount component
+   */
+  _mount(content, options) {
+    // check the type of content:
+
+    // JQuery type
+    if (content instanceof jQuery) {
+      return this._setJqueryContent(content);
+    }
+
+    // String
+    if (_.isString(content)) {
+      let el = $(content);
+      if (!el.length) el = $('<div>' + content + '</div>');
+      return this._setJqueryContent(el);
+    }
+
+    // Vue
+    if (content.mount && 'function' === typeof content.mount) {
+      this._checkDuplicateVueContent(content); // if already exist it removed before based on id
+      return this._setVueContent(content,options)
+    }
+
+    // DOM
+    return this._setDOMContent(content);
   }
-  return d.promise();
-};
 
-proto.forEach = function(cbk) {
-  this.state.contentsdata.forEach(data => cbk(data.content));
-};
+  /**
+   * append jQuery component 
+   */
+  _setJqueryContent(content, options) {
+    $(this._parent).append(content);
+    this.state.contentsdata.push({ content, options });
+    return resolve();
+  }
 
-// Get lenght / numbero of element stored in stack
-proto.getLength = function() {
-  return this.state.contentsdata.length;
-};
+  /**
+   * Append DOM element 
+   */
+  _setDOMContent(content, options) {
+    this._parent.appendChild(content);
+    this.state.contentsdata.push({ content, options });
+    return resolve();
+  }
+
+  /**
+   * Mount component to parent 
+   */
+  _setVueContent(content, options={}) {
+    const d = $.Deferred();
+    const append = options.append || false;
+    content
+      .mount(this._parent, append)
+      .then(() => {
+        $(this._parent).localize();
+        // Insert the content into the array with the following attributes:
+        // content: component object
+        // options: es. title, perc etc ...
+        this.state.contentsdata.push({ content, options });
+        d.resolve(content);
+      });
+    return d.promise();
+  }
+
+  /**
+   * Check duplicate Vue Content 
+   */
+  _checkDuplicateVueContent(content) {
+    let idxToRemove = null;
+    const id = content.getId();
+    const data = this.state.contentsdata;
+    data.forEach((d, i) => { if (d.content.getId && (d.content.getId() == id)) idxToRemove = i; });
+    if (!_.isNull(idxToRemove)) {
+      data[idxToRemove].content.unmount().then(() => data.splice(idxToRemove,1));
+    }
+  }
+
+  /**
+   * unmount component
+   */
+  _unmount(content) {
+    const d = $.Deferred();
+    if (content instanceof Component || content instanceof Panel) {
+      content.unmount().then(() => d.resolve());
+    } else {
+      $(this._parent).empty();
+      d.resolve();
+    }
+    return d.promise();
+  }
+
+  forEach(cbk) {
+    this.state.contentsdata.forEach(d => cbk(d.content));
+  }
+
+  /**
+   * @returns number of elements stored in stack
+   */
+  getLength() {
+    return this.state.contentsdata.length;
+  }
+
+}
