91ce70bf9e2e21ab2fcc08f7856ab36ca4985728
Raruto <Raruto@users.noreply.github.com>
2024-02-22 15:50:59 +0100

    refactor `FloatbarService` and `SidebarService` services

--- src/services/floatbar.js
+++ src/services/floatbar.js
@@ -6,60 +6,68 @@
 import GUI          from 'services/gui';
 import { BarStack } from 'core/g3w-barstack';
 
-function FloatbarService(){
-  this.stack = new BarStack();
-  this.init = function(layout){
-    this.layout = layout;
-    this.sidebarEl = $(this.layout.options.controlSidebarOptions.selector);
-    this._zindex = this.sidebarEl.css("z-index");
+const service = {
+
+  stack: new BarStack(),
+
+  init(layout) {
+    this.layout        = layout;
+    this.sidebarEl     = $(this.layout.options.controlSidebarOptions.selector);
+    this._zindex       = this.sidebarEl.css("z-index");
     this._modalOverlay = null;
-    this._modal = false;
-    this._isopen = false;
-  };
+    this._modal        = false;
+    this._isopen       = false;
+  },
 
-  this.isOpen = function() {
+  isOpen() {
     return this._isopen;
-  };
+  },
 
-  this.open = function() {
+  open() {
     this.layout.floatBar.open(this.sidebarEl,true);
     this._isopen = true;
-  };
+  },
 
-  this.close = function() {
+  close() {
     this.layout.floatBar.close(this.sidebarEl,true);
     this._isopen = false;
-  };
+  },
 
-  this.showPanel = function(panel, options){
-    options = options || {};
-    const append = options.append || false;
-    const modal = options.modal || false;
-    options.parent = "#g3w-floatbarpanel-placeholder";
-    this.stack.push(panel, options);
-    if (!this._isopen) this.open();
-  };
-
-  this.closePanel = function(panel){
-    if (panel) this.stack.remove(panel);
-    else this.stack.pop();
-    if (!this.stack.getLength()) {
-      if (this._modal){
-        GUI.setModal(false);
-        this.close();
-        $('.control-sidebar-bg').toggleClass('control-sidebar-bg-shadow');
-        this.sidebarEl.css("z-index","");
-        this.sidebarEl.css("padding-top","50px");
-        $('.control-sidebar-bg').css("z-index","");
-        this._modal = false;
-      }
-      else this.close();
+  showPanel(panel, opts) {
+    opts        = opts || {};
+    opts.parent = "#g3w-floatbarpanel-placeholder";
+    this.stack.push(panel, opts);
+    if (!this._isopen) {
+      this.open();
     }
-  };
+  },
 
-  this.hidePanel = function(){
+  closePanel(panel) {
+    if (panel) {
+      this.stack.remove(panel);
+    } else {
+      this.stack.pop();
+    }
+
+    const stack_len = this.stack.getLength();
+
+    if (!stack_len && this._modal) {
+      GUI.setModal(false);
+      this.close();
+      $('.control-sidebar-bg').toggleClass('control-sidebar-bg-shadow');
+      this.sidebarEl.css("z-index","");
+      this.sidebarEl.css("padding-top","50px");
+      $('.control-sidebar-bg').css("z-index","");
+      this._modal = false; 
+    } else if (!stack_len && !this._modal) {
+      this.close();
+    }
+  },
+
+  hidePanel() {
     this.close();
-  };
-}
+  },
 
-export default new FloatbarService();
+};
+
+export default service;
--- src/services/sidebar.js
+++ src/services/sidebar.js
@@ -14,162 +14,198 @@ const G3WObject         = require('core/g3wobject');
  */
 const SidebarItem = Vue.extend(SidebarItemComponent);
 
-function SidebarService() {
-  //set sidebar stack
-  this.stack = new BarStack();
-  // set setter for close sidebarpanel to catch event
-  // of closing panel of the sidebar
-  this.setters = {
-    closeSidebarPanel() {},
-    openCloseItem(bool) {}
-  };
-  //service state
-  this.state = {
-    components: [],
-    gui: {
-      title: ''
-    },
-    disabled: false
-  };
-  //inizialize method
-  this.init = function(layout) {
+class SidebarService extends G3WObject {
+
+  constructor() {
+    super();
+
+    // sidebar stack
+    this.stack = new BarStack();
+
+    // service state
+    this.state = {
+      components: [],
+      gui: {
+        title: ''
+      },
+      disabled: false
+    };
+
+    /**
+     * setter for close sidebarpanel to catch event
+     * of closing panel of the sidebar
+     */
+    this.setters = {
+      closeSidebarPanel() {},
+      openCloseItem(bool) {}
+    };
+
+  }
+
+  /**
+   * init method
+   */
+  init(layout) {
     this.layout = layout;
-  };
-  // add component to sidebar
-  this.addComponents = function(components, options={}) {
-    //for each component of the sidebar it is call addComponent method
-    components.forEach(component => this.addComponent(component, options));
+  }
+
+  /**
+   * add component to sidebar, internally call `addComponent`
+   * method on each component of the sidebar 
+   */
+  addComponents(components, options={}) {
+    components.forEach(c => this.addComponent(c, options));
     return true;
   };
-  // add each component to the sidebar
-  // add also position insiede the sidebar
-  this.addComponent = function(component, options={}) {
-    const {position, before=true, info} = options;
-    if (isMobile.any && !component.mobile) {
+
+  /**
+   * add component to the sidebar and set position inside the sidebar
+   */
+  addComponent(comp, opts={}) {
+
+    if (isMobile.any && !comp.mobile) {
       return false
     }
-    const sidebarItem = new SidebarItem({
-      service: this,
-      info,
-      component
-    });
-    sidebarItem.title = component.title || sidebarItem.title;
-    sidebarItem.info = component.info || sidebarItem.info;
-    sidebarItem.actions = component.actions || sidebarItem.actions;
-    sidebarItem.open = component.state.open; //(component.open === undefined) ? sidebarItem.open : component.open;
-    sidebarItem.icon = component.icon || sidebarItem.icon;
-    sidebarItem.iconColor = component.iconColor;
-    sidebarItem.state = component.state || true;
-    sidebarItem.collapsible = (typeof component.collapsible === 'boolean') ? component.collapsible : true;
-    sidebarItem.isolate = (typeof component.isolate === 'boolean') ? component.isolate: false;
-    //append component to  g3w-sidebarcomponents (template sidebar.html)
-    const DOMComponent = sidebarItem.$mount().$el;
-    this.state.components.push(component);
-    const isPanelSidebarShow = $('.g3w-sidebarpanel').is(':visible');
-    const sidebarcomponetsdomid = `#g3w-sidebarcomponents${isPanelSidebarShow ? ':hidden': ''}`;
-    const children = $(sidebarcomponetsdomid).children().filter(function(){
-      return this.style.display !== 'none'
-    });
-    const childrenLength = children.length;
-    if (position === null || position === undefined || position < 0 || position >= childrenLength) $(sidebarcomponetsdomid).append(DOMComponent);
-    else children.each((index, element) => {
-      const findElementIndexId = Number.isInteger(position) ? position === index : element.id === position;
-      findElementIndexId && $(DOMComponent)[`insert${before ? 'Before' : 'After'}`](element);
-    });
-    //mount componet to g3w-sidebarcomponent-placeholder (template sidebar-item.html);
-    component.mount("#g3w-sidebarcomponent-placeholder");
-    // check if componentonent has iniService method
-    component.initService && component.initService();
-    // add click handler
-    this.setComponentClickHandler(component);
-    return true;
-  };
 
-  this.setComponentClickHandler = function(component){
-    component.click = ({open=false}={}) => {
-      open = open || false;
-      $(component.getInternalComponent().$el).siblings('a').click();
-      component.setOpen(open);
+    opts.before      = undefined === opts.before ? true : opts.before;
+
+    const item       = new SidebarItem({ service: this, info: opts.info, component: comp });
+    item.title       = comp.title       || item.title;
+    item.info        = comp.info        || item.info;
+    item.actions     = comp.actions     || item.actions;
+    item.open        = comp.state.open; // (comp.open === undefined) ? item.open : comp.open;
+    item.icon        = comp.icon        || item.icon;
+    item.iconColor   = comp.iconColor;
+    item.state       = comp.state       || true;
+    item.collapsible = ('boolean' === typeof comp.collapsible) ? comp.collapsible : true;
+    item.isolate     = ('boolean' === typeof comp.isolate)     ? comp.isolate: false;
+
+    // append component to `g3w-sidebarcomponents`
+    const el = item.$mount().$el;
+    const id = `#g3w-sidebarcomponents${ $('.g3w-sidebarpanel').is(':visible') ? ':hidden': ''}`;
+
+    this.state.components.push(comp);
+
+    const children = $(id).children().filter(function() { return this.style.display !== 'none' });
+  
+    if (null === opts.position || undefined === opts.position || opts.position < 0 || opts.position >= children.length) {
+      $(id).append(el);
+    } else {
+      children.each((i, c) => {
+        const found = Number.isInteger(opts.position) ? opts.position === i : c.id === opts.position;
+        if (found) {
+          $(el)[`insert${before ? 'Before' : 'After'}`](c);
+        }
+      });
     }
-  };
 
-  // get component by id
-  this.getComponent = function(id) {
-    return this.state.components.find(component => component.getId() === id)
-  };
+    // mount component to `g3w-sidebarcomponent-placeholder`
+    comp.mount("#g3w-sidebarcomponent-placeholder");
 
-  // get all components
-  this.getComponents = function() {
+    if (comp.initService) {
+      comp.initService();
+    }
+
+    // add click handler
+    this.setComponentClickHandler(comp);
+
+    return true;
+  }
+
+  /**
+   * @param comp component
+   */
+  setComponentClickHandler(comp) {
+    comp.click = ({ open = false } = {}) => {
+      open = open || false;
+      $(comp.getInternalComponent().$el).siblings('a').click();
+      comp.setOpen(open);
+    };
+  }
+
+  /**
+   * get component by id
+   */
+  getComponent(id) {
+    return this.state.components.find(c => c.getId() === id)
+  }
+
+  /**
+   * get all components
+   */
+  getComponents() {
     return this.state.components;
-  };
+  }
 
   /**
    * close for the moment only conlapsbale
    */
-  this.closeOpenComponents = function(collapsible=true){
+  closeOpenComponents(collapsible=true) {
     this.getComponents().forEach(c => c.getOpen() && c.state.closewhenshowviewportcontent && c.collapsible && c.click({ open: false }))
-  };
+  }
 
-  this.reloadComponent = function(id) {
+  reloadComponent(id) {
     const component = this.getComponent(id);
     component && component.reload();
-  };
+  }
 
-  this.reloadComponents = function() {
+  reloadComponents() {
     // force close of the panel
     this.closePanel();
-    this.state.components.forEach(component => {
-      if (component.collapsible && component.state.open) component.click({open: false});
-      component.reload();
-    })
-  };
-  //remove component
-  this.removeComponent = function(component, options={}) {
-    const {position} = options;
-    this.state.components.forEach((sidebarComponent, index) => {
-      if (component === sidebarComponent) {
-        component.unmount();
-        this.state.components.splice(index, 1);
-        if (position !== undefined && Number.isInteger(position)) $('#g3w-sidebarcomponents').children(':visible')[position].remove();
-        else $('#g3w-sidebarcomponents').children(`#${component.id}`).remove();
+    this.state.components.forEach(c => { if (c.collapsible && c.state.open) { c.click({open: false}); } component.reload(); })
+  }
+
+  /**
+   * remove component 
+   */
+  removeComponent(comp, opts={}) {
+    this.state.components.forEach((c, i) => {
+      if (comp === c) {
+        comp.unmount();
+        this.state.components.splice(i, 1);
+        if (opts.position !== undefined && Number.isInteger(opts.position)) {
+          $('#g3w-sidebarcomponents').children(':visible')[opts.position].remove();
+        } else {
+          $('#g3w-sidebarcomponents').children(`#${comp.id}`).remove();
+        }
         return false;
       }
     })
-  };
-  // show panel on stack
-  this.showPanel = function(panel, options={}) {
-    return new Promise((resolve, reject) => {
-      this.state.gui.title = panel.title;
-      const parent =  "#g3w-sidebarpanel-placeholder";
-      this.stack.getCurrentContentData() && $(this.stack.getCurrentContentData().content.internalPanel.$el).hide();
-      this.stack.push(panel, {
-        parent,
-        ...options
-      }).then(content => resolve(content))
-    })
-  };
+  }
 
-  // close panel
-  this.closePanel = function() {
+  /**
+   * show panel on stack
+   */
+  async showPanel(panel, options={}) {
+    this.state.gui.title = panel.title;
+    const data = this.stack.getCurrentContentData();
+    if (data) {
+      $(data.content.internalPanel.$el).hide();
+    } 
+    return await this.stack.push(panel, { parent: '#g3w-sidebarpanel-placeholder', ...options });
+  }
+
+  /**
+   * close panel
+   */
+  closePanel() {
     this.closeSidebarPanel();
     this.stack.pop().then(content => {
       content = null;
-      if (this.stack.getCurrentContentData()) {
-        $(this.stack.getCurrentContentData().content.internalPanel.$el).show();
-        this.state.gui.title = this.stack.getCurrentContentData().content.title;
+      const data = this.stack.getCurrentContentData();
+      if (data) {
+        $(data.content.internalPanel.$el).show();
+        this.state.gui.title = data.content.title;
       }
     });
-  };
+  }
 
-  this.closeAllPanels = function(){
+  closeAllPanels() {
     this.state.gui.title = null;
     this.closeSidebarPanel();
     this.stack.clear();
-  };
+  }
 
-  base(this);
 }
-
-inherit(SidebarService, G3WObject);
 
 export default new SidebarService();
