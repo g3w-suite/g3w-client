41b6717982de72c7bf2b4b1f15ec2779d82241c2
Raruto <Raruto@users.noreply.github.com>
2024-02-26 16:28:21 +0100

    remove `modal` querybuilder

--- src/components/g3w-querybuilder.js
+++ src/components/g3w-querybuilder.js
@@ -7,25 +7,13 @@ import Panel        from 'core/g3w-panel';
 
 import * as vueComp from 'components/QueryBuilder.vue';
 
-const QueryBuilder = Vue.extend(vueComp);
-
 /**
  * ORIGINAL SOURCE: src/app/gui/querybuilder/querybuilderuifactory.js@v3.9.3
  */
 export default {
-  type: null,
-  show({ type='sidebar', options = {} } = {}) {
-    let QB; 
-    this.type = this.type === null ? type : this.type;
-    if ('modal' === this.type) {
-      QB = new QueryBuilder({ options });
-      GUI.showModalDialog({ title: 'Query Builder', message: QB.$mount().$el, className: 'modal-background-dark ' });
-    } else {
-      options.title = 'Query Builder';
-      options.internalPanel = new QueryBuilder(options); 
-      options.show = true;
-      QB = (new Panel(options)).getInternalPanel();
-    }
-    return QB;
-  }
+  show: ({ opts = {} } = {}) => (new Panel(Object.assign(opts, {
+      title: 'Query Builder',
+      show: true,
+      internalPanel: new (Vue.extend(vueComp))(opts),
+    }))).getInternalPanel(),
 };
