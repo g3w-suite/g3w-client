548099a0b10afcc6c7a53fb2b2ad25e3590c31b6
volterra79 <boccacci.francesco@gmail.com>
2024-03-06 12:25:07 +0100

    check mapproxy provider

--- src/app/core/layers/map/wmstlayer.js
+++ src/app/core/layers/map/wmstlayer.js
@@ -18,14 +18,18 @@ const proto = WMSTLayer.prototype;
 
 proto._makeOlLayer = function(withLayers) {
   const olLayer = new RasterLayers.TiledWMSLayer({
-    url:             this.layers[0] && this.layers[0].getWmsUrl ? this.layers[0].getWmsUrl() : this.config.url,
-    id:              this.config.id,
-    projection:      this.config.projection,
-    iframe_internal: this.iframe_internal,
-    layers:          (withLayers) ? this.layers.map(layer => layer.getWMSLayerName()) : this.layers,
-    cache_provider:  this.config.cache_provider, /** @since 3.10.0 **/
+    url:               'mapproxy' === this.config.cache_provider ? this.config.url : this.layers[0] && this.layers[0].getWmsUrl ? this.layers[0].getWmsUrl() : this.config.url,
+    id:                this.config.id,
+    projection:        this.config.projection,
+    iframe_internal:   this.iframe_internal,
+    layers:            (withLayers) ? this.layers.map(layer => layer.getWMSLayerName()) : this.layers,
+    cache_provider:    this.config.cache_provider, /** @since 3.10.0 **/
+    cache_type:        this.config.cache_type, /** @since 3.10.0  tms, wms**/
+    cache_layer:       this.config.cache_layer,
+    cache_extent:      this.config.cache_extent,
+    cache_grid:        this.config.cache_grid,
+    cache_grid_extent: this.config.cache_grid_extent,
   }, this.extraParams, this._method);
-
   olLayer.getSource().on('tileloadstart', () => this.emit('loadstart'));
   olLayer.getSource().on('tileloadend',   () => this.emit('loadend'));
   olLayer.getSource().on('tileloaderror', () => this.emit('loaderror'));
