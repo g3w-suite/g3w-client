26b00fe47fb5f937801d278fc68323152d0c5cfa
volterra79 <boccacci.francesco@gmail.com>
2024-03-07 17:28:46 +0100

    clean code

--- src/mixins/select.js
+++ src/mixins/select.js
@@ -11,11 +11,11 @@ export default {
       return getAppLanguage();
     },
     changeSelect(value) {
-      this.state.value = value === 'null' ? null : value;
+      this.state.value = 'null' === value ? null : value;
       this.change();
     },
     getValue(value) {
-      return value === null ? 'null' : value;
+      return null === value ? 'null' : value;
     },
     resetValues() {
       this.state.input.options.values.splice(0);
@@ -23,15 +23,18 @@ export default {
   },
   computed: {
     autocomplete() {
-      return this.state.input.type === 'select_autocomplete' && this.state.input.options.usecompleter;
+      return 'select_autocomplete' === this.state.input.type && this.state.input.options.usecompleter;
     },
 
   },
   watch:{
     async notvalid(value) {
       await this.$nextTick();
-      if (this.select2)
-        value ? this.select2.data('select2').$container.addClass("input-error-validation") : this.select2.data('select2').$container.removeClass("input-error-validation")
+      if (this.select2) {
+        value
+          ? this.select2.data('select2').$container.addClass("input-error-validation")
+          : this.select2.data('select2').$container.removeClass("input-error-validation")
+      }
     }
   }
 };
--- src/mixins/select2.js
+++ src/mixins/select2.js
@@ -4,22 +4,26 @@
  */
 
 import ApplicationState from 'store/application-state';
-import resizeMixin from 'mixins/resize';
+import resizeMixin      from 'mixins/resize';
 
 export default {
   mixins: [resizeMixin],
   methods: {
-    setValue(){
+    setValue() {
       this.select2.val(this.state.value).trigger('change');
     },
     resize() {
-      this.select2 && !ApplicationState.ismobile && this.select2.select2('close');
+      if (this.select2 && !ApplicationState.ismobile) {
+        this.select2.select2('close');
+      }
     }
   },
   beforeDestroy() {
     //destroy a select2  dom element
-    this.select2 && this.select2.select2('destroy');
-    // remove all event
+    if (this.select2) {
+      this.select2.select2('destroy');
+    }
+    // remove all events
     this.select2.off();
     this.select2 = null;
   }
