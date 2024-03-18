eda87753cc95e5729482b155f59511aee376bc66
Raruto <Raruto@users.noreply.github.com>
2024-03-18 11:23:03 +0100

    avoid `Promise.resolve().then(async`

--- src/utils/createInputsFormFromFilter.js
+++ src/utils/createInputsFormFromFilter.js
@@ -17,151 +17,151 @@ export async function createInputsFormFromFilter(state) {
   const inputdependance   = state.input.dependance;
   const inputdependencies = state.input.dependencies;
   const cachedependencies = state.input.cached_deps;
-  const filter            = state.filter || [];
-  const searchLayer       = state.search_layers[0];
-  const search_endpoint   = state.search_endpoint || searchLayer.getSearchEndPoint();
+  const search_endpoint   = state.search_endpoint || state.search_layers[0].getSearchEndPoint();
 
-  // set key value for select
-  const valuesToKeysValues = values => values.length ? ('Object' !== toRawType(values[0]) ? values.map(value => ({ key: value, value })) : values) : values;
-
-  for (let i = 0; i <= filter.length - 1; i++) {
-
+  (state.filter || []).forEach(async (d, i, a) => {
     const input = {
-      label:     filter[i].label,
-      attribute: filter[i].attribute,
-      type:      filter[i].input.type || 'textfield',
-      options:   { ...filter[i].input.options },
+      label:     d.label,
+      attribute: d.attribute,
+      type:      d.input.type || 'textfield',
+      options:   { ...d.input.options },
       value:     null,
-      operator:  filter[i].op,
-      logicop:   i === (filter.length - 1) ? null : filter[i].logicop,
-      id:        filter[i].id || getUniqueDomId(),
+      operator:  d.op,
+      logicop:   i === (a.length - 1) ? null : d.logicop,
+      id:        d.id || getUniqueDomId(),
       loading:   false,
       widget:    null,
     };
 
     // check if it has a dependence
-    const dep_strict             = undefined !== input.options.dependance_strict ? input.options.dependance_strict : false;
-    const dep                    = undefined !== input.options.dependance        ? input.options.dependance        : false;
-    const isInputSelectType      = ['selectfield', 'autocompletefield'].includes(input.type);
-    input.options.values         = undefined !== input.options.values            ? input.options.values            : [];
-    const { values }             = input.options;
+    const dep_strict     = undefined !== input.options.dependance_strict ? input.options.dependance_strict : false;
+    const dep            = undefined !== input.options.dependance        ? input.options.dependance        : false;
+    const is_select      = ['selectfield', 'autocompletefield'].includes(input.type);
+    input.options.values = undefined !== input.options.values            ? input.options.values            : [];
+    const { values }     = input.options;
 
     //In case of select input
     if ('selectfield' ===  input.type) {
       // Request to server value for a specific select field
-       Promise
-        .resolve()
-        .then(async () => {
-          // ensure setting values options to an empty array when undefined
-          input.loading = true;
+      // ensure setting values options to an empty array when undefined
+      input.loading = true;
 
-          let _values;
+      let _values;
 
+      try {
+
+        // in case of dependence load right now
+        if ((dep && dep_strict) || dep_strict) {
+          return;
+        }
+
+        // if defined layer_id dependence
+        if (input.options.layer_id) {
+          // get value relation values
           try {
-
-            // in case of dependence load right now
-            if ((dep && dep_strict) || dep_strict) {
-              return;
-            }
-
-            // if defined layer_id dependence
-            if (input.options.layer_id) {
-              //array of unique values
-              const uniqueValues = await getUniqueValuesFromField({
-                layers: state.search_layers,
-                field: input.attribute,
-                inputdependance,
-                cachedependencies,
-              });
-              const filter = createFilterFormInputs({
+            const {data = []} = await DataRouterService.getData('search:features', {
+              inputs: {
                 layer: CatalogLayersStoresRegistry.getLayerById(input.options.layer_id),
                 search_endpoint,
-                inputs: [{value: uniqueValues, attribute: input.options.value, logicop: "OR", operator: "eq"}]
-              });
-              // get value relation values
-              try {
-                const {data = []} = await DataRouterService.getData('search:features', {
-                  inputs: {
-                    layer: CatalogLayersStoresRegistry.getLayerById(input.options.layer_id),
-                    search_endpoint,
-                    filter,
-                    ordering: input.options.key
-                  },
-                  outputs: false
-                });
-                const values = [];
-                (data && data[0] && data[0].features || []).forEach(feature => {
-                  values.push({key: feature.get(input.options.key), value: feature.get(input.options.value)})
-                });
-                _values = values;
-              } catch (e) {
-                console.warn(e);
-                _values = [];
-              }
-              return;
-            }
-
-            // Relation reference
-            if (input.options.relation_reference) {
-              //call filter data with fformatter
-              const response = await searchLayer.getFilterData({fformatter: input.attribute});
-              //check response
-              if (response && response.result && response.data) {
-                input.options.values = response.data.map(([value, key]) => ({key, value}));
-              }
-            }
-
-            // return mapped values
-            if (input.options.values.length > 0) {
-              _values = input.options.values.filter(value => SEARCH_ALLVALUE !== value);
-              return;
-            }
-            _values = await getUniqueValuesFromField({
-              field: input.attribute,
-              layers: state.search_layers,
-              inputdependance,
-              cachedependencies,
-            })
+                filter: createFilterFormInputs({
+                  layer: CatalogLayersStoresRegistry.getLayerById(input.options.layer_id),
+                  search_endpoint,
+                  inputs: [{
+                    // array of unique values
+                    value: await getUniqueValuesFromField({
+                      layers: state.search_layers,
+                      field: input.attribute,
+                      inputdependance,
+                      cachedependencies,
+                    }),
+                    attribute: input.options.value,
+                    logicop: "OR",
+                    operator: "eq"
+                  }]
+                }),
+                ordering: input.options.key
+              },
+              outputs: false
+            });
+            _values = (data && data[0] && data[0].features || []).map(f => ({key: f.get(input.options.key), value: f.get(input.options.value)}));
           } catch (e) {
             console.warn(e);
-            values.length = 0;
-          } finally {
-            if (undefined !== _values) {
-              values.splice(0, values.length, ...valuesToKeysValues(_values));
-            }
-            input.loading = false;
-            if (values.length && SEARCH_ALLVALUE !== values[0].value) {
-              values.unshift({value: SEARCH_ALLVALUE});
-            } else {
-              values.push({value: SEARCH_ALLVALUE});
-            }
-            input.value = SEARCH_ALLVALUE;
+            _values = [];
           }
-        });
+          return;
+        }
+
+        // Relation reference
+        if (input.options.relation_reference) {
+          //call filter data with fformatter
+          const response = await state.search_layers[0].getFilterData({ fformatter: input.attribute });
+          //check response
+          if (response && response.result && response.data) {
+            input.options.values = response.data.map(([value, key]) => ({ key, value }));
+          }
+        }
+
+        // return mapped values
+        if (input.options.values.length > 0) {
+          _values = input.options.values.filter(v => SEARCH_ALLVALUE !== v);
+          return;
+        }
+
+        _values = await getUniqueValuesFromField({
+          field: input.attribute,
+          layers: state.search_layers,
+          inputdependance,
+          cachedependencies,
+        })
+      } catch (e) {
+        console.warn(e);
+        values.length = 0;
+      } finally {
+        // set key value for select
+        if (undefined !== _values) {
+          values.splice(0,
+            values.length,
+            _values.length
+              ? (
+                'Object' !== toRawType(_values[0])
+                  ? _values.map(value => ({ key: value, value }))
+                  : _values
+                )
+              : _values
+            );
+        }
+        if (values.length && SEARCH_ALLVALUE !== values[0].value) {
+          values.unshift({ value: SEARCH_ALLVALUE });
+        } else {
+          values.push({ value: SEARCH_ALLVALUE });
+        }
+        input.value   = SEARCH_ALLVALUE;
+        input.loading = false;
+      }
     }
 
     // there is a dependence
-    if (isInputSelectType && dep) {
-      inputdependance[input.attribute] = dep;              // set dependence of input
-      state.loading[dep]        = false;
-      input.options.disabled    = dep_strict;       // disabled for BACKCOMP
+    if (is_select && dep) {
+      inputdependance[input.attribute] = dep;        // set dependence of input
+      state.loading[dep]               = false;
+      input.options.disabled           = dep_strict; // disabled for BACKCOMP
       // set input dependencies
       inputdependencies[dep] = (undefined !== inputdependencies[dep] ? inputdependencies[dep] : []);
       inputdependencies[dep].push(input);
     }
 
     // set a widget type for fill dependency
-    if (isInputSelectType && dep && values.length > 0) {
+    if (is_select && dep && values.length > 0) {
       input.widget          = 'valuemap';
       input.options._values = [...values];
     }
 
-    //Set input widget
-    if (isInputSelectType && dep && !values.length && input.options.layer_id) {
+    // Set input widget
+    if (is_select && dep && !values.length && input.options.layer_id) {
       input.widget = 'valuerelation';
     }
 
     // add form inputs to list of search input
     state.forminputs.push(input);
-  }
+  });
 }
