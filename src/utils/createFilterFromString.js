93db858149a91f9f00f5b094f9cf11a4217ea2f8
Raruto <Raruto@users.noreply.github.com>
2024-03-20 13:25:36 +0100

    static `operators`

--- src/utils/createFilterFromString.js
+++ src/utils/createFilterFromString.js
@@ -2,12 +2,14 @@ import { FILTER_EXPRESSION_OPERATORS } from 'app/constant';
 
 const Filter     = require('core/layers/filter/filter');
 const Expression = require('core/layers/filter/expression');
+const operators  = Object.entries(FILTER_EXPRESSION_OPERATORS);
 
 export function createFilterFromString({
   layer,
   search_endpoint = 'ows',
   filter          = '',
 }) {
+
   switch (search_endpoint) {
 
     case 'ows':
@@ -17,18 +19,16 @@ export function createFilterFromString({
       break;
 
     case 'api':
-      filter = Object
-        .values(FILTER_EXPRESSION_OPERATORS)
-        .reduce((acc, op) => acc
+      filter = operators
+        .reduce((acc, [_, op]) => acc
           .replace(new RegExp(`\\s+${op}\\s+`, 'g'), `${op}`) // remove all blank space between operators
           .replace(new RegExp(`'${op}`, 'g'), `${op}`)        // leading single quote
           .replace(new RegExp(`${op}'`, 'g'), `${op}`)        // trailing single quote
         , filter)
         .replace(/'$/g, '')
         .replace(/"/g, '');
-      filter = Object
-        .entries(FILTER_EXPRESSION_OPERATORS)
-        .reduce((acc, [k, v]) => acc.replace(new RegExp(v, "g"), ['AND', 'OR'].includes(v) ? `|${k},` : `|${k}|`), filter)
+      filter = operators
+        .reduce((acc, [k, op]) => acc.replace(new RegExp(op, 'g'), ['AND', 'OR'].includes(op) ? `|${k},` : `|${k}|`), filter)
         // encode value
         .split('|')
         .map((v, i) => (0 === (i+1) % 3) ? encodeURIComponent(v) : v)
