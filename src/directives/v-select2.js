4e2ab4f5469c5ab6d53107dd128a3a7a42ea7e9e
Raruto <Raruto@users.noreply.github.com>
2024-05-03 16:05:30 +0200

    reduce conditionals nesting level: `v-select2.js`

--- src/directives/v-select2.js
+++ src/directives/v-select2.js
@@ -11,8 +11,10 @@ export default {
       multiple  = false,
       search    = true,
       select2_value,
-      indexItem, /** @since 3.9.1 */
-      createTag = false, /**@since v3.10.0 used to create a new value option dynamically */
+      /** @since 3.9.1 */
+      indexItem,
+      /**@since 3.10.0 whether to dynamically create a new "<option>" value */
+      createTag = false,
     } = vnode.data.attrs || {};
     const isArray = binding.value
       && Array.isArray(vnode.context[binding.value]) // check if is an array
@@ -24,51 +26,43 @@ export default {
         dropdownCssClass: 'skin-color',
         templateResult,
         templateSelection,
-        minimumResultsForSearch: !search ? -1 : undefined,
+        minimumResultsForSearch: search ? undefined : -1,
         createTag(params) {
           const value = params.term.trim();
-          if (!value) { return null; }
-          return {
+          return value ? {
             id:     value,
             text:   value,
             newTag: true // add additional value
-          }
+          } : null;
         },
       })
       .on('select2:select', (e) => {
-        if (binding.value) {
-          //get value
-          const value = e.params.data.id;
-          //check is can have multiple value
-          if (multiple && (
-            (isArray
-              ? vnode.context[binding.value][indexItem].value
-              : vnode.context[binding.value]
-            ).filter(d => value === d).length === 0)
-          ) {
-            (isArray
-              ? vnode.context[binding.value][indexItem].value
-              : vnode.context[binding.value]
-            ).push(value);
-          } else {
-            if (isArray) {
-              vnode.context[binding.value][indexItem].value = value;
-            } else {
-              //take in an account text binding value single world or object (e.s state.name)
-              const attrs = `${binding.value}`.split('.');
-              const last = attrs.pop();
-              (attrs.reduce((acc, a) => { acc = acc[a]; return acc; }, vnode.context))[last] = value;
-            }
-          }
+        if (!binding.value) {
+          return;
+        }
+        //get value
+        const value = e.params.data.id;
+        const arr = (isArray ? vnode.context[binding.value][indexItem].value : vnode.context[binding.value]);
+        // check is can have multiple value
+        if (multiple && arr.every(d => value !== d)) {
+          arr.push(value);
+        } else if (isArray) {
+          vnode.context[binding.value][indexItem].value = value;
+        } else {
+          // take in an account text binding value single world or object (eg. state.name)
+          const attrs = `${binding.value}`.split('.');
+          const last = attrs.pop();
+          (attrs.reduce((acc, a) => { acc = acc[a]; return acc; }, vnode.context))[last] = value;
         }
       })
       .on('select2:unselect', (e) => {
-        if (binding.value && multiple) {
-          if (isArray) {
-            vnode.context[binding.value][indexItem].value = vnode.context[binding.value][indexItem].value.filter(d => e.params.data.id !== d);
-          } else {
-            vnode.context[binding.value] = vnode.context[binding.value].filter(d => e.params.data.id !== d);
-          }
+        if (!binding.value || !multiple) {
+          return;
+        }
+        if (isArray) {
+          vnode.context[binding.value][indexItem].value = vnode.context[binding.value][indexItem].value.filter(d => e.params.data.id !== d);
+        } else {
+          vnode.context[binding.value] = vnode.context[binding.value].filter(d => e.params.data.id !== d);
         }
       });
       if (binding.value && select2_value) {
