44cf4b5e6c54c6b4174bbe1b687295c33e96de67
volterra79 <boccacci.francesco@gmail.com>
2024-06-27 15:12:53 +0200

    Fix https://github.com/g3w-suite/g3w-client/issues/642 in v3.8.x

--- src/utils/getDataForSearchInput.js
+++ src/utils/getDataForSearchInput.js
@@ -1,5 +1,4 @@
 import { SEARCH_ALLVALUE }            from 'app/constant';
-import { createSingleFieldParameter } from 'utils/createSingleFieldParameter';
 
 /**
  * @returns { Array } of unique values from field
@@ -7,32 +6,21 @@ import { createSingleFieldParameter } from 'utils/createSingleFieldParameter';
 export async function getDataForSearchInput({ state, field, suggest }) {
 
   try {
-
-    // check if a field has a dependance
-    const parent = state.forminputs.find(d => d.attribute === field);
-    let dep      = parent && parent.dependance;
-    const cached = dep && state.forminputs.some(d => dep === d.dependance && d.dvalues.length);
-
     // get unique value from each layers
-    let response = (
+    return (
       await Promise.allSettled(state.search_layers.map(l => l.getFilterData({
-          suggest,
-          fformatter: field,
-          ordering: field,
-          field: getDataForSearchInput.field({
-            state,
-            field: dep,
-            fields: cached && ![SEARCH_ALLVALUE, undefined].includes(parent.value)
-              ? [createSingleFieldParameter({ field: dep, value: parent.value, operator: state.forminputs.find(d =>  d.attribute === dep).operator }) ]
-              : [],
-          }),
-        })))
+        suggest,
+        fformatter: field,
+        ordering:   field,
+        field: getDataForSearchInput.field({
+          state,
+          field: (state.forminputs.find(i => i.attribute === field) || {}).dependance || field,
+        }),
+      })))
     )
       .filter(d => 'fulfilled' === d.status)
       .reduce((acc, d) => acc.concat(d.value.data || []), []) // uniques by fformatter
       .map(([value, key]) => ({ key, value }));
-
-    return response;
 
   } catch(e) { console.warn(e); }
 
@@ -43,21 +31,19 @@ export async function getDataForSearchInput({ state, field, suggest }) {
  * Traverse field dependecies
  */
 getDataForSearchInput.field = ({ state, field, fields = [] } = {}) => {
-  const parent = state.forminputs.find(d => d.attribute === field);
-  let dep      = parent && parent.dependance;
-  const cached = dep && state.forminputs.some(d => dep === d.dependance && d.dvalues.length);
+  field        = state.forminputs.find(i => i.attribute === field);
+  const parent = state.forminputs.find(i => i.attribute === field.dependance);
 
-  if (!cached || SEARCH_ALLVALUE === parent.value) {
+  if (!parent || SEARCH_ALLVALUE === parent.value) {
     return (fields || []).join() || undefined;
   }
-
   // get current field dependance
   // In case of some input dependency is not filled
   if (undefined !== parent.value) {
     // need to set to lower a case for api purpose
-    const { op, logicop } = state.forminputs.find(f =>  f.attribute === dep).operator;
-    fields.unshift(`${dep}|${op.toLowerCase()}|${encodeURI(parent.value)}|` + (fields.length ? logicop.toLowerCase() : ''));
+    const { operator, logicop } = parent;
+    fields.unshift(`${parent.attribute}|${operator.toLowerCase()}|${encodeURI(parent.value)}|` + (fields.length ? logicop : ''));
   }
 
-  return getDataForSearchInput.field({ state, fields, field: dep });
+  return getDataForSearchInput.field({ state, fields, field: parent.attribute });
 }
