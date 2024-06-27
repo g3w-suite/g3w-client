25776d0b479bbda566caaafd46e41aa1b53f75ca
volterra79 <boccacci.francesco@gmail.com>
2024-06-27 16:11:28 +0200

    style to mapcontrols

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,7 +18,8 @@
       <div
         ref        = "g3w-map-controls"
         class      = "g3w-map-controls"
-        style      = "display: flex"
+        style      = "display: flex;"
+        :style     = "{ right: state.mapControl.style.right }"
         v-disabled = "disableMapControls"
         :class     = "mapcontrolsalignement"
       ></div>
@@ -40,106 +41,106 @@
       <addlayer :service = "service" />
 
       <!-- @since 3.8.0   -->
-      <div class="g3w-map-controls-left-bottom"></div>
+      <div class = "g3w-map-controls-left-bottom"></div>
 
     </div>
 
     <!-- Footer (bottom part) where scale and other component can be set -->
-    <map-footer :service="service"/>
+    <map-footer :service = "service"/>
 
   </div>
 </template>
 
 <script>
-import AddLayerComponent from 'components/MapAddLayer.vue';
-import MapFooter         from 'components/MapFooter.vue';
+  import AddLayerComponent from 'components/MapAddLayer.vue';
+  import MapFooter         from 'components/MapFooter.vue';
 
-export default {
+  export default {
 
-  /** @since 3.8.6 */
-  name: 'g3w-map',
+    /** @since 3.8.6 */
+    name: 'g3w-map',
 
-  data() {
-    const { service } = this.$options;
-    return {
-      target:         this.$options.service.target,
-      maps_container: this.$options.service.maps_container,
-      hidemaps:       service.state.hidemaps,
-      map_info:       service.state.map_info,
-      service,
-    }
-  },
-  components: {
-    'addlayer': AddLayerComponent,
-    MapFooter
-  },
-  computed: {
-    mapcontrolsalignement() {
-      return this.service.state.mapcontrolsalignement;
+    data() {
+      const { service } = this.$options;
+      return {
+        target:         service.target,
+        maps_container: service.maps_container,
+        hidemaps:       service.state.hidemaps,
+        map_info:       service.state.map_info,
+        service,
+      }
     },
-    disableMapControls() {
-      return this.service.state.mapControl.disabled;
-    }
-  },
-  methods: {
-    showHideControls() {
-      this.service.getMapControls().forEach(c => "scaleline" !== c.type && c.control.showHide());
-    }
-  },
-  async mounted() {
-    this.crs = this.service.getCrs();
-    await this.$nextTick();
-    this.service.setMapControlsContainer($(this.$refs['g3w-map-controls']));
-    // listen of after addHideMap
-    this.service.onafter('addHideMap', async ({ratio, layers=[], mainview=false, switchable=false} = {}) => {
+    components: {
+      'addlayer': AddLayerComponent,
+      MapFooter
+    },
+    computed: {
+      mapcontrolsalignement() {
+        return this.service.state.mapcontrolsalignement;
+      },
+      disableMapControls() {
+        return this.service.state.mapControl.disabled;
+      }
+    },
+    methods: {
+      showHideControls() {
+        this.service.getMapControls().forEach(c => "scaleline" !== c.type && c.control.showHide());
+      }
+    },
+    async mounted() {
+      this.crs = this.service.getCrs();
       await this.$nextTick();
-      this.service._addHideMap({ratio, layers, mainview, switchable});
-    });
-  },
-  destroyed() {
-    this.service.clear();
-  }
-};
+      this.service.setMapControlsContainer($(this.$refs['g3w-map-controls']));
+      // listen of after addHideMap
+      this.service.onafter('addHideMap', async ({ ratio, layers = [], mainview = false, switchable = false} = {}) => {
+        await this.$nextTick();
+        this.service._addHideMap({ratio, layers, mainview, switchable});
+      });
+    },
+    destroyed() {
+      this.service.clear();
+    }
+  };
 </script>
 <style scoped>
-#marker {
-  width: 15px;
-  height: 15px;
-  border: 2px solid yellow;
-  border-radius: 10px;
-  background-color: yellow;
-  opacity: 0.8;
-}
-.g3w-map-controls-left-bottom {
-  position: absolute;
-  bottom: 75px;
-  left: 10px;
-  z-index: 1;
-}
-#g3w-map-info {
-  position: absolute;
-  top: 60px;
-  left: 5px;
-  font-weight: bold;
-  z-index: 100;
-  background: rgba(255,255,255, 0.6);
-  padding: 5px;
-  border-radius: 3px;
-}
-#g3w-maps {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
-.g3w-map {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-}
-.g3w-map.show {
-  display: block;
-}
-.g3w-map.hide {
-  display: none;
-} 
+  #marker {
+    width: 15px;
+    height: 15px;
+    border: 2px solid yellow;
+    border-radius: 10px;
+    background-color: yellow;
+    opacity: 0.8;
+  }
+  .g3w-map-controls-left-bottom {
+    position: absolute;
+    bottom: 75px;
+    left: 10px;
+    z-index: 1;
+  }
+  #g3w-map-info {
+    position: absolute;
+    top: 60px;
+    left: 5px;
+    font-weight: bold;
+    z-index: 100;
+    background: rgba(255,255,255, 0.6);
+    padding: 5px;
+    border-radius: 3px;
+  }
+  #g3w-maps {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  .g3w-map {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+  .g3w-map.show {
+    display: block;
+  }
+  .g3w-map.hide {
+    display: none;
+  }
 </style>
