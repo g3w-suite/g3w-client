ad1b07ca77cfe8b8762673cae2e80ffcd62b9b5e
Raruto <Raruto@users.noreply.github.com>
2024-07-08 09:05:27 +0200

    extract helper function: `_setMouseCursor`

--- src/app/g3w-ol/controls/querybboxcontrol.js
+++ src/app/g3w-ol/controls/querybboxcontrol.js
@@ -21,6 +21,14 @@ const condition = {
   }
 };
 
+function _setMouseCursor(map, toggled) {
+  if (toggled) {
+    setTimeout(() => map.getViewport().classList.add('ol-crosshair'));
+  } else {
+    map.getViewport().classList.remove('ol-crosshair');
+  }
+}
+
 module.exports = class QueryBBoxControl extends InteractionControl {
 
   constructor(options = {}) {
@@ -100,24 +108,9 @@ module.exports = class QueryBBoxControl extends InteractionControl {
   setMap(map) {
     InteractionControl.prototype.setMap.call(this, map);
 
-    /** @FIXME mouse cursor when editing constraint is enabled */
     // set mouse cursor (crosshair)
-    // this.on('toggled', ({ toggled }) => {
-    //   if (toggled) {
-    //     setTimeout(() => map.getViewport().classList.toggle('ol-crosshair', true));
-    //     return;
-    //   }
-    //   map.getViewport().classList.toggle('ol-crosshair', toggled);
-    // });
-
-    this._interaction.on('change:active', (e) => {
-      // https://openlayers.org/en/v5.3.0/apidoc/module-ol_Object.ObjectEvent.html to ge new value of the event
-      if (e.target.get(e.key)) {
-        setTimeout(() => map.getViewport().classList.add('ol-crosshair'));
-      } else {
-        map.getViewport().classList.remove('ol-crosshair');
-      }
-    });
+    this.on('toggled', ({ toggled }) => _setMouseCursor(map, toggled));
+    this._interaction.on('change:active', e => _setMouseCursor(map, e.target.get(e.key)));
 
     this._interaction.on('boxstart',        e => this._startCoordinate = e.coordinate);
     this._interaction.on('boxend', throttle(e => {
