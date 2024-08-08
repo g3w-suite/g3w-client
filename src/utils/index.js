839078c4b036340a53e08dd13c565957c2a09290
Raruto <Raruto@users.noreply.github.com>
2024-08-08 09:46:37 +0200

    remove unusued utils: `Math.round10`, `Math.floor10`, `Math.ceil10`, `String::hashCode`

--- src/utils/index.js
+++ src/utils/index.js
@@ -45,66 +45,6 @@ import { sortAlphabeticallyArray }           from './sortAlphabeticallyArray';
 import { sortNumericArray }                  from './sortNumericArray';
 import { sameOrigin }                        from './sameOrigin';
 
-/**
- * Decimal adjustment of a number.
- *
- * @param {String}  type  The type of adjustment.
- * @param {Number}  value The number.
- * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
- * @returns {Number} The adjusted value.
- */
-function decimalAdjust(type, value, exp) {
-  // If the exp is undefined or zero...
-  if (typeof exp === 'undefined' || +exp === 0) return Math[type](value);
-  value = +value;
-  exp = +exp;
-  // If the value is not a number or the exp is not an integer...
-  if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
-    return NaN;
-  }
-  // Shift
-  value = value.toString().split('e');
-  value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
-  // Shift back
-  value = value.toString().split('e');
-  return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
-}
-
-// Decimal round
-if (!Math.round10) {
-  /** @TODO deprecate (unused code + bad practice) */
-  Math.round10 = function(value, exp) {
-    return decimalAdjust('round', value, exp);
-  };
-}
-// Decimal floor
-if (!Math.floor10) {
-  /** @TODO deprecate (unused code + bad practice) */
-  Math.floor10 = function(value, exp) {
-    return decimalAdjust('floor', value, exp);
-  };
-}
-
-// Decimal ceil
-if (!Math.ceil10) {
-  /** @TODO deprecate (unused code + bad practice) */
-  Math.ceil10 = function(value, exp) {
-    return decimalAdjust('ceil', value, exp);
-  };
-}
-
-/** @TODO deprecate (unused code + bad practice) */
-String.prototype.hashCode = function() {
-  let hash = 0, i, chr, len;
-  if (this.length === 0) return hash;
-  for (i = 0, len = this.length; i < len; i++) {
-    chr   = this.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0;
-  }
-  return hash;
-};
-
 const utils = {
   getUniqueDomId,
   uniqueId: getUniqueDomId,
