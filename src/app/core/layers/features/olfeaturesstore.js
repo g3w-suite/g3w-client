4232aa3b7d0efdf89382e1a5d90517c56e9668a2
volterra79 <boccacci.francesco@gmail.com>
2024-08-12 16:29:08 +0200

    :recycle: Convert into Class

--- src/app/core/layers/features/olfeaturesstore.js
+++ src/app/core/layers/features/olfeaturesstore.js
@@ -1,92 +1,87 @@
-const { inherit, base } = require('utils');
 const FeaturesStore     = require('core/layers/features/featuresstore');
 
-// Storage of the feature in vector layer
-function OlFeaturesStore(options={}) {
-  base(this, options);
-  this._features = options.features || new ol.Collection([]);
-}
-
-inherit(OlFeaturesStore, FeaturesStore);
-
-const proto = OlFeaturesStore.prototype;
-
-proto.getLength = function() {
-  return this._features.getLength();
-};
-
-//overwrite
-proto.setFeatures = function(features = []) {
-  //remove features
-  this._features.clear();
-  //add new features
-  this.addFeatures(features);
-  this._features.dispatchEvent('change');
-};
-// overwrite
-proto.readFeatures = function() {
-  return this._features.getArray();
-};
-
-proto.getFeaturesCollection = function() {
-  return this._features;
-};
-
-proto.getFeatureById = function(featureId) {
-  return this._features.getArray().find(f => featureId == f.getId());
-};
-
-proto.getFeatureByUid = function(uid) {
-  return this._features.getArray().find(f => uid === f.getUid());
-};
-
-proto._addFeature = function(feature) {
-  this._features.push(feature);
-  // useful for ol.source.Vector
-  this._features.dispatchEvent('change')
-};
-
-//substitute the feature after modifying
-proto._updateFeature = function(feature) {
-  // set index at -1
-  let index = -1;
-  const featuresArray = this._features.getArray();
-  for (let i = 0; featuresArray.length; i++) {
-    const _feature = featuresArray[i];
-    if (_feature.getUid() === feature.getUid()) {
-      index = i;
-      break;
-    }
+module.exports = class OlFeaturesStore extends FeaturesStore {
+  constructor(opts = {}) {
+    super(opts);
+    this._features = opts.features || new ol.Collection([]);
   }
-  if (index >=0) {
-    this._features.removeAt(index);
-    this._features.insertAt(index, feature);
+
+  getLength() {
+    return this._features.getLength();
+  }
+
+  //overwrite
+  setFeatures(features = []) {
+    //remove features
+    this._features.clear();
+    //add new features
+    this.addFeatures(features);
+    this._features.dispatchEvent('change');
+  };
+// overwrite
+  readFeatures() {
+    return this._features.getArray();
+  };
+
+  getFeaturesCollection() {
+    return this._features;
+  }
+
+  getFeatureById(featureId) {
+    return this._features.getArray().find(f => featureId == f.getId());
+  }
+
+  getFeatureByUid(uid) {
+    return this._features.getArray().find(f => uid === f.getUid());
+  }
+
+  _addFeature(feature) {
+    this._features.push(feature);
+    // useful for ol.source.Vector
     this._features.dispatchEvent('change')
   }
-};
 
-// remove feature from store
-proto._removeFeature = function(feature) {
-  const featuresArray = this._features.getArray();
-  for (let i = 0; i < featuresArray.length; i++) {
-    const feat = featuresArray[i];
-    if (feature.getUid() === feat.getUid()) {
-      this._features.removeAt(i);
-      break;
+//substitute the feature after modifying
+  _updateFeature(feature) {
+    // set index at -1
+    let index = -1;
+    const featuresArray = this._features.getArray();
+    for (let i = 0; featuresArray.length; i++) {
+      const _feature = featuresArray[i];
+      if (_feature.getUid() === feature.getUid()) {
+        index = i;
+        break;
+      }
+    }
+    if (index >=0) {
+      this._features.removeAt(index);
+      this._features.insertAt(index, feature);
+      this._features.dispatchEvent('change')
     }
   }
-  this._features.dispatchEvent('change');
-};
 
-
-proto._clearFeatures = function() {
-  try {
-    this._features.clear();
-  } catch(e) {
-    console.warn(e);
+// remove feature from store
+  _removeFeature(feature) {
+    const featuresArray = this._features.getArray();
+    for (let i = 0; i < featuresArray.length; i++) {
+      const feat = featuresArray[i];
+      if (feature.getUid() === feat.getUid()) {
+        this._features.removeAt(i);
+        break;
+      }
+    }
+    this._features.dispatchEvent('change');
   }
-  this._features = null;
-  this._features = new ol.Collection([]);
-};
 
-module.exports = OlFeaturesStore;
+
+  _clearFeatures() {
+    try {
+      this._features.clear();
+    } catch(e) {
+      console.warn(e);
+    }
+    this._features = null;
+    this._features = new ol.Collection([]);
+  }
+
+}
