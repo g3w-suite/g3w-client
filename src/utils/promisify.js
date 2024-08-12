b152b2dcb993504602096cf7a7f9146b57516b31
volterra79 <boccacci.francesco@gmail.com>
2024-08-12 14:07:19 +0200

    Clean code - console.warn

--- src/utils/promisify.js
+++ src/utils/promisify.js
@@ -24,13 +24,13 @@ export function promisify(promise) {
 export function $promisify(promise) {
   if (undefined === promise) {
     console.trace();
-    return $.Deferred(d=> d.reject('not a promise')).promise();
+    return $.Deferred(d => d.reject('not a promise')).promise();
   }
   if (promise.always) {
     return promise;
   }
   return $.Deferred(async d => {
     try { d.resolve(await (promise instanceof Promise ? promise : promise())); }
-    catch (e) { d.reject(e); }
+    catch (e) { console.warn(e); d.reject(e); }
   }).promise();
 }
