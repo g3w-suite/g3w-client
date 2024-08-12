a83f1624c785ec624ca3dfcec4930535add33dae
volterra79 <boccacci.francesco@gmail.com>
2024-08-12 16:11:32 +0200

    :recycle: Convert Feature to Class

--- src/app/core/layers/features/feature.js
+++ src/app/core/layers/features/feature.js
@@ -2,185 +2,176 @@ import CONSTANT from 'app/constant';
 
 const { uniqueId } = require('utils');
 
-const geometryFields = CONSTANT.GEOMETRY_FIELDS;
+module.exports = class Feature extends ol.Feature {
+  constructor(opts = {}) {
+    super();
+    this.state = {
+      new:     false,
+      state:   null,
+      visible: true
+    };
 
-const Feature = function(options={}) {
-  ol.Feature.call(this);
-  this._uid = uniqueId();
-  this._newPrefix = '_new_';
-  this._geometry = false;
-  const {feature, properties} = options;
-  if (feature) {
-    // check if it has to set only some properties or all feature properties
-    if (properties && Array.isArray(properties)) {
-      properties.forEach(property => this.set(property, feature.get(property)));
-    } else {
-      this.setProperties(feature.getProperties());
+    this._uid       = uniqueId();
+    this._newPrefix = '_new_';
+    this._geometry  = false;
+
+    const { feature, properties } = opts;
+
+    if (feature) {
+      // check if it has to set only some properties or all feature properties
+      if (properties && Array.isArray(properties)) {
+        properties.forEach(property => this.set(property, feature.get(property)));
+      } else {
+        this.setProperties(feature.getProperties());
+      }
+      this.setId(feature.getId());
+      this.setGeometryName(feature.getGeometryName());
+      const geometry = feature.getGeometry();
+      this._geometry = !!geometry;
+      geometry && this.setGeometry(geometry);
+      const style = this.getStyle();
+      if (style) {
+        this.setStyle(style);
+      }
     }
-    this.setId(feature.getId());
-    this.setGeometryName(feature.getGeometryName());
-    const geometry = feature.getGeometry();
-    this._geometry = !!geometry;
-    geometry && this.setGeometry(geometry);
-    const style = this.getStyle();
-    style && this.setStyle(style);
+
   }
-  this.state = {
-    new: false,
-    state: null,
-    visible: true
+
+  /**
+   * Return unique id
+   * @returns {*}
+   */
+  getUid() {
+    return this._uid
   };
-};
 
-ol.inherits(Feature, ol.Feature);
+  /**
+   * set new uid
+   * @param uid
+   * @private
+   */
+  _setUid(uid) {
+    this._uid = uid;
+  };
 
-const proto = Feature.prototype;
+  isGeometry() {
+    return this._geometry;
+  };
 
-//change constructor
-proto.constructor = 'Feature';
+  /**
+   * Clone a feature with id and pk new
+   * @param pk field <Object> send pk field
+   * @returns {Feature}
+   */
+  cloneNew(pk) {
+    const clone = this.clone();
+    clone._setUid(uniqueId());
+    clone.setTemporaryId();
+    //in the case of send pk field object set temporary new value
+    //to avoid duplicate pk when save clone feature on server
+    if (pk && false === pk.editable) {
+      //need to be set null
+      clone.set(pk.name, null);
+    }
+    return clone;
+  };
 
-/**
- * Return unique id
- * @returns {*}
- */
-proto.getUid = function(){
-  return this._uid
-};
+  /**
+   * clone existing feature
+   * @returns {Feature}
+   */
+  clone() {
+    const feature = super.clone();
+    feature.setId(this.getId());
+    if (this.isGeometry()) {
+      feature.setGeometry(feature.getGeometry().clone());
+    }
+    const clone = new Feature({ feature });
+    clone._setUid(this.getUid());
+    clone.setState(this.getState());
+    if (this.isNew()) {
+      clone.setNew();
+    }
+    return clone;
+  };
 
-/**
- * set new uid
- * @param uid
- * @private
- */
-proto._setUid = function(uid){
-  this._uid = uid;
-};
+  setTemporaryId() {
+    this.setId(`${this._newPrefix}${uniqueId()}`);
+    this.setNew();
+  };
 
-proto.isGeometry = function(){
-  return this._geometry;
-};
+  setNew() {
+    this.state.new = true;
+  };
 
-/**
- * Clone a feature with id and pk new
- * @param pk field <Object> send pk field
- * @returns {Feature}
- */
-proto.cloneNew = function(pk){
-  const clone = this.clone();
-  const uid = uniqueId();
-  clone._setUid(uid);
-  clone.setTemporaryId();
-  //in case of send pk field object set temporary new value
-  //to avoid duplicate pk when save clone feature on server
-  if (pk && false === pk.editable) {
-    //need to be set null
-    clone.set(pk.name, null);
-  }
-  return clone;
-};
+  delete() {
+    this.state.state = 'delete';
+    return this;
+  };
 
-/**
- * clone existing feature
- * @returns {Feature}
- */
-proto.clone = function() {
-  const feature = ol.Feature.prototype.clone.call(this);
-  feature.setId(this.getId());
-  this.isGeometry() && feature.setGeometry(feature.getGeometry().clone());
-  const clone = new Feature({
-    feature
-  });
-  const uid = this.getUid();
-  clone._setUid(uid);
-  clone.setState(this.getState());
-  this.isNew() && clone.setNew();
-  return clone;
-};
+  update() {
+    this.state.state = 'update';
+    return this;
+  };
 
-proto.setTemporaryId = function() {
-  const newValue = `${this._newPrefix}${uniqueId()}`;
-  this.setId(newValue);
-  this.setNew();
-};
+  add() {
+    this.state.state = 'add';
+    return this;
+  };
 
-proto.setNew = function() {
-  this.state.new = true;
-};
+  isNew() {
+    return this.state.new;
+  };
 
-proto.delete = function() {
-  this.state.state = 'delete';
-  return this;
-};
+  isAdded() {
+    return 'add' === this.state.state;
+  };
 
-proto.update = function() {
-  this.state.state = 'update';
-  return this;
-};
+  isUpdated() {
+    return 'update' === this.state.state;
+  };
 
-proto.add = function() {
-  this.state.state = 'add';
-  return this;
-};
+  isDeleted() {
+    return 'delete' === this.state.state;
+  };
 
-proto.isNew = function() {
-  return this.state.new;
-};
+  setState(state) {
+    this.state.state = state;
+  };
 
-proto.isAdded = function() {
-  return this.state.state === 'add';
-};
+  getState() {
+    return this.state.state;
+  };
 
-proto.isUpdated = function() {
-  return this.state.state === 'update';
-};
+  getAlphanumericProperties() {
+    return Object
+      .entries(this.getProperties())
+      .filter(([name, _]) => !CONSTANT.GEOMETRY_FIELDS.includes(name))
+      .reduce((attrs, [n, v]) => { attrs[n] = v; return attrs }, {})
+  };
 
-proto.isDeleted = function() {
-  return this.state.state === 'delete';
-};
+  /**
+   * clean state of the features
+   */
+  clearState() {
+    this.state.state = null;
+    this.state.new   = false;
+  };
 
-proto.setFullState = function(state) {
-  this.state = state;
-};
+  /**
+   * need to filter features visiblity on table
+   * @returns {boolean}
+   */
+  isVisible() {
+    return this.state.visible;
+  };
 
-proto.getFullState = function() {
-  return this.state;
-};
-
-proto.setState = function(state) {
-  this.state.state = state;
-};
-
-proto.getState = function() {
-  return this.state.state;
-};
-
-proto.getAlphanumericProperties = function() {
-  const properties = this.getProperties();
-  const alphanumericproperties = {};
-  for (let name in properties) {
-    if (geometryFields.indexOf(name) === -1)
-      alphanumericproperties[name] = properties[name];
-  }
-  return alphanumericproperties;
-};
-
-//clean state of the features
-proto.clearState = function() {
-  this.state.state = null;
-  this.state.new = false;
-};
-
-/**
- * need to filter features visiblity on table
- * @returns {boolean}
- */
-proto.isVisible = function(){
-  return this.state.visible;
-};
-
-proto.setVisible = function(bool=true){
-  this.state.visible = bool;
-};
+  setVisible(bool = true) {
+    this.state.visible = bool;
+  };
 
 
-module.exports = Feature;
+}
+
+
+
