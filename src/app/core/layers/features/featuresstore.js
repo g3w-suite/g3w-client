03ec89122b2bdde0cc930150ea6936feb24fbcf5
volterra79 <boccacci.francesco@gmail.com>
2024-08-12 12:27:08 +0200

    Clean code - Readability and refactor code removing methods

--- src/app/core/layers/features/featuresstore.js
+++ src/app/core/layers/features/featuresstore.js
@@ -8,11 +8,11 @@ const _cloneDeep        = require('lodash.clonedeep');
 function FeaturesStore(options = {}) {
   this._features  = options.features || [];
   this._provider  = options.provider || null;
-  this._loadedIds = []; // store locked ids
+  this._loadedIds = []; // store features id load by current user
   this._lockIds   = []; // store locked features
   //setters
   this.setters = {
-    addFeatures(features=[]) {
+    addFeatures(features = []) {
       features.forEach(f => this._addFeature(f))
     },
     addFeature(feature) {
@@ -27,16 +27,22 @@ function FeaturesStore(options = {}) {
     clear() {
       this._clearFeatures();
     },
-    getFeatures(options={}) {
+    getFeatures(options = {}) {
       return this._getFeatures(options);
     },
+    /**
+     *
+     * @param commitItems
+     * @param featurestore Its is used????
+     * @return {*}
+     */
     commit(commitItems, featurestore) {
       return this._commit(commitItems, featurestore);
     },
     /**
      * setter to know when some features are locked
      */
-    featuresLockedByOtherUser(features=[]) {},
+    featuresLockedByOtherUser(features = []) {},
   };
 
   base(this);
@@ -70,13 +76,13 @@ proto.unlock = function() {
 /*
  * Gets all features from server or attribute _features
  */
-proto._getFeatures = function(options={}) {
+proto._getFeatures = function(options = {}) {
   const d = $.Deferred();
   if (this._provider) {
     //call provider getFeatures to get features from server
     this._provider.getFeatures(options)
       .then(options => {
-        //get features base on response from server features, featurelockis etc ...
+        //get the features base on response from server features, featurelockis etc ...
         const features = this._filterFeaturesResponse(options);
         this.addFeatures(features);
         d.resolve(features);
@@ -88,70 +94,78 @@ proto._getFeatures = function(options={}) {
   return d.promise();
 };
 
-//filter features to add
-proto._filterFeaturesResponse = function(options={}) {
+/**
+ * Filter features to add
+ * @param options
+ * @private
+ * @return Array of features to add
+ */
+proto._filterFeaturesResponse = function(options = {}) {
   /**
-   * get features returned from server and feature that are currently locked.
+   * features uis array of feature returned from server and feature that are currently locked.
+   * featurelocks is array of the feature that can be locker by current client request (not locked by another user)
+   * featurelocks array item
+   * {
+   *   featureid: Is current id of feature locked
+   *   lockid: Is a server unique lock id number
+   * }
+   * ex.
+   * {featureid: "1", lockid: "6bbab1c1c03332fb39b8ffae35e557ba"}
+   *
    * If featurelocks are less than features, it means that another user is editing these features
+   *
+   *
    * @type {*[]}
    */
   const { features = [], featurelocks = [] } = options;
 
-  //if no features locks mean all feature requests are locked by another user
+  //if no features locks mean another user locks all feature requests
   if (0 === featurelocks.length) {
-    //if there are feature on response are locked
+    //if there are features on response
     if (features.length > 0) {
+      //It means that another user locks these features
       this.featuresLockedByOtherUser(features);
     }
     return [];
   }
 
-  this._filterLockIds(featurelocks);
+  //get already loaded feature id locked by current user
+  const fids = this._lockIds.map(({ featureid }) => featureid);
+  featurelocks
+    .filter(({ featureid }) => !fids.includes(featureid)) //exclude features already locked by current user
+    .forEach(fl => this._lockIds.push(fl)) //update lockIds based on a featurelocks array from response
 
+  //store features locked by another user
   const lockFeatures = [];
 
+  //Store features to add to layers source
   const featuresToAdd = features.filter(f => {
+    //get feature id
     const featureId = f.getId();
+    //check if feature id is locked features
+    //it means that is not locked by another user.
     if (featurelocks.find(({ featureid }) => featureId == featureid)) {
+      //check if feature is not yet added for the current user
       if (this._loadedIds.indexOf(featureId) === -1) {
         this._loadedIds.push(featureId);
         return true;
+      } else {
+        return false; //feature locked by the current user
       }
     } else {
       lockFeatures.push(f);
+      return false; //feature locked by another user
     }
   });
 
-  //if count features
+  //if features locks are less than features get from server,
+  // it means that another user locks some features
   if (featurelocks.length < features.length) {
     this.featuresLockedByOtherUser(lockFeatures);
   }
 
   return featuresToAdd;
-};
-
-/**
- *
- * @param featurelocks Array of lock feature locked by server fo a request
- * Element of an array is
- * {
- *   featureid: Is current id of feature locked
- *   lockid: Is a server unique lock id  number
- * }
- * ex.
- * {featureid: "1", lockid: "6bbab1c1c03332fb39b8ffae35e557ba"}
- * @private
- */
-proto._filterLockIds = function(featurelocks = []) {
-  this._lockIds = [
-    ...this._lockIds,
-    ...(featurelocks.filter(fl => this._lockIds.map(({ featureid }) => featureid).indexOf(fl.featureid) === -1))
-  ];
-};
-
-proto.addLoadedIds = function(id) {
-  this._loadedIds.push(id);
-};
+}
 
 proto.getLockIds = function() {
   return this._lockIds;
@@ -202,7 +216,7 @@ proto._addFeature = function(feature) {
 //substitute feature after update
 proto._updateFeature = function(feature) {
   this._features.find((feat, idx) => {
-    if (feat.getUid() === feature.getUid()) {
+    if (feature.getUid() === feat.getUid() ) {
       this._features[idx] = feature;
       return true;
     }
