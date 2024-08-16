0b991edcd137a22a92bc8c5520fa40dab06c9630
volterra79 <boccacci.francesco@gmail.com>
2024-08-16 16:16:56 +0200

    :recycle: replace GUI as Class

--- src/services/gui.js
+++ src/services/gui.js
@@ -1,77 +1,81 @@
 import ApplicationService from 'services/application';
 import ComponentsRegistry from 'store/components';
 
-const {
-  base,
-  inherit,
-  noop
-}               = require('utils');
+const { noop }  = require('utils');
 const G3WObject = require('core/g3wobject');
 
 // API della GUI.
-// methods have be defined by application
+// methods have been defined by application
 // app should call GUI.ready() when GUI is ready
-function GUI() {
+export default new (class GUI extends G3WObject {
+  constructor(opts) {
+    super(opts);
 
-  this.setters = {
-    setContent(options = {}) {
-      this.emit('opencontent', true);
-      this._setContent(options)
-    }
-  };
+    this.setters = {
+      setContent(opts = {}) {
+        this.emit('opencontent', true);
+        this._setContent(opts)
+      }
+    };
 
-  this.isready          = false;
-  // images urls
-  this.getResourcesUrl  = noop;
-  // show a Vue form
-  this.showForm         = noop;
-  this.closeForm        = noop;
-  this.showListing      = noop;
-  this.closeListing     = noop;
-  this.hideListing      = noop;
-  // modal
-  this.setModal         = noop;
-  this.showFullModal    = noop;
-  // modal
-  this.showQueryResults = noop;
-  this.hideQueryResults = noop;
-  this.showPanel        = noop;
-  this.hidePanel        = noop;
-  this.reloadComponents = noop;
-  this.showUserMessage  = noop;
-  this.closeUserMessage = noop;
-  this.showModalDialog  = noop;
-  //property to how a result has to be adding or close all and show new
-  // false mean create new and close all open
-  this.push_content     = false;
+    this.isready          = false;
+    // images urls
+    this.getResourcesUrl  = noop;
+    // show a Vue form
+    this.showForm         = noop;
+    this.closeForm        = noop;
+    this.showListing      = noop;
+    this.closeListing     = noop;
+    this.hideListing      = noop;
+    // modal
+    this.setModal         = noop;
+    this.showFullModal    = noop;
+    // modal
+    this.showQueryResults = noop;
+    this.hideQueryResults = noop;
+    this.showPanel        = noop;
+    this.hidePanel        = noop;
+    this.reloadComponents = noop;
+    this.showUserMessage  = noop;
+    this.closeUserMessage = noop;
+    this.showModalDialog  = noop;
+    this.notify           = noop;
+    this.dialog           = noop;
+    this.isMobile         = noop;
+    //property to how a result has to be adding or close all and show new
+    // false mean create new and close all open
+    this.push_content     = false;
+    this._closeUserMessageBeforeSetContent = true;
 
-  this.setPushContent = function (bool = false) {
+  }
+  setPushContent(bool = false) {
     this.push_content = bool;
-  };
-  this.getPushContent = function() {
+  }
+
+  getPushContent() {
     return this.push_content;
-  };
+  }
 
-  this._closeUserMessageBeforeSetContent = true;
-
-  this.setComponent = function(component) {
+  setComponent(component) {
     ComponentsRegistry.registerComponent(component);
-  };
-  this.getComponent = function(id) {
+  }
+  getComponent(id) {
     return ComponentsRegistry.getComponent(id);
-  };
-  this.getComponents = function() {
+  }
+
+  getComponents() {
     return ComponentsRegistry.getComponents();
-  };
-  this.ready = function() {
+  }
+  ready() {
     this.emit('ready');
     this.isready = true;
-  };
-  this.guiResized = function() {
+  }
+
+  guiResized() {
     this.emit('guiresized');
-  };
+  }
   //ready GUI
-  this.isReady = function(){
+  isReady() {
     return new Promise(resolve => this.isready ? resolve() : this.once('ready', resolve));
   };
   /**
@@ -79,43 +83,32 @@ function GUI() {
    * @param componentId
    * @returns {*}
    */
-  this.getService = function(componentId) {
+  getService(componentId) {
     const component = this.getComponent(componentId);
     return component && component.getService();
-  };
-  /* spinner */
-  this.showSpinner = function(options={}) {};
-  this.hideSpinner = function(id) {};
-  /* end spinner */
-  this.notify      = noop;
-  this.dialog      = noop;
-  this.isMobile    = noop;
-  //useful to register setters
-  base(this);
-}
-
-inherit(GUI, G3WObject);
-
-/**
- * Wrapper for download
- * 
- * @param { Function } downloadFnc function to call
- * @param { Object }   options     Object parameters
- * 
- * @since 3.9.0
- */
-GUI.prototype.downloadWrapper = async function(downloadFnc, options = {}) {
-  const download_caller_id = ApplicationService.setDownload(true);
-  this.setLoadingContent(true);
-
-  try {
-    await downloadFnc(options);
-  } catch(e) {
-    this.showUserMessage({ type: 'alert', message: e || 'server_error', textMessage: !!e })
   }
-  ApplicationService.setDownload(false, download_caller_id);
+  /* spinner */
+  showSpinner(opts ={}) {};
+  hideSpinner(id) {};
+  /* end spinner */
+  /**
+   * Wrapper for download
+   *
+   * @param { Function } downloadFnc function to call
+   * @param { Object }   options     Object parameters
+   *
+   * @since 3.9.0
+   */
+  async downloadWrapper(downloadFnc, options = {}) {
+    this.setLoadingContent(true);
 
-  this.setLoadingContent(false);
-};
+    try {
+      await downloadFnc(options);
+    } catch(e) {
+      this.showUserMessage({ type: 'alert', message: e || 'server_error', textMessage: !!e })
+    }
+    ApplicationService.setDownload(false, ApplicationService.setDownload(true));
 
-export default new GUI();
+    this.setLoadingContent(false);
+  }
+});
