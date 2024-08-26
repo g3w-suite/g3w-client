2a2c03eee293473a1177dbb8cb0b737358e5dd1e
volterra79 <boccacci.francesco@gmail.com>
2024-08-26 14:04:24 +0200

    use $promisify instead of $.Deferred

--- src/utils/getAjaxResponses.js
+++ src/utils/getAjaxResponses.js
@@ -1,30 +1,31 @@
+import { $promisify } from 'utils/promisify';
+
 export function getAjaxResponses(listRequests = []) {
-  const d = $.Deferred();
-  const DoneRespones = [];
-  const FailedResponses = [];
-  let requestsLenght = listRequests.length;
+  return $promisify(new Promise((resolve) => {
+    const DoneRespones    = [];
+    const FailedResponses = [];
+    let requestsLenght    = listRequests.length;
 
-  listRequests
-    .forEach((request) => {
-      request
-        .then((response) => {
-          DoneRespones.push(response)
-        })
-        .fail(e => {
-          console.warn(e);
-          FailedResponses.push(e);
-        })
-        .always(() => {
-          requestsLenght = requestsLenght > 0 ? requestsLenght - 1: requestsLenght;
-          if (0 === requestsLenght) {
-            d.resolve({
-              done: DoneRespones,
-              fail: FailedResponses
-            })
-          }
+    listRequests
+      .forEach((request) => {
+        request
+          .then((response) => {
+            DoneRespones.push(response)
+          })
+          .fail(e => {
+            console.warn(e);
+            FailedResponses.push(e);
+          })
+          .always(() => {
+            requestsLenght = requestsLenght > 0 ? requestsLenght - 1: requestsLenght;
+            if (0 === requestsLenght) {
+              resolve({
+                done: DoneRespones,
+                fail: FailedResponses
+              })
+            }
 
-    })
-  });
-
-  return d.promise();
+          })
+      });
+  }))
 }
