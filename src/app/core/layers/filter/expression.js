e720a1adce48f7697a7db17e30309b6af5a3adde
volterra79 <boccacci.francesco@gmail.com>
2024-08-27 11:06:46 +0200

    Clean code - readability

--- src/app/core/layers/filter/expression.js
+++ src/app/core/layers/filter/expression.js
@@ -3,81 +3,164 @@ import { FILTER_EXPRESSION_OPERATORS } from 'app/constant';
 //Expression
 module.exports = class Expression {
   
-  constructor(options={}) {
-    this._layerName  = options.layerName;
-    this._expression = options.filter || '';
+  constructor(opts = {}) {
+    this._layerName  = opts.layerName;
+    this._expression = opts.filter || '';
   }
 
+  /**
+   * AND
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   and(field, value) {
-    this._expression = this._expression ? this._expression + ' AND ': this._expression;
+    this._expression = this._expression ? `${this._expression} AND `: this._expression;
     if (field && value) {
       this.eq(field, value);
     }
     return this;
   }
 
+  /**
+   * OR
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   or(field, value) {
     if (field && value) {
-      this._expression = this._expression ? this._expression + ' OR ' : this._expression;
+      this._expression = this._expression ? `${this._expression} OR ` : this._expression;
       this.eq(field, value);
     }
     return this;
   }
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   eq(field, value) {
-    this._expression = this._expression + this._build('eq', field, value);
+    this._expression = this._build('eq', field, value);
     return this;
   }
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   like(field, value) {
-    this._expression = this._expression + this._build('LIKE', field, value);
+    this._expression = this._build('LIKE', field, value);
     return this;
   };
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   ilike(field, value) {
-    this._expression = this._expression + this._build('ILIKE', field, value);
+    this._expression = this._build('ILIKE', field, value);
     return this;
   }
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   not(field, value) {
-    this._expression = this._expression + this._build('NOT', field, value);
+    this._expression = this._build('NOT', field, value);
     return this;
   }
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   gt(field, value) {
-    this._expression = this._expression + this._build('gt', field, value);
+    this._expression = this._build('gt', field, value);
     return this;
   }
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   gte(field, value) {
-    this._expression = this._expression + this._build('gte', field, value);
+    this._expression = this._build('gte', field, value);
     return this;
   }
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   lt(field, value) {
-    this._expression = this._expression + this._build('lt', field, value);
+    this._expression = this._build('lt', field, value);
     return this;
   }
 
+  /**
+   *
+   * @param field
+   * @param value
+   * @return {Expression}
+   */
   lte(field, value) {
-    this._expression = this._expression + this._build('lte', field, value);
+    this._expression = this._build('lte', field, value);
     return this;
   }
 
+  /**
+   *
+   * @return {Expression}
+   */
   clear() {
     this._expression = '';
     return this;
   }
 
-  // get expression method to get the realt value of the expression
+  /**
+   * Get expression method to get the real value of the expression
+   * @return {string|*|string}
+   */
   get() {
     return this._layerName ? `${this._layerName}: ${this._expression}`: this._expression;
   }
 
+  /**
+   *
+   * @param operator
+   * @param field
+   * @param value
+   * @return {string}
+   * @private
+   */
   _build(operator, field, value) {
-    return [`"${field}"`, FILTER_EXPRESSION_OPERATORS[operator], `${value}`].join(' ');
+    return `${this._expression}${[`"${field}"`, FILTER_EXPRESSION_OPERATORS[operator], `${value}`].join(' ')}`;
   }
 
+  /**
+   *
+   * @param value
+   * @param attribute
+   * @param operator
+   * @param logicop
+   * @return {string}
+   */
   createSingleExpressionElement({ value, attribute, operator, logicop } = {}) {
     const op    = FILTER_EXPRESSION_OPERATORS[operator];
     const logic = logicop && ` ${logicop} ` || '';
@@ -99,7 +182,12 @@ module.exports = class Expression {
     }
   }
 
-  createExpressionFromFilterObject(filter={}) {
+  /**
+   *
+   * @param filter
+   * @return {string | boolean}
+   */
+  createExpressionFromFilterObject(filter = {}) {
     let filters = [];
     let rootFilter;
     for (const operator in filter) {
@@ -120,12 +208,25 @@ module.exports = class Expression {
     return rootFilter;
   }
 
-  createExpressionFromField({layerName, field, value, operator='eq'}) {
+  /**
+   *
+   * @param layerName
+   * @param field
+   * @param value
+   * @param operator
+   * @return {Expression}
+   */
+  createExpressionFromField({ layerName, field, value, operator = 'eq' }) {
     this._expression = `${layerName}:${this.createSingleExpressionElement({ attribute: field, value, operator })}`;
     return this;
   }
 
-  createExpressionFromFilterArray(inputs=[]) {
+  /**
+   *
+   * @param inputs
+   * @return {*|undefined}
+   */
+  createExpressionFromFilterArray(inputs = []) {
     return inputs.reduce((acc, input, i) => {
       const filter = this.createSingleExpressionElement(input);
     // set logicop of last element to null
@@ -133,6 +234,12 @@ module.exports = class Expression {
     }, '') || undefined;
   }
 
+  /**
+   *
+   * @param filter
+   * @param layerName
+   * @return {Expression}
+   */
   createExpressionFromFilter(filter, layerName) {
     const fparam = Array.isArray(filter) ? this.createExpressionFromFilterArray(filter) : this.createExpressionFromFilterObject(filter);
     if (fparam) this._expression = `${layerName}:${fparam}`;
