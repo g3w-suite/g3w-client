a54e10e351bdb6345784ffa6aa79cb30733bb752
Raruto <Raruto@users.noreply.github.com>
2024-09-18 16:18:55 +0200

    same as: https://github.com/g3w-suite/g3w-client/pull/663/commits/6d0c94e2ee67233148d2469b59ee6107d82d8a66

--- src/utils/getCatalogLayers.js
+++ src/utils/getCatalogLayers.js
@@ -3,6 +3,6 @@ import ApplicationState from 'store/application';
 /**
  * ORIGINAL SOURCE: src/app/core/layers/layersstoreregistry.js@v3.10.2
  */
-export function getCatalogLayers(filter) {
-  return Object.values(ApplicationState.catalog).flatMap(s => s.getLayers(filter, { TOC_ORDER: true }));
+export function getCatalogLayers(filter, options = {}) {
+  return Object.values(ApplicationState.catalog).flatMap(s => s.getLayers(filter, options));
 }
