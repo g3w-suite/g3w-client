1b15191a628515f6796ec5cab5410ddc00ed7bbf
Raruto <Raruto@users.noreply.github.com>
2024-09-18 09:40:47 +0200

    Update distance.js

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,13 +1,10 @@
 /**
+ * core/geometry/geom::distance@v3.4
  * core/geometry/geom::squaredDistance@v3.4
  */
-function squaredDistance(c1, c2) {
-  return Math.pow(c2[0] - c1[0], 2) + Math.pow(c2[1] - c1[1], 2);
-}
-
-/**
- * core/geometry/geom::distance@v3.4
- */
 export function distance(c1, c2) {
-  return Math.sqrt(squaredDistance(c1, c2));
+  return Math.sqrt(
+    Math.pow(c2[0] - c1[0], 2) +
+    Math.pow(c2[1] - c1[1], 2)
+  );
 }
