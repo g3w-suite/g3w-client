b12d5b0027027c4d38a5ee11a837d7afcf7fe31b
volterra79 <boccacci.francesco@gmail.com>
2024-09-18 15:43:41 +0200

    :bug: Fix order layers from catalog registry follow TOC order

--- src/store/map-layers.js
+++ src/store/map-layers.js
@@ -2,7 +2,15 @@
  * @file Store Open Layers levels (raster images, vector shapes, markers, ...)
  * @since v3.6
  */
-
 const LayersStoresRegistry = require('core/layers/layersstoresregistry');
 
-export default new LayersStoresRegistry();
+export default new (class CatalogLayersStoresRegistry extends LayersStoresRegistry  {
+  constructor() {
+    super()
+  }
+
+  getLayers(filter) {
+    return Object.values(this.stores).flatMap(s => s.getLayers(filter, { TOC: true }));
+  }
+
+});
