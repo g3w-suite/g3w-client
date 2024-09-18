df665a0106772df36f75c8ef7f4d75c48fdd36c4
Raruto <Raruto@users.noreply.github.com>
2024-09-18 09:40:50 +0200

    Update sanitizeFidFeature.js

--- src/utils/sanitizeFidFeature.js
+++ src/utils/sanitizeFidFeature.js
@@ -1,7 +1,7 @@
 export function sanitizeFidFeature(fid) {
   if ('string' === typeof fid && Number.isNaN(1*fid))  {
     fid = fid.split('.');
-    fid = 2 === fid.length ? fid[1] : fid[0];
+    fid = fid.at(2 === fid.length ? 1 : 0);
   }
   return fid;
 }
