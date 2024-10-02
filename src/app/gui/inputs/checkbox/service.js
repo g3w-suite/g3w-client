b807df53dd855b876f7950905ed8f75ec22dd95f
volterra79 <boccacci.francesco@gmail.com>
2024-10-02 17:43:04 +0200

    :bug:Show label when edit input

--- src/app/gui/inputs/checkbox/service.js
+++ src/app/gui/inputs/checkbox/service.js
@@ -1,29 +1,25 @@
 const Service           = require('gui/inputs/service');
 module.exports = class CheckBoxService extends Service {
   constructor(opts = {}) {
-    const value = opts.state.input.options.values.find(v => false === v.checked);
     opts.validatorOptions = {
       values: opts.state.input.options.values.map(v => v)
     };
-    if (null === opts.state.value && !opts.state.forceNull) {
-      opts.state.value = value.value
-    }
     super(opts);
   }
 
   convertCheckedToValue(checked) {
     checked          = [null, undefined].includes(checked) ? false : checked;
-    this.state.value = (this.state.input.options.values.find(v => checked === v.checked) || {}).value;
-
+    // Need to convert it to string.
+    // Server return always string value
+    this.state.value = `${(this.state.input.options.values.find(v => checked === v.checked) || {}).value}`;
     return this.state.value;
   };
 
   convertValueToChecked() {
-    const valueToCheck = this.state.value;
-    if ([null, undefined].includes(valueToCheck)) { return false }
-    let option = this.state.input.options.values.find(value => valueToCheck == value.value);
+    if ([null, undefined].includes(this.state.value)) { return false }
+    let option = this.state.input.options.values.find(v => this.state.value == v.value);
     if (undefined === option) {
-      option = this.state.input.options.values.find(value => false === value.checked);
+      option = this.state.input.options.values.find(v => false === v.checked);
       this.state.value = option.value;
     }
     return option.checked;
