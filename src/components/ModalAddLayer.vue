e2282397f2a84e1ed68588578d9f6374091f6319
Raruto <Raruto@users.noreply.github.com>
2024-10-30 15:25:09 +0100

    invalid html

--- src/components/ModalAddLayer.vue
+++ src/components/ModalAddLayer.vue
@@ -129,7 +129,7 @@
               <!-- LAYER INFO -->
               <fieldset v-if="wms_config.abstract" class="form-group" style="border: 1px solid #c0c0c0; padding: 4.9px 8.75px 8.75px 10.5px;border-radius: 3px;">
                 <legend style="width: 15px;height: 15px;border: 1px solid;border-radius: 50%;background-color: #222d32;font-weight: bold;color: #fff;font-size: 0.7em; text-align: center; margin: 0 -14px;user-select: none;">i</legend>
-                <span v-t = "wms_config.abstract"></span>
+                {{ wms_config.abstract }}
               </fieldset>
 
               <!-- LAYERS NAME   -->
@@ -801,6 +801,8 @@ export default {
           contentType: 'application/json',
           data:        JSON.stringify({ url: url || this.url, service: "wms" })
         });
+
+        console.log(config);
 
         // skip on invalid response
         if (!config.result) {
