9efdd915715a97d3eb4718f59988592436334784
volterra79 <boccacci.francesco@gmail.com>
2024-11-05 15:11:37 +0100

    :bug: Remove _layers.concat as https://github.com/g3w-suite/g3w-client/pull/681/commits/1c2379ed53c572bd2a96fb4dcb6da9d180f33af0

--- gulpfile.js
+++ gulpfile.js
@@ -198,7 +198,7 @@ const browserify_plugin = (pluginName, watch = true) => {
   .on('log', (info) => watch && !production && gutil.log(GREEN__ + '[' + pluginName + ']' + __RESET + ' →', info));
 
   // remove source map file
-  del([`${src}/plugin.js.map`]);
+  del([`${src}/plugin.js.map`], {force: true});
 
   const rebundle = () => {
     const version = get_version(pluginName);
