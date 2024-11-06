b422f4d76717db03f695b46ef008aca373ad16ba
volterra79 <boccacci.francesco@gmail.com>
2024-11-06 14:09:45 +0100

    set the first pagination page length array item to 10

--- src/g3w-constants.js
+++ src/g3w-constants.js
@@ -421,7 +421,7 @@ export const SELECTION = {
  * Page length for pagination
  * @type {number[]}
  */
-export const PAGELENGTHS = [5, 25, 50, 100];
+export const PAGELENGTHS = [10, 25, 50, 100];
 
 export default {
   APP_VERSION,
