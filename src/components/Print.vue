6adb4206fc4c10d355caef4f40c137b6a50e7d6b
volterra79 <boccacci.francesco@gmail.com>
2024-12-12 12:08:35 +0100

    :lipstick: alignment

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -18,7 +18,7 @@
         <fieldset style="border: 1px solid; padding: 4.9px 8.75px 8.75px 10.5px; border-radius: 3px; background-color: rgb(56, 66, 70); color: rgb(255, 255, 255); user-select:none">
           <legend style="width: 15px; height: 15px; border: 1px solid; border-radius: 50%; background-color: rgb(34, 45, 50); font-weight: bold; color: rgb(255, 255, 255); font-size: 0.7em; text-align: center; margin: 0px -14px; user-select: none;">i</legend>
           <details>
-            <summary style="cursor: pointer;display: flex;justify-content: space-between;width: 100%;" v-t-tooltip:right.create = "'show_more'">
+            <summary style="cursor: pointer;display: flex;justify-content: space-between; align-items: center; width: 100%;" v-t-tooltip:right.create = "'show_more'">
               <span style="text-overflow: ellipsis;overflow: hidden;" v-t="'sdk.print.help'"></span>
               <i class="far fa-eye"></i>
             </summary>
