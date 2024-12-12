e4ae8f4263e04d480795664f24377c25166a0f95
volterra79 <boccacci.francesco@gmail.com>
2024-12-12 11:29:13 +0100

    Clean code

--- src/g3w-plugin.js
+++ src/g3w-plugin.js
@@ -464,7 +464,7 @@ export class PluginService extends G3WObject {
     this.vm = new Vue();
     this.unwatch = this.vm.$watch(
       () => ApplicationState.gui.layout.__current,
-      layoutName => this.currentLayout = layoutName !== this.name ? layoutName : this.currentLayout
+      layoutName => this.currentLayout = layoutName === this.name ? this.currentLayout : layoutName
     );
   }
 
