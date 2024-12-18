cdd4c3a8e0cfa1a3eab894f9002d745fd74a21a8
volterra79 <boccacci.francesco@gmail.com>
2024-12-18 08:35:56 +0100

    :bug: Fix handle media input when commit changes to server (commit and updates/new)

--- src/components/InputMedia.vue
+++ src/components/InputMedia.vue
@@ -24,7 +24,7 @@
       </div>
       <bar-loader :loading = "loading"/>
       <g3w-media :state = "data">
-        <div class = "clearmedia" @click.stop = "clearMedia()">
+        <div class = "clearmedia" @click.stop = "clearMedia">
           <i :class = "g3wtemplate.font['trash-o']" class = "g3w-icon"></i>
         </div>
       </g3w-media>
@@ -56,7 +56,7 @@
           mime_type: null
         },
         mediaid: `media_${getUniqueDomId()}`,
-        loading: false
+        loading: false,
       }
     },
     methods: {
@@ -66,24 +66,24 @@
       clearMedia() {
         this.data.value = this.data.mime_type = this.state.value = null;
         this.change();
+      },
+      setMedia() {
+        if (this.state.value) {
+          this.data.value     = this.state.value.value;
+          this.data.mime_type = this.state.value.mime_type;
+        }
       }
     },
     created() {
-      if (this.state.value) {
-        this.data.value = this.state.value.value;
-        this.data.mime_type = this.state.value.mime_type;
-      }
+      this.setMedia();
     },
     watch: {
-      'state': {
-        deep:      true,
-        immediate: false,
-        handler({ value = {} }) {
-          this.data.value     = value.value;
-          this.data.mime_type = value.mime_type;
-        }
+      /**
+       * @since 3.11.0
+       */
+      'state.value'() {
+        this.setMedia();
       }
-
     },
     async mounted() {
       const fieldName = this.state.name;
@@ -107,7 +107,7 @@
             this.change();
           }
         },
-        fail: (e) => { console.warn(e); GUI.notify.error(t("info.server_error")) },
+        fail:   e => { console.warn(e); GUI.notify.error(t("info.server_error")) },
         always: () => this.loading = false
       });
 
