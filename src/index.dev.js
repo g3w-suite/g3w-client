c91951fb8f8ba53f4b751ce9ae64565c46f5727e
volterra79 <boccacci.francesco@gmail.com>
2025-02-18 09:32:39 +0100

    🐛 Sent message to main window content wvery time iframe sent a message

--- src/index.dev.js
+++ src/index.dev.js
@@ -231,11 +231,9 @@ g3wsdk.gui.GUI.once('ready', () => {
         onclick() {
           const w = window.open('about:blank', '_blank', `fullscreen=yes`);
           w.document.write(`<!doctype HTML><html><head><title>Test Iframe</title><style>html,body,iframe{width:100%;height:100%;margin:0;border:0;display:block;}</style></head><body><iframe src="${location.href}"></iframe></body></html>`);
-          // send message to iframe when app is ready
+          // send message to iframe every time ifrema send a message con contentWindow
           w.addEventListener('message', e => {
-            if (e.data.action === 'app:ready') {
-              setTimeout(() => g3wsdk.gui.GUI.emit('iframe:message', w.document.querySelector('iframe').contentWindow, e), 2000)
-            }
+            setTimeout(() => g3wsdk.gui.GUI.emit('iframe:message', w.document.querySelector('iframe').contentWindow, e), 2000)
           }, false);
           // prevent page refresh (eg. CTRL+R)
           w.onbeforeunload = () => w.close();
