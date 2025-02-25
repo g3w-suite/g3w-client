bd4e0b489d04c41ce3d35f30c5f99c739105b282
volterra79 <boccacci.francesco@gmail.com>
2025-02-25 10:01:54 +0100

    💡 comment

--- src/utils/getDataForSearchInput.js
+++ src/utils/getDataForSearchInput.js
@@ -22,7 +22,7 @@ export async function getDataForSearchInput({ state, field, suggest }) {
     )
       .filter(d => 'fulfilled' === d.status)
       .reduce((acc, d, i) => 0 === i
-        ? acc.concat(d.value.data || [])                                                       // TODO: add description
+        ? acc.concat(d.value.data || [])                                                       // for first layer get all uninques values 
         : [...new Set([...(d.value.data || []), ...acc].map(JSON.stringify))].map(JSON.parse), // ensure uniques values (search performed on multiple serach_layers)
         [] 
       )
