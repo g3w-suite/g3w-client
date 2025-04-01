3a0537dda69eccc57c9d40adde41bd1c646e1f58
volterra79 <boccacci.francesco@gmail.com>
2025-04-01 08:44:32 +0200

    ♻️ add filed and method using POST request in case of long columns fields

--- src/utils/createRelationsUrl.js
+++ src/utils/createRelationsUrl.js
@@ -13,10 +13,30 @@ export function createRelationsUrl({
   page      , //@since 3.11.2
   page_size,  //@since 3.11.2
   ordering,   //@since 3.11.3
+  field,      // @since 4.0.0 search columns purpose 
+  method = 'GET' // @since v4.0.0 <GET or POST> In case of post return a pst object with url data etc ..
 }) {
-  return `${ApplicationState.project.getLayerById(
+  const url = `${ApplicationState.project.getLayerById(
     undefined === relation.father
       ? (layer.id === relation.referencedLayer ? relation.referencingLayer : relation.referencedLayer)
       : (layer.id === relation.father          ? relation.child            : relation.father)
-  ).getUrl(type)}?relationonetomany=${relation.id}|${sanitizeFidFeature(fid)}&formatter=${formatter}${page ? '&page=' + page: ''}${page_size ? '&page_size=' + page_size: ''} ${ordering ? '&ordering=' + ordering: ''}`;
+  ).getUrl(type)}?relationonetomany=${relation.id}|${sanitizeFidFeature(fid)}`
+
+  if ('GET' === method) {
+    return `${url}&formatter=${formatter}${page ? '&page=' + page: ''}${page_size ? '&page_size=' + page_size: ''} ${ordering ? '&ordering=' + ordering: '', field ? '&field=' + field : ''}`;
+  }
+
+  if ('POST' === method) {
+    return {
+      url,
+      data: JSON.stringify({
+        page,
+        page_size,
+        formatter,
+        ordering,
+        field,
+      }),
+      contentType: 'application/json',
+    }
+  }
 }
