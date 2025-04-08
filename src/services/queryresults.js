9dd88b74ecd4cf91e1a64c4395bebd009b5f07ec
volterra79 <boccacci.francesco@gmail.com>
2025-04-08 16:33:19 +0200

    🐛 In case of query with autofilter, set feature selection selected attribute to true

--- src/services/queryresults.js
+++ src/services/queryresults.js
@@ -240,7 +240,7 @@ export default new (class QueryResultsService extends G3WObject {
                 id:         external ? f.getId() : (f instanceof ol.Feature ? f.getId() : f.id),
                 attributes: f instanceof ol.Feature ? f.getProperties() : f.properties,
                 geometry:   f instanceof ol.Feature ? f.getGeometry()   : f.geometry,
-                selection:  { selected: false },
+                selection:  { selected: !!queryResponse.query.autofilter }, //@snce 3.11.8 check if autofilter is set
                 show:       true,
               })),
               hasgeometry:            Array.isArray(features) && !rawdata && features.some(f => f instanceof ol.Feature ? f.getGeometry() : f.geometry),
