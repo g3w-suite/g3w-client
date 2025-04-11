c00634970e2bd29dd65e9d032cbc1b556706eb26
volterra79 <boccacci.francesco@gmail.com>
2025-04-11 15:57:29 +0200

    set layer parameter as an array

--- src/components/g3w-search.js
+++ src/components/g3w-search.js
@@ -233,7 +233,7 @@ async function doSearch({
         inputs: {
           layer,
           filter: createFilterFormInputs({
-            layer,
+            layer:  [layer], //set as an array according to the function
             inputs: features.map(f => ({
               attribute: (1 === referencedField.length ? referencedField[0] : referencedField),
               logicop:   'OR',
