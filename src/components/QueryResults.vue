56b099d2b2d574884a6082d1c3274f73c1181765
volterra79 <boccacci.francesco@gmail.com>
2025-04-24 15:25:48 +0200

    🐛 change features per page on pagination

--- src/components/QueryResults.vue
+++ src/components/QueryResults.vue
@@ -53,7 +53,8 @@
                   </span>
                   {{ layer.title }}
                   <template v-show = "!layer.rawdata">
-                    <span v-if = "state.query.pagination"
+                    <span 
+                      v-if   = "state.query.pagination && state.query.pagination.counts[index] > layer.features.length"
                       class  = "query-layer-feature-count"
                     >({{ layer.features.length + ((state.query.pagination.current[index] - 1) * getCurrentPagSize(index))}} - {{ state.query.pagination.counts[index] }})
                     </span>
