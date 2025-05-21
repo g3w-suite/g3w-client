71411a3ddcc5cedafc7a071c066ccdde3a097af7
Raruto <Raruto@users.noreply.github.com>
2025-05-21 09:50:11 +0200

    remove prefix

--- src/assets/geocoding-providers/qes.js
+++ src/assets/geocoding-providers/qes.js
@@ -14,7 +14,7 @@
   // }
 
   Object.assign(window.initConfig.mapcontrols.geocoding.providers['qes'], {
-    label: `Project (${window.location.host})`,
+    label: window.location.host,
     fetch: async (opts) => ({
       provider: 'qes',
       icon:     'layer-group',
@@ -22,7 +22,7 @@
       (
         await g3wsdk.core.utils.XHR.get({ url: `${initConfig.baseurl}qes/api/search/${g3wsdk.core.ApplicationState.project.getId()}/?q=${opts.query}` })
       ).results.map(result => ({
-        layer_id:  result.layer_id, //layer
+        layer_id:  result.layer_id,
         name:      result.attributes.name || result.feature_id,
         type:      result.layer_name,
         qes:       result,
