758d325aae053bfdd5c86422834f73d0229b636b
volterra79 <boccacci.francesco@gmail.com>
2025-06-04 13:47:05 +0200

    Implement ANNOTATION parameter on getPrint convert it to 4326 and set only useful properties

--- src/map/controls/annotationcontrol.js
+++ src/map/controls/annotationcontrol.js
@@ -10,6 +10,10 @@ import { saveBlob }               from 'utils/saveBlob';
 import { createMeasureTooltip }   from 'utils/createMeasureTooltip';
 import { areCoordinatesEqual }    from 'utils/areCoordinatesEqual';
 
+//Methods used to get lengh and area oa a feature 
+const parse_length = len  => len > 100 ? (Math.round((len / 1000) * 100) / 100) +  ' km' : (Math.round(len * 100) / 100) + ' m';
+const parse_area   = area => area > 10000 ? (Math.round((area / 1000000) * 100) / 100) +  ' km²' : (Math.round(area * 100) / 100) + ' m²';
+
 export class AnnotationControl extends InteractionControl {
 
   /** Incremental counter for added features */
@@ -607,7 +611,42 @@ export class AnnotationControl extends InteractionControl {
       const features = this._annotation.layer.getSource().getFeatures();
       //@TODO convert based on https://github.com/g3w-suite/g3w-admin/pull/1112
       if (features.length > 0) {
-        params.ANNOTATIONS = JSON.stringify(this.#convertFrom4326(new ol.format.GeoJSON().writeFeatures(features)));
+        params.ANNOTATIONS = JSON.stringify(
+          new ol.format.GeoJSON().writeFeatures(
+            this.#convertTo4326(features)
+            .map(f => {
+              //get style
+              const { color, width, radius, opacity, rotation, fontsize, direction } = f.get('style');
+              let label = '';
+              switch(f.get('type')) {
+                case 'Text':
+                  label = f.get('text');
+                  f.set('style', { rotation, fontsize });
+                  break;
+                case 'Point':
+                  label = `${f.get('show_info') && `${`${f.getGeometry().getCoordinates()}`} \n` || ''}${f.get('show_text') && f.get('text') || ''}`;
+                  f.set('style', { color, radius, fontsize });
+                  break;
+                case 'LineString':
+                  label = `${f.get('show_info') && (parse_length(f.getGeometry().getLength()) + '\n') || ''}${f.get('show_text') && f.get('text') || ''}`;
+                  f.set('style', { color, width, fontsize, direction });
+                  break;
+                case 'Polygon':
+                case 'Rectangle':
+                  label = `${f.get('show_info') && (parse_area(f.getGeometry().getArea()) + '\n') || ''}${f.get('show_text') && f.get('text') || ''}`;
+                  f.set('style', { color, width, fontsize, opacity });
+                  break;
+                case 'Circle':
+                  break;      
+              }
+              //remove text- Unused on print
+              f.unset('text');
+              //add label
+              f.set('label', label);
+              return f;
+            })
+          )
+        );
       }
     });
   }
@@ -748,7 +787,7 @@ export class AnnotationControl extends InteractionControl {
       }
       // In case of not Circle, transform geometry coordinates
       if ('Circle' !== f.get('type')) {
-        _f.getGeometry().setCoordinates(ol.proj.transform(f.getGeometry().getCoordinates(), epsg, 'EPSG:4326'));
+        _f.getGeometry().transform(epsg, 'EPSG:4326');
       }
       return _f;
     })
@@ -1101,9 +1140,6 @@ export class AnnotationControl extends InteractionControl {
     const stroke       = new ol.style.Stroke({ color: '#FFF', width: 3 });
     const font_family  = 'Titillium Web';
     const image        = new ol.style.Circle({ radius: 5, stroke: new ol.style.Stroke({ color: '#000', width: 3 }) });
-
-    const parse_length = len  => len > 100 ? (Math.round((len / 1000) * 100) / 100) +  ' km' : (Math.round(len * 100) / 100) + ' m';
-    const parse_area   = area => area > 10000 ? (Math.round((area / 1000000) * 100) / 100) +  ' km²' : (Math.round(area * 100) / 100) + ' m²';
 
     if ('Text' === type) {
       return feat => new ol.style.Style({  
