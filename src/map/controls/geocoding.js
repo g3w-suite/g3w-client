bea14e3f6f883ec7d6ca3ef150c5e90dc411b70d
Raruto <Raruto@users.noreply.github.com>
2025-06-09 16:30:15 +0200

    readGeometry

--- src/map/controls/geocoding.js
+++ src/map/controls/geocoding.js
@@ -504,7 +504,7 @@ class GeocodingControl extends ol.control.Control {
       // lazy load item geometry
       if (PROVIDERS[item.provider]?.fetch_geom) {
         const geom = await PROVIDERS[item.provider].fetch_geom(item);
-        feature = geom && new ol.Feature({ geometry: (new ol.format.GeoJSON(geom)).readGeometry()});
+        feature = geom && new ol.Feature({ geometry: (new ol.format.GeoJSON()).readGeometry(geom) });
       }
 
       // skip invalid items (ie. no geometry)
