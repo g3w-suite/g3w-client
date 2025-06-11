cf9eee4d908de44e01eaa4e739dbd2fa3aa9d4b2
Raruto <Raruto@users.noreply.github.com>
2025-06-11 11:51:54 +0200

    `pointer-events: none`

--- src/utils/createMeasureTooltip.js
+++ src/utils/createMeasureTooltip.js
@@ -1,19 +1,35 @@
-import ApplicationState          from 'store/application';
-import { isLineGeometryType }    from "utils/isLineGeometryType";
-import { isPolygonGeometryType } from "utils/isPolygonGeometryType";
-import { isMultiGeometry }       from "utils/isMultiGeometry";
+import ApplicationState from 'store/application';
+
+const round   = val      => (Math.round(val * 100) / 100).toFixed(2);
+const degrees = (c1, c2) => parseInt(Math.atan2(c1[0] - c2[0], c1[1] - c2[1]) * 180 / Math.PI);
+
+const formatted_area = area => 'nautical' === ApplicationState.map.unit
+  ? `${area * 0.000000291553349598122862913947445759414840765222583489217190918463024037990567} nmi²`
+  : area > 1000000
+    ? `${round(area / 1000000)} km²`
+    : `${round(area)} m²`;
+
+const formatted_length = length => 'nautical' === ApplicationState.map.unit
+  ? `${length * 0.0005399568} nm`
+  : length > 1000
+    ? `${round(length / 1000)} km`
+    : `${round(length)} m`;
+
+const is_spherical = map => 'EPSG:3857' === map.getView().getProjection().getCode() || 'degrees' === map.getView().getProjection().getUnits();
 
 /**
  * create and add measure tooltip 
  */
 export function createMeasureTooltip({ map, feature } = {}) {
-  const element     = document.createElement('div');
-  element.className = 'mtooltip mtooltip-measure';
+  const element               = document.createElement('div');
+  element.className           = 'mtooltip mtooltip-measure';
+  element.style.pointerEvents = 'none';
 
   const tooltip = new ol.Overlay({
     element,
     offset:      [0, -15],
-    positioning: 'bottom-center'
+    positioning: 'bottom-center',
+    stopEvent:   false // disable pointer events
   });
 
   map.addOverlay(tooltip);
@@ -21,90 +37,48 @@ export function createMeasureTooltip({ map, feature } = {}) {
   const unbyKey = feature
     .getGeometry()
     .on('change', e => {
-      let coords;
       const geom = e.target;
 
+      const segments = /^Polygon|^MultiPolygon/.test(geom.getType()) && (geom?.getPolygons?.() || [geom]).flatMap(p => p.getLinearRing().getCoordinates()) || [];
+
+      const length = /^Line|^MultiLine/.test(geom.getType()) || (/^Polygon|^MultiPolygon/.test(geom.getType()) && segments.length > 2)
+        ? is_spherical(map)
+          ? ol.sphere.getLength(segments.length ? new ol.geom.LineString(segments) : geom, { projection: map.getView().getProjection().getCode() })
+          : /^Multi/.test(geom.getType())
+            ? geom.getLineStrings().reduce((len, geom) => len+=geom.getLength(), 0)
+            : (new ol.geom.LineString(segments)).getLength()
+        : undefined;
+
+      const area = /^Polygon|^MultiPolygon/.test(geom.getType())
+        ? is_spherical(map)
+            ? ol.sphere.getArea(geom, { projection: map.getView().getProjection().getCode() })
+            : geom.getArea()
+        : undefined;
+
+      const radius = 'Circle' === geom.getType() ? geom.getRadius() : undefined;
+
+      element.innerHTML = [
+        area   && `Area: ${formatted_area(area)} <br>`,
+        area   && length && `<div style="width: 100%; padding: 3px; border-bottom: 2px solid #fff"></div> `,
+        length && formatted_length(length),
+        radius && `${degrees(geom.getCenter(), feature.get('endCoordinates'))}°, ${formatted_length(radius)}`,
+      ].filter(Boolean).join('');
+
       if (geom instanceof ol.geom.Polygon) {
-        coords = geom.getInteriorPoint().getCoordinates();
+        tooltip.setPosition(geom.getInteriorPoint().getCoordinates());
       }
 
       if (geom instanceof ol.geom.MultiPolygon) {
-        coords = geom.getInteriorPoints().getCoordinates()[0];
+        tooltip.setPosition(geom.getInteriorPoints().getCoordinates()[0]);
       }
 
       if (geom instanceof ol.geom.LineString || geom instanceof ol.geom.MultiLineString) {
-        coords = geom.getLastCoordinate();
+        tooltip.setPosition(geom.getLastCoordinate());
       }
 
       if (geom instanceof ol.geom.Circle && feature.get('endCoordinates')) {
-        coords = feature.get('endCoordinates');
+        tooltip.setPosition(feature.get('endCoordinates'));
       }
-
-      const projection = map.getView().getProjection();
-      const is_line    = isLineGeometryType(geom.getType());
-      const is_poly    = isPolygonGeometryType(geom.getType());
-      const is_circle  = 'Circle' === geom.getType(); // add circle geometry type
-      const is_multi   = isMultiGeometry(geom.getType());
-      const is_sphere  = 'EPSG:3857' === projection.getCode() || 'degrees' === projection.getUnits();
-
-      let segments     = [];
-
-
-      if (is_poly && is_multi) {
-        (geom.getPolygons() || []).forEach(p => p.getLinearRing().getCoordinates().forEach(coords => segments.push(coords)));
-      }
-
-      if (is_poly && !is_multi) {
-        segments = geom.getLinearRing().getCoordinates();
-      }
-
-      const _geom = (is_poly && segments.length > 2) ? new ol.geom.LineString(segments) : geom;
-
-      let length = (is_line || (is_poly && segments.length > 2))
-        ? is_sphere
-          ? ol.sphere.getLength(_geom, { projection: projection.getCode() })
-          : isMultiGeometry(_geom.getType())
-            ? _geom.getLineStrings().reduce((totalLength, lineGeometry) => totalLength+= lineGeometry.getLength(), 0)
-            : _geom.getLength()
-        : undefined;
-
-      let area = is_poly
-        ? Math.round(
-            is_sphere
-              ? ol.sphere.getArea(geom, { projection: projection.getCode() })
-              : geom.getArea()
-          )
-        : undefined;
-
-      let radius = is_circle ? geom.getRadius() : undefined;
-
-      if (undefined !== radius) {
-        radius = radius > 1000
-          ? `${(Math.round(radius / 1000 * 100) / 100).toFixed(3)} km`
-          : `${(Math.round(radius * 100) / 100).toFixed(2)} m`;
-      }  
-
-      if (undefined !== length) {
-        length = 'nautical' === ApplicationState.map.unit
-          ? `${length * 0.0005399568} nm`
-          : length > 1000
-            ? `${(Math.round(length / 1000 * 100) / 100).toFixed(3)} km`
-            : `${(Math.round(length * 100) / 100).toFixed(2)} m`;
-      }
-
-      if (undefined !== area) {
-        area = 'nautical' === ApplicationState.map.unit
-          ? `${area * 0.000000291553349598122862913947445759414840765222583489217190918463024037990567}  nmi²`
-          : area > 1000000
-            ? `${(Math.round(area / 1000000 * 100) / 100).toFixed(6)} km<sup>2</sup>`
-            : `${(Math.round(area * 100) / 100).toFixed(3)} m<sup>2</sup>`;
-      }
-
-      element.innerHTML = (undefined !== area ? `Area: ${area} <br>` : '')
-                        + (undefined !== area && undefined !== length ? `<div style="width: 100%; padding: 3px; border-bottom: 2px solid #ffffff"></div> ` : '')
-                        + (undefined !== length ? length : '')
-                        + (undefined !== radius ? `${parseInt(Math.atan2(geom.getCenter()[0] - coords[0], geom.getCenter()[1] - coords[1]) * 180 / Math.PI)}°, ${radius}` : '');
-      tooltip.setPosition(coords);
     });
 
   return {
