f0afdfc4df6653eceb1b734237b8e52a160ce129
Raruto <Raruto@users.noreply.github.com>
2025-07-02 17:14:16 +0200

    fix nesting

--- src/directives/v-t-tooltip.js
+++ src/directives/v-t-tooltip.js
@@ -82,9 +82,8 @@ document.addEventListener('mousemove', showTooltip);
 document.addEventListener('mousedown', showTooltip);
 
 function showTooltip(e) {
-  let element = document.elementFromPoint(e.clientX, e.clientY);
-  let title = document.elementFromPoint(e.clientX, e.clientY)?.closest('[data-i18n-title], [title]');
-  title   = title?.getAttribute('data-i18n-title') ?? title?.getAttribute('title');
+  const element = document.elementFromPoint(e.clientX, e.clientY)?.closest('[data-i18n-title], [title]');
+  const title   = element?.getAttribute('data-i18n-title') ?? element?.getAttribute('title');
 
   if (!element || !title || element?.closest('.select2')) {
     tooltip.hidePopover();
