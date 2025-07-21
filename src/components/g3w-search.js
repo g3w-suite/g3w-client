0f5a37575a82c9c289cc8e692c34fb2eb2481cb5
volterra79 <boccacci.francesco@gmail.com>
2025-07-21 12:56:31 +0200

    Undo remove a part of code

--- src/components/g3w-search.js
+++ src/components/g3w-search.js
@@ -121,6 +121,11 @@ export function SearchPanel(opts = {}, show = false) {
           ]
         ).map(value => 'Object' === toRawType(value) ? value : ({ key: value, value })) : [];
 
+      //In case of search with autofilter that return no data, need to setup select input to all
+      if (1 === input.values.length && SEARCH_ALLVALUE === input.values[0].value && ['selectfield', 'autocompletefield'].includes(input.type)) {
+        input.value = 'in' === input.operator ? [SEARCH_ALLVALUE] : SEARCH_ALLVALUE; // set default value for select
+      };
+      
       // there is a dependance
       if (input.dependance) {
         state.loading[input.dependance] = false;
