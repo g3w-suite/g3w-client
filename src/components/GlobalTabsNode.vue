36ef350a172279c11dc34cc1f14fefdbdd8d0aee
volterra79 <boccacci.francesco@gmail.com>
2025-08-11 15:35:52 +0200

    🐛 Get alias from editor from structure when as field label

--- src/components/GlobalTabsNode.vue
+++ src/components/GlobalTabsNode.vue
@@ -218,7 +218,13 @@
        */
       getField(node) {
         if (node.relation) { return node }
-        return this.fields.find(f => (node.field_name ? node.field_name.replace(/ /g,"_") : node.field_name) === f.name);
+        //get field of layer
+        const field = this.fields.find(f => (node.field_name ? node.field_name.replace(/ /g,"_") : node.field_name) === f.name);
+        //in case of showlabel is true, set label to alias
+        if (node.showlabel) {
+          field.label = node.alias;
+        }
+        return field;
       },
       /**
        *
