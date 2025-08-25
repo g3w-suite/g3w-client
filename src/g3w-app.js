422b3ba37f847a0aea3833cf65f9ca31f850fb4b
volterra79 <boccacci.francesco@gmail.com>
2025-08-25 14:38:01 +0200

    Fix query layer, editable, not yet load editing plugin

--- src/g3w-app.js
+++ src/g3w-app.js
@@ -2131,12 +2131,12 @@ export default new (class GUI extends Emitter {
           expandable:             true,
           addfeaturesresults:     { active: false },
           downloadformats:        { active: false },
-          editable:               is_layer   ? layer.isEditable() && layer.config.editing.visible : false,
-          inediting:              is_layer   ? layer.isInEditing()                                : false,
-          source:                 is_layer   ? layer.getSource()                                  : undefined,
-          infoformat:             is_layer   ? layer.getInfoFormat()                              : undefined,
-          infoformats:            is_layer   ? layer.getInfoFormats()                             : [],
-          downloads:              is_layer   ? layer.getDownloadableFormats()                     : [],
+          editable:               is_layer   ? layer.isEditable() && layer.config.editing?.visible : false,
+          inediting:              is_layer   ? layer.isInEditing()                                 : false,
+          source:                 is_layer   ? layer.getSource()                                   : undefined,
+          infoformat:             is_layer   ? layer.getInfoFormat()                               : undefined,
+          infoformats:            is_layer   ? layer.getInfoFormats()                              : [],
+          downloads:              is_layer   ? layer.getDownloadableFormats()                      : [],
           formStructure:          structure  ? {
             structure,
             // get field show
