06f19440cc85c1eb5c41d765a424d9364faf5910
Raruto <Raruto@users.noreply.github.com>
2025-08-26 14:34:31 +0200

    alert

--- src/index.dev.js
+++ src/index.dev.js
@@ -15,8 +15,6 @@ import 'g3w-globals';
 // print some debug info
 window.g3wsdk.info();
 
-alert('ciao');
-
 // custom header links
 g3w.app.once('initconfig', () => {
   initConfig.header_custom_links = [
