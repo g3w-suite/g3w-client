7a4ee338f2ccd825367d7c8724f7a921448a0152
Raruto <Raruto@users.noreply.github.com>
2025-08-27 09:45:24 +0200

    jsdoc

--- test/dev.g3wsuite.it.js
+++ test/dev.g3wsuite.it.js
@@ -1,3 +1,8 @@
+/**
+ * @file test your local JS code (development) against a remote server (production)
+ * @since 4.1.0
+ */
+
 const { chromium } = require('playwright');
 const path         = require('path');
 const fs           = require('fs');
