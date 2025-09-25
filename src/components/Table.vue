6885bd14c804c0dfb2c292f1693d1ae1245dfcce
volterra79 <boccacci.francesco@gmail.com>
2025-09-25 14:50:43 +0200

    search to state.search

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -150,9 +150,9 @@
 
       <!-- PAGINATION BUTTONS -->
       <div style="margin-left: auto;">
-        <button @click.stop = "changePage(-1)" class="btn" v-disabled = "1 === search.page">«</button>
-        <select v-model = "search.page" style = "padding: 5px 12px; appearance: none; border: 0; text-align: center; border-radius: 3px;">
-          <option v-for="p in 10" :selected="p == search.page">{{ p }}</option>
+        <button @click.stop = "changePage(-1)" class="btn" v-disabled = "1 === state.search.page">«</button>
+        <select v-model = "state.search.page" style = "padding: 5px 12px; appearance: none; border: 0; text-align: center; border-radius: 3px;">
+          <option v-for="p in 10" :selected = "p == state.search.page">{{ p }}</option>
         </select>
         <button @click.stop = "changePage(+1)" class="btn">»</button>
       </div>
