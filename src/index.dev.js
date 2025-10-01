16568552957cdca7e357d2e690ae2efb873e550c
volterra79 <boccacci.francesco@gmail.com>
2025-10-01 17:22:21 +0200

    Reduce layer id input

--- src/index.dev.js
+++ src/index.dev.js
@@ -355,7 +355,7 @@ g3wsdk.gui.GUI.once('ready', () => {
                   <iframe src="${location.href}"></iframe>
                   <div id = "g3w-iframe-simpleediting" style = "display: flex; flex-direction: column; justify-content: space-between; width:40vw;height:100%">
                     <div id = "g3w-iframe-simpleediting-input" style = " display: flex; flex-direction: column; height: 100%;">
-                      <input    id = "g3w-iframe-simpleediting-layerid" style = "height: 50px; " placeholder  = "Insert Layer Id" />
+                      <input    id = "g3w-iframe-simpleediting-layerid" style = "height: 30px; " placeholder  = "Insert Layer Id" />
                       <textarea id = "g3w-iframe-simpleediting-geojson" style = "flex-grow: 2; border-width: 1px 0 0 0;" placeholder  = "Paste GeoJson"></textarea>
                     </div>
                     <div id = "g3w-iframe-simpleediting-buttons" style = "display: flex; flex-direction: column;">
