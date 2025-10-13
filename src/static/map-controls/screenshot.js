6508af5621f24f7390155b54a53ee233af1adc9f
volterra79 <boccacci.francesco@gmail.com>
2025-10-13 16:38:02 +0200

    i18n translation screenshot

--- src/static/map-controls/screenshot.js
+++ src/static/map-controls/screenshot.js
@@ -75,7 +75,7 @@ template: /*html*/`
       :select2_value = "state.template"
       :style         = "{ marginBottom: this.state.atlas && '10px' }"
     >
-      <option v-for = "print in state.print" :value = "print.name">{{ print.label || print.name }}</option>
+      <option v-for = "print in state.print" :value = "print.name" v-t = "print.label || print.name"></option>
     </select>
 
     <details v-if="is_customizable" class="custom-settings">
@@ -1101,7 +1101,7 @@ GUI.setupControl.geoscreenshot = function(type) {
     });
     control.on('toggled', ({ toggled }) => showPrintUserMessage(toggled, 'screenshot'));
     //add screenshot template
-    ApplicationState.project.state.print.push({ name: '__G3W_SCREENSHOT__', label: (new Vue()).$t('Screenshot'), maps: [], labels: [] });
+    ApplicationState.project.state.print.push({ name: '__G3W_SCREENSHOT__', label: 'Screenshot', maps: [], labels: [] });
     GUI.addControl('screenshot', control);
     Vue.watch(
       () => ApplicationState.usermessage.show,
