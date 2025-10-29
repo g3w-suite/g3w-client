05cd02279e09ed378cb97d1d9fd712655885c2ca
Raruto <Raruto@users.noreply.github.com>
2025-10-29 13:55:23 +0100

    template literal

--- src/static/map-controls/zoomhistory.js
+++ src/static/map-controls/zoomhistory.js
@@ -15,27 +15,29 @@ GUI.setupControl.zoomhistory = function() {
       ol: new (class extends ol.control.Control {
         constructor() {
           super({
-            element: Object.assign(document.createElement('div'), { className: 'ol-zoom-history ol-unselectable ol-control' }),
             target: document.querySelector('.g3w-map-controls-left-bottom'),
+            element: Object.assign(document.createElement('template'), {
+              innerHTML: /* html */ `
+                <div class="ol-zoom-history ol-unselectable ol-control" style="display:flex; gap: 5px;">
+                  <div title="Zoom Last" data-placement="top">
+                    <button type="button" value="last" class="fas fa-reply g3w-disabled" style="font-weight: 900;"></button>
+                  </div>
+                  <div title="Zoom Next" data-placement="top">
+                    <button type="button" value="next" class="fas fa-share g3w-disabled" style="font-weight: 900;"></button>
+                  </div>
+                </div>
+              `.trim()
+            }).content.firstChild,
           });
           const map     = GUI.getMap();
           const history = [];
           let curr      = 0;
-          this.element.style.display = 'flex';
-          this.element.style.gap     = '5px';
-          this.element.innerHTML     = /* html */`
-            <div data-i18n-title="Zoom Last"><button type="button" value="last" class="fas fa-reply g3w-disabled" style="font-weight: 900;"></button></div>
-            <div data-i18n-title="Zoom Next"><button type="button" value="next" class="fas fa-share g3w-disabled" style="font-weight: 900;"></button></div>
-          `;
-          this.element.querySelectorAll('button').forEach(btn => {
-            btn.parentElement.setAttribute('data-placement', 'top');
-            btn.addEventListener('click', e => {
-              curr += 'last' === e.currentTarget.value ? -1 : +1;
-              GUI.getMap().getView().fit(history.at(curr));
-              this.element.querySelector('button[value=last]').classList.toggle('g3w-disabled', 0 === curr);
-              this.element.querySelector('button[value=next]').classList.toggle('g3w-disabled', history.length - 1 === curr);
-            })
-          });
+          this.element.querySelectorAll('button').forEach(btn => btn.addEventListener('click', e => {
+            curr += 'last' === e.currentTarget.value ? -1 : +1;
+            map.getView().fit(history.at(curr));
+            this.element.querySelector('button[value=last]').classList.toggle('g3w-disabled', 0 === curr);
+            this.element.querySelector('button[value=next]').classList.toggle('g3w-disabled', history.length - 1 === curr);
+          }));
           history.push(map.getView().calculateExtent(map.getSize()));
           map.getView().on('change' , debounce(() => {
             if (curr !== history.length - 1) {
@@ -46,7 +48,7 @@ GUI.setupControl.zoomhistory = function() {
             this.element.querySelector('button[value=last]').classList.toggle('g3w-disabled', 0 === curr);
             this.element.querySelector('button[value=next]').classList.toggle('g3w-disabled', history.length - 1 === curr);
           }, 600));
-          GUI.getMap().addControl(this);
+          map.addControl(this);
         }
       })
     }
