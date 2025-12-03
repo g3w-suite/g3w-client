f18d2523a6242662f3beab4b6bca71b6d5cc1598
volterra79 <boccacci.francesco@gmail.com>
2025-12-03 12:11:44 +0100

    Code format

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -333,19 +333,19 @@
       </a>
     </li>
 
-    <li v-if="'map' === context" @click="queryCoords">{{ $t('Query layer') }}</li>
-    <li v-if="'map' === context" @click="zoomIn">{{ $t('Zoom in') }}</li>
-    <li v-if="'map' === context" @click="zoomOut">{{ $t('Zoom out') }}</li>
-    <li v-if="'map' === context" @click="zoomHome">{{ $t('Fit map extent') }}</li>
-    <li v-if="'map' === context && initConfig.mapcontrols.screenshot" @click="takeScreenshot">{{ $t('Screen capture') }}</li>
-    <li v-if="'map' === context && initConfig.mapcontrols.streetview" @click="showStreetView">{{ $t('StreetView') }}</li>
+    <li v-if = "'map' === context" @click = "queryCoords">{{ $t('Query layer') }}</li>
+    <li v-if = "'map' === context" @click = "zoomIn">{{ $t('Zoom in') }}</li>
+    <li v-if = "'map' === context" @click = "zoomOut">{{ $t('Zoom out') }}</li>
+    <li v-if = "'map' === context" @click = "zoomHome">{{ $t('Fit map extent') }}</li>
+    <li v-if = "'map' === context && initConfig.mapcontrols.screenshot" @click="takeScreenshot">{{ $t('Screen capture') }}</li>
+    <li v-if = "'map' === context && initConfig.mapcontrols.streetview" @click="showStreetView">{{ $t('StreetView') }}</li>
 
     <!-- Click to open G3W-ADMIN's project page -->
     <li v-if = "edit_url && ['project', 'map'].includes(context)">
       <a :href = "edit_url" @click.stop = "closeMenu" target = "_blank" style = "color: initial">
         <!-- TODO: g3wtemplate.getFontClass('qgis') -->
         <i>
-          <svg xmlns="http://www.w3.org/2000/svg" xml:space = "preserve" viewBox = "0 0 32 32" style = "height: 14px; vertical-align: -1.5px; fill: currentColor;">
+          <svg xmlns = "http://www.w3.org/2000/svg" xml:space = "preserve" viewBox = "0 0 32 32" style = "height: 14px; vertical-align: -1.5px; fill: currentColor;">
             <path d = "m17.61 17.63 4.36-.02-4-3.98h-4.36v4l4 4.45z"/>
             <path d = "m31.61 27.22-7.62-7.6-4.38.01v4.33l7.24 7.67h4.76z"/>
             <path d = "M18 25.18c-.68.16-1.17.2-1.9.2a9.77 9.77 0 0 1-9.68-9.88c0-5.57 4.4-9.78 9.68-9.78s9.48 4.2 9.48 9.78c0 .91-.15 1.96-.36 2.8l4.88 4.65a15 15 0 0 0 1.95-7.48C32.05 6.87 25.19.44 16 .44 6.86.44 0 6.84 0 15.47c0 8.68 6.86 15.2 16 15.2 2.36 0 4.23-.3 6.2-1.1L18 25.18z"/>
