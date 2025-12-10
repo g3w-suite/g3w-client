01c724d495258eec649068821efb49941dcca510
Raruto <Raruto@users.noreply.github.com>
2025-12-10 15:59:20 +0100

    custom context

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -15,7 +15,8 @@
       left: left + 'px',
     }"
   >
-    <!--ÒSINCE 4.1.0 CUSTOM CONTENT MENU ITEM-->
+
+    <!-- SINCE 4.1.0 CUSTOM CONTENT MENU ITEM-->
     <li v-for = "item in items" :key = "item.id"  @click.prevent.stop = "item.cbk">
       <i v-if = "item.children" :class = "$fa('arrow-right')" style  = "position: absolute; right: 0; margin-top: 3px"></i>
       <i v-if = "item.icon" :class = "$fa(item.icon)"></i> {{ $t(item.label) }}
@@ -31,6 +32,7 @@
         </li>
       </ul>
     </li>
+
     <!-- MENU NAME -->
     <li v-if="['project', 'layer'].includes(context)" class = "title">
       <div v-if = "'project' === context">G3W-ADMIN {{ ApplicationState.project.getName() }}</div>
@@ -508,6 +510,34 @@
         if (!layerstree && !layer) {
           this.context = 'map'; 
         }
+
+      /**
+       * @since 4.1.0 custom context menu items
+       * 
+       * Example: Single item addition
+       * ```js
+       * GUI.once('map:context-menu', menu => {
+       *   menu.items.push({
+       *     id: 'test',
+       *     icon: 'pencil',
+       *     label: 'TEST',
+       *     cbk: () => alert('Test')
+       *   });
+       * });
+       * ```
+       * Example : Item with children (sub menu)
+       * ```js
+       * GUI.once('layer:context-menu', menu => {
+       *   menu.items.push({
+       *     id: 'testsubmenu',
+       *     icon: 'pencil',
+       *     label: 'TEST CHILDREN',
+       *     children: [{ id: 'child1', labe: 'Child 1', cbk: () => alert('child 1') }]
+       *   });
+       * });
+       * ```
+       */
+        GUI.emit(`${this.context}:context-menu`, this);
 
         await this.$nextTick();
 
@@ -1046,27 +1076,6 @@
           this.map_coords = GUI.getMap().getCoordinateFromPixel([e.pixel[0], e.pixel[1]]);
           GUI.emit('context-menu', e.originalEvent);
         }
-      });
-
-      /**
-       * @since 4.1.0 Add remove custom context menu item
-       * 
-       * Example : Single item addition
-       * GUI.emit('content-menu-item', { actions: 'add', item: { id: 'test', icon: 'pencil', label: 'TEST', cbk: () => alert('Test')} })
-       * 
-       * Example : Item with children (sub menu)
-       * GUI.emit('content-menu-item', { actions: 'add', item: { id: 'testsubmenu', icon: 'pencil', label: 'TEST CHILDREN', children: [{ id: 'child1', labe: 'Child 1', cbk: () => alert('chaild 1') }]} })
-       */
-      GUI.on('content-menu-item', ({ action = "add", item = {} }) => {
-        //add new Item
-        if ('add' === action) {
-          this.items.push(item);
-        }
-        //remove item
-        if ('delete' === action) {
-          this.items = this.items.filter(i => item.id !== i.id);
-        }
-        
       });
     
     },
