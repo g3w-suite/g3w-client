a8e09a3ea5c4017908ed0788052eed740d42debc
Raruto <Raruto@users.noreply.github.com>
2026-01-27 09:45:20 +0100

    revert i18n

--- src/index.prod.js
+++ src/index.prod.js
@@ -355,7 +355,7 @@ initConfig.header_custom_links.unshift({
   type:   'metadata',
   target: '#metadata_credits',
   icon:   'far fa-question-circle',
-  title:  'help',
+  title:  'Credits',
   i18n:    true,
 });
 
