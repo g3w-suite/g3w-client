5dd07fae44a5116465b240a7cc8cd79323199f17
volterra79 <boccacci.francesco@gmail.com>
2026-01-28 16:18:14 +0100

    Remove select arrow from languages

--- src/components/App.vue
+++ src/components/App.vue
@@ -174,7 +174,6 @@
           <button type = "button" commandfor = "nav-lang-dialog" command = "show-modal" style = "display: flex; gap:5px;">
             <img :src = "urls.staticurl +'img/flags/' + language.toLowerCase() + '.png'" width = "24" height = "16" alt = "" />
             {{ languages.find(l => l[0] === language).at(1) }}
-            <i class = "triangle" style = "margin-top: 8px;"></i>
           </button>
           <dialog 
             id     = "nav-lang-dialog" 
