9361bea694d7a3b9dbf0ea47b553193e027a1d38
volterra79 <boccacci.francesco@gmail.com>
2026-01-30 12:00:26 +0100

    https://github.com/g3w-suite/g3w-client/pull/882

--- src/utils/downloadFeatures.js
+++ src/utils/downloadFeatures.js
@@ -150,8 +150,8 @@ export async function downloadFeatures({
       ApplicationState.download = true;
       try {
         const format              = dialog.querySelector('[name="format"]').value;
-        const down_with_relations = Number(dialog.querySelector('[name="down_with_relations"]').value);
-        const down_with_polygon   = dialog.querySelector('[name="down_with_polygon"]')?.value;
+        //@since 4.0.6 Check if layer has relation downloadble otherwise force to 0
+        const down_with_relations = 1 * Boolean(catalog_layer?.hasDowloadableRelations?.()) * Number(dialog.querySelector('[name="down_with_relations"]').value);        const down_with_polygon   = dialog.querySelector('[name="down_with_polygon"]')?.value;
         let blob, filename;
 
         if ('external-url' === format) {
