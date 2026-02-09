2a3e304b138a74c78b4ef0f3d0514ecfe7653129
Raruto <Raruto@users.noreply.github.com>
2026-02-09 08:32:12 +0100

    extract xyz as helper function

--- src/index.dev.js
+++ src/index.dev.js
@@ -55,6 +55,13 @@ g3w.app.once('initconfig', () => {
 
 // custom context menu items
 g3w.app.on('map:context-menu', menu => {
+  const xyz = () => {
+    const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
+    const lon    = coords[0].toFixed(6);
+    const lat    = coords[1].toFixed(6);
+    const zoom   = Math.round(g3w.app.getMap().getView().getZoom()) || 17;
+    return { lat, lon, zoom };
+  };
   menu.items.push({
     label: 'View on external map',
     icon: 'external-link',
@@ -62,73 +69,49 @@ g3w.app.on('map:context-menu', menu => {
       {
         label: 'Google',
         cbk: () => {
-          const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
-          const lon    = coords[0].toFixed(6);
-          const lat    = coords[1].toFixed(6);
-          const zoom   = Math.round(g3w.app.getMap().getView().getZoom()) || 17;
+          const { lat, lon, zoom } = xyz();
           window.open(`https://www.google.com/maps/@${lat},${lon},${zoom}z`, '_blank');
         },
       },
       {
         label: 'Bing',
         cbk: () => {
-          const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
-          const lon    = coords[0].toFixed(6);
-          const lat    = coords[1].toFixed(6);
-          const zoom   = Math.round(g3w.app.getMap().getView().getZoom()) || 17;
+          const { lat, lon, zoom } = xyz();
           window.open(`https://www.bing.com/maps/?cp=${lat}~${lon}&lvl=${zoom}`, '_blank');
         },
       },
       {
         label: 'Yandex',
         cbk: () => {
-          const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
-          const lon    = coords[0].toFixed(6);
-          const lat    = coords[1].toFixed(6);
-          const zoom   = Math.round(g3w.app.getMap().getView().getZoom()) || 17;
+          const { lat, lon, zoom } = xyz();
           window.open(`https://yandex.com?maps/ll=${lat}%2C${lon}&z=${zoom}`, '_blank');
         },
       },
       {
         label: 'OSM',
         cbk: () => {
-          const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
-          const lon    = coords[0].toFixed(6);
-          const lat    = coords[1].toFixed(6);
-          const zoom   = Math.round(g3w.app.getMap().getView().getZoom()) || 17;
+          const { lat, lon, zoom } = xyz();
           window.open(`https://www.openstreetmap.org/#map=${zoom}/${lat}/${lon}`, '_blank');
         },
       },
       {
         label: 'Mapy.cz',
         cbk: () => {
-          const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
-          const lon    = coords[0].toFixed(6);
-          const lat    = coords[1].toFixed(6);
-          const zoom   = Math.round(g3w.app.getMap().getView().getZoom()) || 17;
+          const { lat, lon, zoom } = xyz();
           window.open(`https://mapy.com/it/turisticka?x=${lat}&y=${lon}&z=${zoom}`, '_blank');
         },
       },
       {
         label: 'Wikimapia',
         cbk: () => {
-          const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
-          const lon    = coords[0].toFixed(6);
-          const lat    = coords[1].toFixed(6);
-          const zoom   = Math.round(g3w.app.getMap().getView().getZoom()) || 17;
+          const { lat, lon, zoom } = xyz();
           window.open(`https://wikimapia.org/#lang=ro&lat=${lat}&lon=${lon}&z=${zoom}`, '_blank');
-
-          dialog.querySelector('#btn-meteoblu').onclick = () => openUrl(`https://www.meteoblue.com/en/weather/week/${lat}N${lon}E`);
-          
-          dialog.showModal();
         },
       },
       {
         label: 'Meteo blu',
         cbk: () => {
-          const coords = ol.proj.toLonLat(g3w.app.getMap().getView().getCenter());
-          const lon    = coords[0].toFixed(6);
-          const lat    = coords[1].toFixed(6);
+          const { lat, lon } = xyz();
           window.open(`https://www.meteoblue.com/en/weather/week/${lat}N${lon}E`, '_blank');
         },
       },
