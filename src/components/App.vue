1423104625a21e0806f06a01bbdf7607a209a228
volterra79 <boccacci.francesco@gmail.com>
2026-02-16 14:28:09 +0100

    await return

--- src/components/App.vue
+++ src/components/App.vue
@@ -1493,14 +1493,14 @@ export default {
               // if it has a style settled
               if (node.style) {
                 const promise = new Promise(resolve => {
-                  const setCurrentStyleAndResolvePromise = node => {
+                  const setCurrentStyleAndResolvePromise = async node => {
                     if (undefined === changes.layers[node.id]) {
                       changes.layers[node.id] = {
                         visibility: false,
                         style:      false
                       }
                     }
-                    changes.layers[node.id].style = project.getLayerById(node.id).changeCurrentStyle(node.style);
+                    changes.layers[node.id].style = await project.getLayerById(node.id).changeCurrentStyle(node.style);
                     resolve();
                   };
                   if (project.getLayersStore()) { setCurrentStyleAndResolvePromise(node) }
