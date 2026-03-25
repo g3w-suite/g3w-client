8758e5d8b7fd6709d6a267b4cf26b8eff2d41619
Raruto <Raruto@users.noreply.github.com>
2026-03-25 09:53:02 +0100

    code format

--- src/utils/isPointGeometryType.js
+++ src/utils/isPointGeometryType.js
@@ -1,6 +1,6 @@
 const TYPES = [
   'MultiPoint', 'MultiPointZ', 'MutliPointM', 'MultiPointZM', 'MultiPoint25D',
-  'Point',           'PointZ',      'PointM',      'PointZM',      'Point25D',
+       'Point',      'PointZ',      'PointM',      'PointZM',      'Point25D',
 ];
 
 export function isPointGeometryType(geometryType) {
