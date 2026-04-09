675fe4510dd76be9ab9a181ce724418dee2f27b5
Raruto <Raruto@users.noreply.github.com>
2026-04-09 16:58:47 +0200

    deprecate `devConfig()`

--- config.template.js
+++ config.template.js
@@ -1,40 +1,43 @@
 const { version } = require('./package.json');
 
-const G3W_PLUGINS = [                 // override "initConfig->group->plugins" attribute for custom plugin development
-  // "your-plugin-folder-name-1",
-  // "your-plugin-folder-name-2",
-  // "your-plugin-folder-name-3",
-];
-
 let conf = {
   pluginsFolder:          './src/plugins',                                  // path to G3W-CLIENT plugins folder
   admin_plugins_folder:   '../g3w-admin/g3w-admin',                         // path to G3W-ADMIN plugins folder
   admin_overrides_folder: '../g3w-suite-docker/config/g3w-suite/overrides', // path to G3W-SUITE overrides folder
   docker_plugins_folder:  '../g3w-suite-docker/shared-volume/plugins',      // path to G3W-SUITE plugins folder
-  plugins:                G3W_PLUGINS,
+  plugins:                [                                                 // override "initConfig->group->plugins" attribute for custom plugin development
+    // "your-plugin-folder-name-1",
+    // "your-plugin-folder-name-2",
+    // "your-plugin-folder-name-3",
+  ],
   proxy:                  'https://dev.g3wsuite.it/',                       // remote server url to be proxied
+  /**
+   * @deprecated since 4.1.0, use index.dev.js instead.
+   */
   devConfig() {
-    g3wsdk.core.ApplicationService.once('ready', () => { });
-    g3wsdk.core.ApplicationService.once('initconfig', () => {
-      initConfig.group.plugins = Object.assign(initConfig.group.plugins || {}, G3W_PLUGINS.reduce((a, v) => ({ ...a, [v]: { ...initConfig.group.plugins[v], gid: initConfig.group.initproject, baseUrl: initConfig.staticurl }}), {}));
-    });
-    //Every time a new iframe is created, listen for messages
-    g3wsdk.gui.GUI.on('iframe:message', (w, e) => { 
-      //Once app is ready, send a message to the iframe
-      if (e.data.action === 'app:ready') {
-        w.postMessage({ // test MESSAGE sent to "Open in iframe" map control
-          id: null,
-          action: 'app:getcenter',                                        // or 'app:getextent'
-          data: { epsg: 4326 }	
-        }, '*')
-      }
-    });
-    g3wsdk.gui.GUI.once('ready', () => { console.log('ready'); });
-    // dark mode
-    g3wsdk.gui.GUI.isReady().then(() => {
-      document.querySelector('nav').style.setProperty('--skin-color', '#212c31');
-    });
-    document.body.style.setProperty('--bgcolor', '#212c31');
+    if (g3wsdk.version.version.localeCompare('4.1.0', undefined, { numeric: true }) < 0) {
+      g3wsdk.core.ApplicationService.once('ready', () => { });
+      g3wsdk.core.ApplicationService.once('initconfig', () => {
+        initConfig.group.plugins = Object.assign(initConfig.group.plugins || {}, conf.plugins.reduce((a, v) => ({ ...a, [v]: { ...initConfig.group.plugins[v], gid: initConfig.group.initproject, baseUrl: initConfig.staticurl }}), {}));
+      });
+      //Every time a new iframe is created, listen for messages
+      g3wsdk.gui.GUI.on('iframe:message', (w, e) => { 
+        //Once app is ready, send a message to the iframe
+        if (e.data.action === 'app:ready') {
+          w.postMessage({ // test MESSAGE sent to "Open in iframe" map control
+            id: null,
+            action: 'app:getcenter',                                        // or 'app:getextent'
+            data: { epsg: 4326 }	
+          }, '*')
+        }
+      });
+      g3wsdk.gui.GUI.once('ready', () => { console.log('ready'); });
+      // dark mode
+      g3wsdk.gui.GUI.isReady().then(() => {
+        document.querySelector('nav').style.setProperty('--skin-color', '#212c31');
+      });
+      document.body.style.setProperty('--bgcolor', '#212c31');
+    }
   }
 };
 
--- src/index.dev.js
+++ src/index.dev.js
@@ -9,11 +9,41 @@ import shpwrite    from '@mapbox/shp-write';
 // expose global variables
 import 'g3w-globals';
 
+const CONF = require('../config');
+
 // apply dev config overrides (config.js)
-(require('../config').devConfig || (() => { })).call();
+(CONF.devConfig || (() => { })).call();
 
 // print some debug info
 window.g3wsdk.info();
+
+// add a custom PAGELENGTH (ref: Table.vue)
+g3w.constants.PAGELENGTHS.unshift(2);
+
+// override "initConfig->group->plugins" attribute for custom plugin development
+g3w.app.once('initconfig', () => {
+  initConfig.plugins = Object.assign(initConfig.plugins || {}, CONF.plugins.reduce((a, v) => ({ ...a, [v]: { ...initConfig.plugins[v], gid: initConfig.initproject, baseUrl: initConfig.staticurl }}), {}));
+});
+
+// Test MESSAGE sent to "Open in iframe" map control
+g3w.app.once('iframe:message', (w, e) => { w.postMessage({
+  id: null,
+  action: 'app:getcenter',                                        // or 'app:getextent'
+  data: { epsg: 4326 }	
+}, '*') });
+
+g3w.app.once('ready', () => { console.log('ready'); });
+
+// add 20 layer styles
+g3w.app.once('app-ready', () => {
+  g3w.state.project.state.layers.forEach(l => (l.styles || []).push(...Array(20).fill().map((_, i) => ({ name: `style_${i}`, current: false}))));
+});
+
+// dark mode
+g3w.app.isReady().then(() => {
+  document.querySelector('nav').style.setProperty('--skin-color', '#212c31');
+});
+document.body.style.setProperty('--bgcolor', '#212c31');
 
 // custom header links
 g3w.app.once('initconfig', () => {
