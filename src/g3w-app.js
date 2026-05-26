de52b72b1d12f58d0217a01a4d127d8b82440159
volterra79 <boccacci.francesco@gmail.com>
2026-05-26 12:13:27 +0200

    v-split full size

--- src/g3w-app.js
+++ src/g3w-app.js
@@ -1905,7 +1905,7 @@ export default new (class GUI extends Emitter {
     // size "content" - content floats on top, map always fills the full viewport
     Object.assign(ApplicationState.content.sizes, {
       width:  content.hidden ? 0 : (h_split ? Math.max(viewW * scale, 200) - is_full * document.querySelector('.main-sidebar').offsetWidth : viewW),
-      height: content.hidden ? 0 : (v_split ? Math.max(viewH * scale, 200) : viewH - map_footer.offsetHeight) } 
+      height: content.hidden ? 0 : (v_split ? Math.max(viewH * scale, 200) - is_full * map_footer.offsetHeight : viewH - map_footer.offsetHeight) } 
     );
 
     // handle sidebars
