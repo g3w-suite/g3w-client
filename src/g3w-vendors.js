70606c12e97659781256bf00a1b34e90ee1bf3d1
volterra79 <boccacci.francesco@gmail.com>
2026-05-27 10:34:58 +0200

    chore: monkey patch `ol.View.prototype.fitInternal`

--- src/g3w-vendors.js
+++ src/g3w-vendors.js
@@ -87,6 +87,21 @@ Vue.extend = function(opts) {
     opt.padding = default_padding.map((val, index) => val + custom_padding[index]);
     return fitProto.call(this, geom, opt);
   };
+
+  /**
+ * Monkey patch `ol.View.fitInternal` to accumulate default and custom padding.
+ * 
+ * @see https://openlayers.org/en/latest/apidoc/module-ol_View.html#~FitOptions
+ */
+  
+  const fitInternalProto = ol.View.prototype.fitInternal;
+   ol.View.prototype.fitInternal = function (geom, opt = {}) {
+    opt.fitOptions = { 
+      ...(opt.fitOptions || {}),
+      padding: this.get('padding') || [0, 0, 0, 0] 
+    };
+    return fitInternalProto.call(this, geom, opt);
+  };
 })();
 
 /**
