9f8d6fcaa17d28a0e1fef4353a69f57a8452b321
Raruto <Raruto@users.noreply.github.com>
2026-09-10 09:52:26 +0200

    refactor(modal-metadata): optional chaining

--- src/components/ModalMetadata.vue
+++ src/components/ModalMetadata.vue
@@ -559,10 +559,10 @@
     name: "modal-metadata",
 
     data() {
+      // get all layers with no geometry or with a valid EPSG code.
+      const layers  = ApplicationState.project.getLayers().filter(l => 'NoGeometry' === l.getGeometryType() || l.config.crs?.epsg);
       const project = ApplicationState.project.getState();
-      const layers  = ApplicationState.project.getLayers()   
-                      // In case of layers that has geometry and no epsg, filter according to filter of project layers
-                      .filter(l => 'NoGeometry' === l.getGeometryType() || (l.config.crs && l.config.crs.epsg));
+
       // @since 4.1.0 set WMS URL if not set by QGIS project
       if (!project.metadata.wms_url) {
         project.metadata.wms_url = `${project.WMSUrl}?service=WMS&version=1.3.0&request=GetCapabilities`;
